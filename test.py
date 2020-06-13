1ae710651b301251e9f0bea2b03137897f86011b
yzhh <OasisAkari@users.noreply.github.com>
2020-06-13 22:00:18 +0800

    1

--- test.py
+++
@@ -1,5 +0,0 @@
-import os, sys
-s = sys.argv[1]
-g = sys.argv[2]
-sss = s +' '+g
-os.system("python ttttt.py "+sss)
--- usercommand.py
+++
@@ -1,3 +0,0 @@
-import os, sys
-async def usercom(sss):
-    os.system("python ttttt.py "+sss)
