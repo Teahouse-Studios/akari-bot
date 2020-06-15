e8739d3216379dce77b2b2f3130a7fbc5d0c8464
yzhh <OasisAkari@users.noreply.github.com>
2020-06-15 20:04:13 +0800

    try fix

--- checkuser.py
+++ checkuser.py
@@ -1,6 +1,6 @@
 import requests
 import json
-async def checkuser(path,username):
+def checkuser(path,username):
     url = 'https://'+path+'.gamepedia.com/api.php?action=query&list=users&ususers='+username+'&usprop=groups%7Cblockinfo%7Cregistration%7Ceditcount%7Cgender&format=json'
     q = requests.get(url)
     file = json.loads(q.text)
--- test.py
+++ test.py
@@ -1,9 +1,5 @@
-import requests
-import json
-url = 'https://minecraft.gamepedia.com/api.php?action=query&list=users&ususers=Lightyzh&usprop=groups%7Cblockinfo%7Cregistration%7Ceditcount%7Cgender&format=json'
-q = requests.get(url)
-file = json.loads(q.text)
-if ('missing' in file['query']['users'][0]):
+from checkuser import checkuser
+if checkuser('minecraft','Lighyzhh'):
     print('t')
 else:
     print('f')
