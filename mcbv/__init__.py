b418c95cf430c9b0d462e93191bdd7bb53727f47
yzhh <OasisAkari@users.noreply.github.com>
2020-06-23 23:51:43 +0800

    Update __init__.py

--- mcbv/__init__.py
+++ mcbv/__init__.py
@@ -6,15 +6,17 @@ async def mcbv():
     q = requests.get(url)
     w = json.loads(q.text)
     f = []
+    z = []
     for x in w[:]:
         if x['archived'] == False:
             try:
                 e = re.match(r'(.*)Beta$',x['name'])
                 f.append(e.group(1))
             except Exception:
-                u = ('Release: '+x['name'])
+                z.append(x['name'])
         else:
             pass
     h = '| '
     d = h.join(f)
-    return('Beta:  '+str(d)+'\n'+u+'\n（数据来源于MoJira，可能会比官方发布要早一段时间。信息仅供参考。）')
+    u = h.join(z)
+    return('Beta: '+str(d)+'\nRelease: '+u+'\n（数据来源于MoJira，可能会比官方发布要早一段时间。信息仅供参考。）')
