baf13a0701329a0897fbb4c925c1f88910e770c6
yzhh <OasisAkari@users.noreply.github.com>
2020-07-06 17:29:58 +0800

    hardcode fixed

--- pathexist.py
+++ pathexist.py
@@ -3,18 +3,18 @@ import re
 
 def pathexist(ss):
     ss = re.sub('_','',ss)
-    d = '/home/oasisakari/botassests/Favicon/' + ss + '/'
+    d = '/home/wdljt/oasisakari/bot/assests/Favicon/' + ss + '/'
     if not os.path.exists(d):
         os.mkdir(d)
     else:
         pass
-    ddd = '/home/oasisakari/botassests/Favicon/' + ss + '/Wiki.png'
+    ddd = '/home/wdljt/oasisakari/bot/assests/Favicon/' + ss + '/Wiki.png'
     if not os.path.exists(ddd):
         return False
     else:
         return True
 def pathexist2(ss):
-    ddd = '/home/oasisakari/botassests/usercard/' + ss + '.png'
+    ddd = '/home/wdljt/oasisakari/bot/assests/usercard/' + ss + '.png'
     if not os.path.exists(ddd):
         return False
     else:
