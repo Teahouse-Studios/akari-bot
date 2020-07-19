ac0e4a7f04e60cd1fcf826ea996bbe7730084b69
yzhh <OasisAkari@users.noreply.github.com>
2020-07-19 10:25:50 +0800

    fixed a fatal error

--- server/serverraw.py
+++ server/serverraw.py
@@ -16,26 +16,29 @@ async def serverraw(address):
             port1 = '25565'
             port2 = '19132'
 
-        url = 'http://motd.wd-api.com/?ip='+serip+'&port='+port1+'&mode=info'
-        motd = requests.get(url,timeout=10)
-        file = json.loads(motd.text)
         try:
-            if file['code'] == 200:
-                x = file['data']['description']['text']
-                if not x:
-                    extra = file['data']['description']['extra']
-                    servers.append('[JE]\n'+str(extra)+"\n"+"在线玩家："+str(file['data']['players']['online'])+"/"+str(file['data']['players']['max'])+"\n"+"游戏版本："+file['data']['version']['name'])
-                else:
-                    servers.append('[JE]\n'+x+"\n"+"在线玩家："+str(file['data']['players']['online'])+"/"+str(file['data']['players']['max'])+"\n"+"游戏版本："+file['data']['version']['name'])
-            else:
-                print('获取JE服务器信息失败。')
-        except Exception:
+            url = 'http://motd.wd-api.com/?ip='+serip+'&port='+port1+'&mode=info'
+            motd = requests.get(url,timeout=10)
+            file = json.loads(motd.text)
             try:
-                x=file['data']['description']
-                servers.append('[JE]\n'+x+"\n"+"在线玩家："+str(file['data']['players']['online'])+"/"+str(file['data']['players']['max'])+"\n"+"游戏版本："+file['data']['version']['name'])
-            except Exception as e:
-                print('获取JE服务器信息失败。'+str(e))
-                servers.append("[JE]\n发生错误：调用API时发生错误。")
+                if file['code'] == 200:
+                    x = file['data']['description']['text']
+                    if not x:
+                        extra = file['data']['description']['extra']
+                        servers.append('[JE]\n'+str(extra)+"\n"+"在线玩家："+str(file['data']['players']['online'])+"/"+str(file['data']['players']['max'])+"\n"+"游戏版本："+file['data']['version']['name'])
+                    else:
+                        servers.append('[JE]\n'+x+"\n"+"在线玩家："+str(file['data']['players']['online'])+"/"+str(file['data']['players']['max'])+"\n"+"游戏版本："+file['data']['version']['name'])
+                else:
+                    print('获取JE服务器信息失败。')
+            except Exception:
+                try:
+                    x=file['data']['description']
+                    servers.append('[JE]\n'+x+"\n"+"在线玩家："+str(file['data']['players']['online'])+"/"+str(file['data']['players']['max'])+"\n"+"游戏版本："+file['data']['version']['name'])
+                except Exception as e:
+                    print('获取JE服务器信息失败。'+str(e))
+                    servers.append("[JE]\n发生错误：调用API时发生错误。")
+        except Exception as e:
+            print('获取JE服务器信息失败。'+str(e))
         try:
             BE = await main(serip,port2)
             servers.append(BE)
