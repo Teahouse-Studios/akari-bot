621126be1046a00cd41774f02fd3eab7b7a362c7
yzhh <OasisAkari@users.noreply.github.com>
2020-07-26 09:34:36 +0800

    1

--- newbie/__init__.py
+++ newbie/__init__.py
@@ -14,7 +14,12 @@ async def new():
         print(str(d))
         y = await main(d)
         space = '\n'
-        f = re.findall(r'.*\n.*\n.*\n.*\n.*',space.join(y))
-        return('这是当前的新人列表：\n'+f[0]+'\n...仅显示前5条内容')
+        j = space.join(y)
+        f = re.findall(r'.*\n.*\n.*\n.*\n.*',j)
+        g = '这是当前的新人列表：\n'+f[0]+'\n...仅显示前5条内容'
+        if g.find('<吃掉了>') != -1:
+            return(g+'\n检测到外来信息介入，请前往日志查看所有消息。Special:日志?type=newusers')
+        else:
+            return(g)
     except Exception as e:
         return('发生错误：'+str(e))
