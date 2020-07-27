43a9da45d1db5238d19ce9a587e7a37a4dfc960b
yzhh <OasisAkari@users.noreply.github.com>
2020-07-27 20:51:44 +0800

    unused

--- command.py
+++ command.py
@@ -5,46 +5,35 @@ from wikil import im
 async def command(str1,member,group = '0'):
     str1 = re.sub(r'^～','~',str1)
     try:
-        q = re.match(r'^(发现新人：).*',str1)
-        print(q.group(1))
-        if str(group) == '657876815' or str(group) == '676942198':
-            if str(member) == '2736881328':
-                return  ('xrrrlei '+q.group(1))
-            else:
-                return ('paa')
-        else:
-            pass
+        q = re.match(r'^.*(: ~)(.*)',str1)
+        return q.group(2)
     except Exception:
         try:
-            q = re.match(r'^.*(: ~)(.*)',str1)
-            return q.group(2)
-        except Exception:
-            try:
-                q = re.match(r'^~(.*)',str1)
-                if q.group(1).find('爬') != -1:
-                    if member in blacklist():
-                        return ('paa')
-                    else:
-                        return q.group(1)
+            q = re.match(r'^~(.*)',str1)
+            if q.group(1).find('爬') != -1:
+                if member in blacklist():
+                    return ('paa')
                 else:
                     return q.group(1)
+            else:
+                return q.group(1)
+        except Exception:
+            try:
+                q = re.match(r'^!(.*\-.*)',str1)
+                q = str.upper(q.group(1))
+                return ('bug '+q)
             except Exception:
                 try:
-                    q = re.match(r'^!(.*\-.*)',str1)
-                    q = str.upper(q.group(1))
-                    return ('bug '+q)
-                except Exception:
-                    try:
-                        w = re.findall(r'\[\[(.*?)\]\]',str1)
-                        print(str(w))
-                        if str(w) == '[]'or str(w) == "['']":
-                            pass
-                        else:
-                            z = []
-                            c = '\n'
-                            for x in w:
-                                z.append(await im(x))
-                            v = c.join(z)
-                            return('echo '+v)
-                    except Exception:
+                    w = re.findall(r'\[\[(.*?)\]\]',str1)
+                    print(str(w))
+                    if str(w) == '[]'or str(w) == "['']":
                         pass
+                    else:
+                        z = []
+                        c = '\n'
+                        for x in w:
+                            z.append(await im(x))
+                        v = c.join(z)
+                        return('echo '+v)
+                except Exception:
+                    pass
