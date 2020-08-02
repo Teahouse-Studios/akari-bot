523199aeb180a1fffb1bde47665e245768eb4ac5
yzhh <OasisAkari@users.noreply.github.com>
2020-08-02 20:24:12 +0800

    Revert "Update __init__.py"
    This reverts commit 2e78e7893452a10774c477f2b96cbaafa09e32cb.

--- rc/__init__.py
+++ rc/__init__.py
@@ -7,9 +7,10 @@ async def rc():
     url = 'https://minecraft-zh.gamepedia.com/api.php?action=query&list=recentchanges&rcprop=title|user|timestamp&rctype=edit|new&format=json'
     text1 = requests.get(url,timeout=10)
     file = json.loads(text1.text)
+    d = []
     for x in file['query']['recentchanges']:
-        print(x)
-    y = await main(x)
+        d.append(x['title']+' - '+x['user']+' '+x['timestamp'])
+    y = await main(d)
     print(y)
     space = '\n'
     f = re.findall(r'.*\n.*\n.*\n.*\n.*',space.join(y))
