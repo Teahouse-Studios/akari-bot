903a95feaf61bc785d24cc4a373ff58cc2c5d745
yzhh <OasisAkari@users.noreply.github.com>
2020-08-04 18:24:03 +0800

    Update __init__.py

--- wikil/__init__.py
+++ wikil/__init__.py
@@ -1,39 +1,29 @@
 import re
 import traceback
 from .wikitextlib import wi
+from interwikilist import iwlist,iwlink
 async def im(str1):
     try:
         pipe = re.match(r'(.*?)\|.*',str1)
         str1 = pipe.group(1)
     except Exception:
         str1 = str1
+    str1 = re.sub(r'^:','',str1)
     try:
         d = re.match(r'(.*?):(.*)',str1)
         w = d.group(1)
         w = str.lower(w)
-        if (w == "cs" or w == "de" or w == "el" or w == "es" or w == "fr" or w == "hu" or w == "it" or w == "ja" or w == "ko" or w == "nl" or w == "pl" or w == "pt" or w == "ru" or w == "th" or w == "tr" or w == "uk" or w == "zh"):
-            c = 'minecraft-'+w+'.gamepedia.com'
-            pagename = d.group(2)
-            itw = 't'
-        elif w == "en":
-            c = 'minecraft.gamepedia.com'
-            pagename = d.group(2)
-            itw = 't'
-        elif w == 'arc' or w == 'arcaea':
-            c = 'wiki.arcaea.cn'
-            pagename = d.group(2)
-            itw = 't'
-        elif w == 'moe' or w == 'moegirl':
-            c = 'zh.moegirl.org.cn'
+        if w in iwlist():
+            c = iwlink(w)
             pagename = d.group(2)
             itw = 't'
         else:
-            c = 'minecraft-zh.gamepedia.com'
+            c = iwlink('zh')
             pagename = str1
             itw = 'f'
     except Exception:
-        c = 'minecraft-zh.gamepedia.com'
-        pagename = str1
+        c = iwlink('zh')
+        pagename = 'Template:'+str1
         itw = 'f'
         w = '.'
     return(await wi(c,w,pagename,itw,ignoremessage='f'))
@@ -44,6 +34,7 @@ async def imarc(str1):
         str1 = pipe.group(1)
     except Exception:
         str1 = str1
+    str1 = re.sub(r'^:','',str1)
     c = 'wiki.arcaea.cn'
     itw = 'f'
     w = '.'
@@ -55,33 +46,22 @@ async def imt(str1):
         str1 = pipe.group(1)
     except Exception:
         str1 = str1
+    str1 = re.sub(r'^:','',str1)
     try:
         d = re.match(r'(.*?):(.*)',str1)
         w = d.group(1)
         w = str.lower(w)
-        if (w == "cs" or w == "de" or w == "el" or w == "es" or w == "fr" or w == "hu" or w == "it" or w == "ja" or w == "ko" or w == "nl" or w == "pl" or w == "pt" or w == "ru" or w == "th" or w == "tr" or w == "uk" or w == "zh"):
-            c = 'minecraft-'+w+'.gamepedia.com'
-            pagename = 'Template:'+d.group(2)
-            itw = 't'
-        elif w == "en":
-            c = 'minecraft.gamepedia.com'
-            pagename = 'Template:'+d.group(2)
-            itw = 't'
-        elif w == 'arc' or w == 'arcaea':
-            c = 'wiki.arcaea.cn'
-            pagename = 'Template:'+d.group(2)
-            itw = 't'
-        elif w == 'moe' or w == 'moegirl':
-            c = 'zh.moegirl.org.cn'
+        if w in iwlist():
+            c = iwlink(w)
             pagename = 'Template:'+d.group(2)
             itw = 't'
         else:
-            c = 'minecraft-zh.gamepedia.com'
+            c = iwlink('zh')
             pagename = 'Template:'+str1
             itw = 'f'
     except Exception:
-        c = 'minecraft-zh.gamepedia.com'
+        c = iwlink('zh')
         pagename = 'Template:'+str1
         itw = 'f'
         w = '.'
-    return(await wi(c,w,pagename,itw,ignoremessage='f',template='t'))
+    return(await wi(c,w,pagename,itw,ignoremessage='f'))
