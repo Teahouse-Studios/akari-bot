5a467e2ebf5dac4cc58cadbc0a848c6e6b185c91
yzhh <OasisAkari@users.noreply.github.com>
2020-08-04 17:42:07 +0800

    Update __init__.py

--- interwikilist/__init__.py
+++ interwikilist/__init__.py
@@ -3,13 +3,12 @@ import sys
 from os.path import abspath
 def iwlist():
     cp = ConfigParser()
-    print(abspath("."))
-    cp.read(abspath("list.cfg"))
+    cp.read(abspath("./interwikilist/list.cfg"))
     section = cp.sections()[0]
     return(cp.options(section))
 
 def iwlink(iw):
     cp = ConfigParser()
-    cp.read(abspath("list.cfg"))
+    cp.read(abspath("./interwikilist/list.cfg"))
     section = cp.sections()[0]
     return(cp.get(section,iw))
