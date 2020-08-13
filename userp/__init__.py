9ca1f7c38af8e3c4eee9a4fb401b4c54de04b8ab
yzhh <OasisAkari@users.noreply.github.com>
2020-08-13 18:34:10 +0800

    ...maybe

--- userp/__init__.py
+++ userp/__init__.py
@@ -1,20 +1,23 @@
-from wiki import im
-from .puserlib import PUser1, PUser1ban, PUser1bann
 import aiohttp
 import json
 import re
-from .gender import gender
-from UTC8 import UTC8
-import urllib
 import traceback
-async def Userp(path,Username):
+import urllib
+
+from UTC8 import UTC8
+from wiki import im
+from .gender import gender
+from .puserlib import PUser1
+
+
+async def Userp(path, Username):
     try:
         q = re.sub('User:', '', Username)
         q = re.sub('_', ' ', q)
         metaurl = 'https://' + path + '.gamepedia.com'
         url1 = metaurl + '/api.php?action=query&list=users&ususers=' + q + '&usprop=groups%7Cblockinfo%7Cregistration%7Ceditcount%7Cgender&format=json'
         async with aiohttp.ClientSession() as session:
-            async with session.get(url1,timeout=aiohttp.ClientTimeout(total=20)) as req:
+            async with session.get(url1, timeout=aiohttp.ClientTimeout(total=20)) as req:
                 if req.status != 200:
                     print(f"请求发生时错误:{req.status}")
                 else:
@@ -23,28 +26,28 @@ async def Userp(path,Username):
         try:
             User = file['query']['users'][0]['name']
             Gender = gender(file['query']['users'][0]['gender'])
-            Registration = UTC8(file['query']['users'][0]['registration'],'notimezone')
+            Registration = UTC8(file['query']['users'][0]['registration'], 'notimezone')
             Blockedby = str(file['query']['users'][0]['blockedby'])
-            Blockedtimestamp = UTC8(file['query']['users'][0]['blockedtimestamp'],'notimezone')
-            Blockexpiry = UTC8(str(file['query']['users'][0]['blockexpiry']),'full')
+            Blockedtimestamp = UTC8(file['query']['users'][0]['blockedtimestamp'], 'notimezone')
+            Blockexpiry = UTC8(str(file['query']['users'][0]['blockexpiry']), 'full')
             Blockreason = str(file['query']['users'][0]['blockreason'])
             if not Blockreason:
-                await PUser1bann(metaurl, q, path, User, Gender, Registration, Blockedby, Blockedtimestamp, Blockexpiry)
+                await PUser1(metaurl, q, path, User, Gender, Registration, Blockedby, Blockedtimestamp, Blockexpiry)
             else:
-                await PUser1ban(metaurl, q, path, User, Gender, Registration, Blockedby, Blockedtimestamp, Blockexpiry,\
-                        Blockreason)
-            h = '/Userprofile:' +User
-            return(metaurl+urllib.parse.quote(h.encode('UTF-8')))
+                await PUser1(metaurl, q, path, User, Gender, Registration, Blockedby, Blockedtimestamp, Blockexpiry, \
+                                Blockreason)
+            h = '/Userprofile:' + User
+            return (metaurl + urllib.parse.quote(h.encode('UTF-8')))
         except Exception:
             try:
                 User = file['query']['users'][0]['name']
                 Gender = gender(file['query']['users'][0]['gender'])
-                Registration = UTC8(file['query']['users'][0]['registration'],'notimezone')
+                Registration = UTC8(file['query']['users'][0]['registration'], 'notimezone')
                 await PUser1(metaurl, q, path, User, Gender, Registration)
-                h = '/Userprofile:' +User
-                return(metaurl+urllib.parse.quote(h.encode('UTF-8')))
+                h = '/Userprofile:' + User
+                return (metaurl + urllib.parse.quote(h.encode('UTF-8')))
             except Exception:
                 traceback.print_exc()
                 return ('N')
     except Exception as e:
-        return ('发生错误：'+str(e))
+        return ('发生错误：' + str(e))
