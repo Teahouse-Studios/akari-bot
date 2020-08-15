408256e6b81d5d15ac868e6e20a02f0765ddca2d
yzhh <OasisAkari@users.noreply.github.com>
2020-08-15 15:33:04 +0800

    Update __init__.py

--- rc/__init__.py
+++ rc/__init__.py
@@ -17,13 +17,13 @@ async def rc():
                 text1 = await req.text()
     file = json.loads(text1)
     d = []
-    for x in file['query']['recentchanges']:
+    for x in file['query']['recentchanges'][5:]:
         d.append(x['title'] + ' - ' + x['user'] + ' ' + UTC8(x['timestamp'], 'onlytime'))
     y = await pbc2(d)
     print(y)
     space = '\n'
-    f = re.findall(r'.*\n.*\n.*\n.*\n.*', space.join(y))
-    if f[0].find('<吃掉了>') != -1 or f[0].find('<全部吃掉了>') != -1:
+    f = space.join(y)
+    if f.find('<吃掉了>') != -1 or f.find('<全部吃掉了>') != -1:
         return (f[0] + '\n...仅显示前5条内容\n检测到外来信息介入，请前往最近更改查看所有消息。Special:最近更改')
     else:
         return (f[0] + '\n...仅显示前5条内容')
