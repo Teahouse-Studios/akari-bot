a90bf84acb0b23121f4b267d3151a052d347afe9
yzhh <OasisAkari@users.noreply.github.com>
2020-08-20 22:36:47 +0800

    Update __init__.py

--- modules/mcv/__init__.py
+++ modules/mcv/__init__.py
@@ -14,9 +14,24 @@ async def get_data(url: str, fmt: str):
 async def mcv():
     try:
         data = await get_data('http://launchermeta.mojang.com/mc/game/version_manifest.json', "json")
+        message1 = f"最新版：{data['latest']['release']}，最新快照：{data['latest']['snapshot']}"
     except (ConnectionError, OSError):  # Probably...
-        return "发生错误：土豆熟了"
-    return f"最新版：{data['latest']['release']}，最新快照：{data['latest']['snapshot']}"
+        message1 = "获取manifest.json失败。"
+    try:
+        mojira = await get_data('https://bugs.mojang.com/rest/api/2/project/10400/versions','json')
+        release = []
+        prefix = ' | '
+        for v in mojira:
+            if not v['archived']:
+                release.append(v['name'])
+        message2 = prefix.join(release)
+    except:
+        message2 = "获取Mojira信息失败。"
+    return f"""目前启动器内最新版本为：
+{message1}，
+Mojira上所记录最新版本为：
+{message2}
+（以启动器内最新版本为准，Mojira仅作版本号预览用）"""
 
 
 async def mcdv():
