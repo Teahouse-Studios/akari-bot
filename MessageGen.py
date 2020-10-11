b00482f46b5b691fdfd981f4551df25bc844a238
yzhh <OasisAkari@users.noreply.github.com>
2020-10-11 15:49:09 +0800

    Update

--- MessageGen.py
+++ MessageGen.py
@@ -2,16 +2,14 @@ import asyncio
 import os
 import random
 import re
+import traceback
 from os.path import abspath
 
 import graia.application.interrupt as inter
 from graia.application.message.chain import MessageChain
-from graia.application.message.elements.internal import Plain, Image, Source, At
-from modules.camr import camr
-from modules.dfile import dfile
-from modules.findimage import findimage
+from graia.application.message.elements.internal import Plain, Image, Source
 
-import traceback
+from modules.findimage import findimage
 
 try:
     cachepath = abspath('./assets/cache/')
@@ -21,98 +19,103 @@ try:
 except Exception:
     pass
 
+
 async def gen(bcc, app, message, target1, target2='0', msgtype='None', runfun='command'):
     im = inter.InterruptControl(bcc)
-    command = __import__('CommandGen',fromlist=[runfun])
+    command = __import__('CommandGen', fromlist=[runfun])
     command = getattr(command, runfun)
     if msgtype == 'Group':
         run = await command(message.asDisplay(), target1.id)
     else:
         run = await command(message.asDisplay())
-    #    print(run)
+    # print(run)
     if run != None:
-        print(run)
-        msgchain = await makemsgchain(run, msgtype)
-        send = await sendmessage(app, msgchain, target1, target2, msgtype,
-                                 message[Source][0] if msgtype == 'Group' else 0)
-        """
-        if msgtype == 'Group':
-            voice = re.findall(r'https?://.*?/File:.*?\.(?:ogg|m4a|mp3|flac|wav)', run, re.I)
-            for voicelink in voice:
-                try:
-                    findvoicename = re.match(r'(https?://.*?/)File:(.*?\.(?:ogg|m4a|mp3|flac|wav))', voicelink, re.I)
-                    downloadfile = await dfile(findvoicename.group(1), findvoicename.group(2))
-                    print(downloadfile)
-                    conventamr = await camr(downloadfile)
-                    print(conventamr)
-                    readfile = open(conventamr, 'rb+')
-                    uploadvoice = await app.uploadVoice(readfile.read())
-                    voicemsgchain = MessageChain.create([uploadvoice])
-                    await app.sendGroupMessage(target1, voicemsgchain)
-                    readfile.close()
-                    os.remove(downloadfile)
-                    os.remove(conventamr)
-                except Exception:
-                    traceback.print_exc()
-        """
-        if run.find('[一分钟后撤回本消息]') != -1:
-            await asyncio.sleep(60)
-            await app.revokeMessage(send)
-        if run.find('[30秒后撤回本消息]') != -1:
-            await asyncio.sleep(30)
-            await app.revokeMessage(send)
-        if run.find('[wait]') != -1:
-            ranint = random.randint(1, 3)
-            if ranint == 2:
-                waitmsg = await makemsgchain('提示：你可以发送“是”字来将所有无效结果再次查询。（考虑到实现复杂性，恕不提供选择性查询）', msgtype)
-                await sendmessage(app, waitmsg, target1, target2, msgtype)
-            MessageEventImport = __import__('graia.application', fromlist=[f'{msgtype}Message'])
-            MessageEvent = getattr(MessageEventImport, f'{msgtype}Message')
-            InterruptImport = __import__('graia.application.interrupt.interrupts',
-                                         fromlist=[f'{msgtype}MessageInterrupt'])
-            Interrupt = getattr(InterruptImport, f'{msgtype}MessageInterrupt')
-            if msgtype == 'Friend':
-                event: MessageEvent = await im.wait(Interrupt(target1.id))
-            else:
-                event: MessageEvent = await im.wait(Interrupt(target1, target2))
-            print(event)
-            if event.messageChain.asDisplay() == '是':
-                msg2 = await command(run)
-                msgchain = await makemsgchain(msg2, msgtype)
-                await sendmessage(app, msgchain, target1, target2, msgtype)
-            else:
-                pass
+        await msgproc(run, app, im, command, message, target1, target2, msgtype)
 
 
-async def makemsgchain(msg, msgtype):
+async def msgproc(resultmsgraw, app, im, command, message, target1, target2='0', msgtype='None'):
+    print(resultmsgraw)
+    uimg = re.match(r'.*?\[\[uimgc:.*?\]\].*?', resultmsgraw)
+    msgchain = await makemsgchain(resultmsgraw)
+    send = await sendmessage(app, msgchain, target1, target2, msgtype,
+                             message[Source][0] if msgtype == 'Group' else 0)
+    if uimg:
+        await uimgsend(app, message, target1, target2, msgtype, resultmsgraw)
+    r = re.findall(r'(https?://.*?/File:.*?\.(?:png|gif|jpg|jpeg|webp|bmp|ico))', resultmsgraw, re.I)
+    for d in r:
+        d1 = await findimage(d)
+        if d1 is not None:
+            await linkimgsend(app, d1, target1, target2, msgtype)
+    await afterproc(resultmsgraw, app, im, command, send, message, target1, target2, msgtype)
+
+
+async def makemsgchain(msg):
     msg = re.sub('\[wait\]', '', msg)
-    exec('from graia.application.message.elements.internal import UploadMethods')
-    mth = eval(f'UploadMethods.{msgtype}')
-    fuimg = re.findall(r'\[\[uimgc:.*\]\]', msg)
     msgbase = re.sub(r'\[\[uimgc:.*\]\]', '', msg)
     msgchain = MessageChain.create([Plain(msgbase)])
+    return msgchain
+
+
+async def afterproc(resultmsgraw, app, im, command, send, message, target1, target2='0', msgtype='None'):
+    if resultmsgraw.find('[一分钟后撤回本消息]') != -1:
+        await asyncio.sleep(60)
+        await app.revokeMessage(send)
+    if resultmsgraw.find('[30秒后撤回本消息]') != -1:
+        await asyncio.sleep(30)
+        await app.revokeMessage(send)
+    if resultmsgraw.find('[wait]') != -1:
+        ranint = random.randint(1, 3)
+        if ranint == 2:
+            waitmsg = await makemsgchain('提示：你可以发送“是”字来将所有无效结果再次查询。（考虑到实现复杂性，恕不提供选择性查询）')
+            await sendmessage(app, waitmsg, target1, target2, msgtype)
+        MessageEventImport = __import__('graia.application', fromlist=[f'{msgtype}Message'])
+        MessageEvent = getattr(MessageEventImport, f'{msgtype}Message')
+        InterruptImport = __import__('graia.application.interrupt.interrupts',
+                                     fromlist=[f'{msgtype}MessageInterrupt'])
+        Interrupt = getattr(InterruptImport, f'{msgtype}MessageInterrupt')
+        if msgtype == 'Friend':
+            event: MessageEvent = await im.wait(Interrupt(target1.id))
+        else:
+            event: MessageEvent = await im.wait(Interrupt(target1, target2))
+        print(event)
+        if event.messageChain.asDisplay() == '是':
+            msg2 = await command(resultmsgraw)
+            await msgproc(msg2, app, im, command, message, target1, target2)
+        else:
+            pass
+
+
+async def uimgsend(app, message, target1, target2, msgtype, runmsg):
+    exec('from graia.application.message.elements.internal import UploadMethods')
+    mth = eval(f'UploadMethods.{msgtype}')
+    fuimg = re.findall(r'\[\[uimgc:.*\]\]', runmsg)
     for imgc in fuimg:
-        img = re.match(r'\[\[uimgc:(.*)\]\]',imgc)
+        img = re.match(r'\[\[uimgc:(.*)\]\]', imgc)
         if img:
             try:
-                msgchain = msgchain.plusWith(
+                msgchain = MessageChain.create(
                     [Image.fromLocalFile(filepath=abspath(img.group(1)), method=mth)])
-            except asyncio.exceptions.TimeoutError:
-                msgchain = msgchain.plusWith(
-                    [Plain('\n上传超时，发送失败。')])
+                await sendmessage(app, msgchain, target1, target2, msgtype,
+                                  message[Source][0] if msgtype == 'Group' else 0)
+            except (asyncio.exceptions.TimeoutError, asyncio.TimeoutError):
+                msgchain = MessageChain.create(
+                    [Plain('上传超时，发送失败。')])
+                await sendmessage(app, msgchain, target1, target2, msgtype,
+                                  message[Source][0] if msgtype == 'Group' else 0)
             except Exception:
                 traceback.print_exc()
-    r = re.findall(r'(https?://.*?/File:.*?\.(?:png|gif|jpg|jpeg|webp|bmp|ico))', msg, re.I)
-    for d in r:
-        try:
-            d1 = await findimage(d)
-            msgchain = msgchain.plusWith([Image.fromNetworkAddress(url=d1, method=mth)])
-        except asyncio.exceptions.TimeoutError:
-            msgchain = msgchain.plusWith(
-                [Plain('\n上传超时，发送失败。')])
-        except Exception:
-            traceback.print_exc()
-    return msgchain
+
+
+async def linkimgsend(app, sendlink, target1, target2,msgtype):
+    exec('from graia.application.message.elements.internal import UploadMethods')
+    mth = eval(f'UploadMethods.{msgtype}')
+    try:
+        msgchain = MessageChain.create([Image.fromNetworkAddress(url=sendlink, method=mth)])
+        await sendmessage(app, msgchain, target1, target2, msgtype)
+    except (asyncio.exceptions.TimeoutError, asyncio.TimeoutError):
+        msgchain = MessageChain.create(
+            [Plain('上传超时，发送失败。')])
+        await sendmessage(app, msgchain, target1, target2, msgtype)
 
 
 async def sendmessage(app, msgchain, target1, target2, msgtype, quoteid=0):
@@ -127,3 +130,25 @@ async def sendmessage(app, msgchain, target1, target2, msgtype, quoteid=0):
         member = target2
         send = await app.sendTempMessage(group=group, target=member, message=msgchain.asSendable())
     return send
+
+
+"""
+if msgtype == 'Group':
+    voice = re.findall(r'https?://.*?/File:.*?\.(?:ogg|m4a|mp3|flac|wav)', run, re.I)
+    for voicelink in voice:
+        try:
+            findvoicename = re.match(r'(https?://.*?/)File:(.*?\.(?:ogg|m4a|mp3|flac|wav))', voicelink, re.I)
+            downloadfile = await dfile(findvoicename.group(1), findvoicename.group(2))
+            print(downloadfile)
+            conventamr = await camr(downloadfile)
+            print(conventamr)
+            readfile = open(conventamr, 'rb+')
+            uploadvoice = await app.uploadVoice(readfile.read())
+            voicemsgchain = MessageChain.create([uploadvoice])
+            await app.sendGroupMessage(target1, voicemsgchain)
+            readfile.close()
+            os.remove(downloadfile)
+            os.remove(conventamr)
+        except Exception:
+            traceback.print_exc()
+"""
--- modules/findimage.py
+++ modules/findimage.py
@@ -1,5 +1,7 @@
 import re
 
+import traceback
+
 import aiohttp
 from bs4 import BeautifulSoup as bs
 
@@ -8,12 +10,16 @@ async def findimage(url):
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as req:
             if req.status != 200:
-                return f"请求时发生错误：{req.status}"
+                return None
             else:
                 q = await req.text()
-    soup = bs(q, 'html.parser')
-    aa = soup.find('div', id='mw-content-text')
-    src = aa.find_all('div', class_='fullImageLink')
-    z = re.match('.*<a href="(.*)"><.*', str(src), re.S)
-    find = re.sub(r'\?.*', '', z.group(1))
-    return find
+    try:
+        soup = bs(q, 'html.parser')
+        aa = soup.find('div', id='mw-content-text')
+        src = aa.find_all('div', class_='fullImageLink')
+        z = re.match('.*<a href="(.*)"><.*', str(src), re.S)
+        find = re.sub(r'\?.*', '', z.group(1))
+        return find
+    except Exception:
+        traceback.print_exc()
+        return None
