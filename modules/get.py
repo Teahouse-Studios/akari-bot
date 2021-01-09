b4dfd3dcb7065b3051efa9a3d407dec1dd01deec
yzhh <OasisAkari@users.noreply.github.com>
2021-01-09 19:58:10 +0800

    Delete get.py

--- modules/get.py
+++
@@ -1,8 +0,0 @@
-import aiohttp
-
-async def main(url):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, timeout=aiohttp.ClientTimeout(total=20)) as req:
-            return await req.text()
-
-command = {'get': 'get'}
