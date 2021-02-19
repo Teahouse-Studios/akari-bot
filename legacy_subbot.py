5236d67edc5bff6ed88c9105074d4aac8e51bf27
wyapx <a1025952421@outlook.com>
2021-02-19 19:26:55 +0800

    Update legacy_subbot.py

--- legacy_subbot.py
+++ legacy_subbot.py
@@ -27,7 +27,7 @@ async def newbie(app):
                         if xz['title'] in qq:
                             pass
                         else:
-                            s = await check([UTC8(xz['timestamp'], 'onlytime') + '新增新人：' + xz['title']])
+                            s = await check(UTC8(xz['timestamp'], 'onlytime') + '新增新人：' + xz['title'])
                             if s.find("<吃掉了>") != -1 or s.find("<全部吃掉了>") != -1:
                                 await app.sendGroupMessage(731397727, MessageChain.create(
                                     [Plain(s + '\n检测到外来信息介入，请前往日志查看所有消息。Special:日志?type=newusers')]).asSendable())
