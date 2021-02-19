6447f56836e3f5c39b34b799d9172d53a8ef4888
yzhh <OasisAkari@users.noreply.github.com>
2021-02-19 20:05:58 +0800

    Update __init__.py

--- modules/github/__init__.py
+++ modules/github/__init__.py
@@ -50,33 +50,37 @@ async def query(url: str, fmt: str):
             return False
 
 async def repo(kwargs: dict, cmd: list):
-    obj = cmd[1].replace('@', '')
-    result = await query('https://api.github.com/repos/' + obj, 'json')
-    name = result['full_name']
-    url = result['html_url']
-    rid = result['id']
-    desc = result['description']
-    lang = result['language']
-    fork = result['forks_count']
-    star = result['stargazers_count']
-    watch = result['watchers_count']
-    mirror = result['mirror_url']
-    rlicense = result['license']['spdx_id']
-    is_fork = result['fork']
-    created = result['created_at']
-    updated = result['updated_at']
-    parent = False
+    try:
+        obj = cmd[1].replace('@', '')
+        result = await query('https://api.github.com/repos/' + obj, 'json')
+        name = result['full_name']
+        url = result['html_url']
+        rid = result['id']
+        desc = result['description']
+        lang = result['language']
+        fork = result['forks_count']
+        star = result['stargazers_count']
+        watch = result['watchers_count']
+        mirror = result['mirror_url']
+        rlicense = 'Unknown'
+        if 'license' in result:
+            if 'spdx_id' in result['license']:
+                rlicense = result['license']['spdx_id']
+        is_fork = result['fork']
+        created = result['created_at']
+        updated = result['updated_at']
+        parent = False
 
-    if mirror is not None:
-        mirror = f' (This is a mirror of {mirror} )'
-    else:
-        mirror = False
+        if mirror is not None:
+            mirror = f' (This is a mirror of {mirror} )'
+        else:
+            mirror = False
 
-    if is_fork:
-        parent_name = result['parent']['name']
-        parent = f' (This is a mirror of {parent_name} )'
+        if is_fork:
+            parent_name = result['parent']['name']
+            parent = f' (This is a mirror of {parent_name} )'
 
-    msg = f'''{name} ({rid})
+        msg = f'''{name} ({rid})
 {desc}
 
 Language · {lang} | Fork · {fork} | Star · {star} | Watch · {watch}
@@ -84,89 +88,94 @@ License: {rlicense}
 Created {time_diff(created)} ago | Updated {time_diff(updated)} ago
 
 {url}
-'''
+    '''
 
-    if mirror:
-        msg += '\n' + mirror
+        if mirror:
+            msg += '\n' + mirror
 
-    if parent:
-        msg += '\n' + parent
+        if parent:
+            msg += '\n' + parent
 
-    is_dirty = await dirty_check(msg)
-    if is_dirty:
-        msg = 'https://wdf.ink/6OUp'
+        is_dirty = await dirty_check(msg)
+        if is_dirty:
+            msg = 'https://wdf.ink/6OUp'
 
-    await sendMessage(kwargs, MessageChain.create([Plain(msg)]))
+        await sendMessage(kwargs, MessageChain.create([Plain(msg)]))
+    except Exception as e:
+        await sendMessage(kwargs, '发生错误：' + str(e))
 
 async def user(kwargs: dict, cmd: list):
-    obj = cmd[1]
-    result = await query('https://api.github.com/users/' + obj, 'json')
-    login = result['login']
-    name = result['name']
-    uid = result['id']
-    url = result['html_url']
-    bio = result['bio']
-    utype = result['type']
-    if 'company' in result:
-        company = result['company']
-    else:
-        company = False
-    following = result['following']
-    follower = result['followers']
-    repo = result['public_repos']
-    gist = result['public_gists']
-    is_staff = False
-    if 'license' in result:
-        if 'spdx_id' in result['license']:
-            is_staff = result['license']['spdx_id']
-    if 'twitter_username' in result:
-        twitter = result['twitter_username']
-    else:
-        twitter = False
-    created = result['created_at']
-    updated = result['updated_at']
-    if 'blog' in result:
-        website = result['blog']
-    else:
-        website = False
-    if 'location' in result:
-        location = result['location']
-    else:
-        location = False
-    hireable = False
-    if 'hireable' in result:
-        hireable = result['hireable']
-    optional = []
-    if hireable:
-        optional.append('Hireable')
-    if is_staff:
-        optional.append('GitHub Staff')
-    if company:
-        optional.append('Work · ' + company)
-    if twitter:
-        optional.append('Twitter · ' + twitter)
-    if website:
-        optional.append('Site · ' + website)
-    if location:
-        optional.append('Location · ' + location)
-    if bio is None:
-        bio = ''
+    try:
+        obj = cmd[1]
+        result = await query('https://api.github.com/users/' + obj, 'json')
+        login = result['login']
+        name = result['name']
+        uid = result['id']
+        url = result['html_url']
+        bio = result['bio']
+        utype = result['type']
+        if 'company' in result:
+            company = result['company']
+        else:
+            company = False
+        following = result['following']
+        follower = result['followers']
+        repo = result['public_repos']
+        gist = result['public_gists']
+        is_staff = False
+        if 'license' in result:
+            if 'spdx_id' in result['license']:
+                is_staff = result['license']['spdx_id']
+        if 'twitter_username' in result:
+            twitter = result['twitter_username']
+        else:
+            twitter = False
+        created = result['created_at']
+        updated = result['updated_at']
+        if 'blog' in result:
+            website = result['blog']
+        else:
+            website = False
+        if 'location' in result:
+            location = result['location']
+        else:
+            location = False
+        hireable = False
+        if 'hireable' in result:
+            hireable = result['hireable']
+        optional = []
+        if hireable:
+            optional.append('Hireable')
+        if is_staff:
+            optional.append('GitHub Staff')
+        if company:
+            optional.append('Work · ' + company)
+        if twitter:
+            optional.append('Twitter · ' + twitter)
+        if website:
+            optional.append('Site · ' + website)
+        if location:
+            optional.append('Location · ' + location)
+        if bio is None:
+            bio = ''
 
-    optional_text = '\n' + ' | '.join(optional)
-    msg = f'''{login} aka {name} ({uid})
+        optional_text = '\n' + ' | '.join(optional)
+        msg = f'''{login} aka {name} ({uid})
 {bio}
-
+    
 Type · {utype} | Follower · {follower} | Following · {following} | Repo · {repo} | Gist · {gist}{optional_text}
 Account Created {time_diff(created)} ago | Latest activity {time_diff(updated)} ago
-
+    
 {url}
 '''
 
-    is_dirty = await dirty_check(msg)
-    if is_dirty:
-        msg = 'https://wdf.ink/6OUp'
+        is_dirty = await dirty_check(msg)
+        if is_dirty:
+            msg = 'https://wdf.ink/6OUp'
 
-    await sendMessage(kwargs, MessageChain.create([Plain(msg)]))
+        await sendMessage(kwargs, MessageChain.create([Plain(msg)]))
+    except Exception as e:
+        await sendMessage(kwargs, '发生错误：' + str(e))
 
 async def forker(kwargs: dict):
     cmd = kwargs['trigger_msg']
