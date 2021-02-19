27c33eaa6bc5f14222e022fab14c34764428a45a
wyapx <a1025952421@outlook.com>
2021-02-19 19:18:33 +0800

    Update dirty_check.py

--- core/dirty_check.py
+++ core/dirty_check.py
@@ -23,35 +23,30 @@ def computeMD5hash(my_string):
     return m.hexdigest()
 
 
-async def check(text: list):
+async def check(*text):
+    config_path = os.path.abspath('config/config.cfg')
     try:
-        config_path = os.path.abspath('config/config.cfg')
         accessKeyId = config(config_path, "Check_accessKeyId")
         accessKeySecret = config(config_path, "Check_accessKeySecret")
-    except Exception:
+    except FileNotFoundError:
         return '\n'.join(text)
-    print('hello')
     body = {
         "scenes": [
             "antispam"
         ],
         "tasks": list(map(lambda x: {
-            "dataId": "LittleC is god {}".format(time.time()),
+            "dataId": "OasisAkari is god {}".format(time.time()),
             "content": x
         }, text))
     }
-    print(urlencode({
-        'we': 123
-    }))
     clientInfo = '{}'
     root = 'https://green.cn-shanghai.aliyuncs.com'
     url = '/green/text/scan?{}'.format(clientInfo)
 
     GMT_FORMAT = '%a, %d %b %Y %H:%M:%S GMT'
     date = datetime.datetime.utcnow().strftime(GMT_FORMAT)
-    nonce = 'LittleC is god forever {}'.format(time.time())
+    nonce = 'OasisAkari is god forever {}'.format(time.time())
     contentMd5 = base64.b64encode(hashlib.md5(json.dumps(body).encode('utf-8')).digest()).decode('utf-8')
-    print(json.dumps(body))
     headers = {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
@@ -62,7 +57,6 @@ async def check(text: list):
         'x-acs-signature-version': '1.0',
         'x-acs-signature-method': 'HMAC-SHA1'
     }
-    print(headers)
     tmp = {
         'x-acs-version': '2018-05-09',
         'x-acs-signature-nonce': nonce,
@@ -75,16 +69,13 @@ async def check(text: list):
     step3 = "POST\napplication/json\n{contentMd5}\napplication/json\n{date}\n{step1}\n{step2}".format(
         contentMd5=contentMd5,
         date=headers['Date'], step1=step1, step2=step2)
-    print(step3)
     sign = "acs {}:{}".format(accessKeyId, hash_hmac(accessKeySecret, step3, hashlib.sha1))
     headers['Authorization'] = sign
-    print(headers)
     # 'Authorization': "acs {}:{}".format(accessKeyId, sign)
     async with aiohttp.ClientSession(headers=headers) as session:
         async with session.post('{}{}'.format(root, url), data=json.dumps(body)) as resp:
             if resp.status == 200:
                 result = await resp.json()
-                print(result)
                 resultUsers = []
                 for item in result['data']:
                     content = item['content']
@@ -100,4 +91,4 @@ async def check(text: list):
                 return ''.join(resultUsers)
 
             else:
-                return (await resp.text())
+                return await resp.text()
