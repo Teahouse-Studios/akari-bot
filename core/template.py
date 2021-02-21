89a8cce48d90c61729608ad95b732cdb56dc4d85
yzhh <OasisAkari@users.noreply.github.com>
2021-02-21 23:07:14 +0800

    Update template.py

--- core/template.py
+++ core/template.py
@@ -11,13 +11,13 @@ from database import check_superuser
 
 
 async def sendMessage(kwargs: dict, msgchain, Quote=True):
-    '''
+    """
     用于发送一条消息，兼容Group和Friend消息。
     :param kwargs: 函数传入的dict
     :param msgchain: 消息链，若传入str则自动创建一条带有Plain元素的消息链
     :param Quote: 是否引用传入dict中的消息（仅对Group消息有效）
     :return: 被发送的消息链
-    '''
+    """
     if isinstance(msgchain, str):
         msgchain = MessageChain.create([Plain(msgchain)])
     if 'TEST' not in kwargs:
@@ -66,11 +66,11 @@ async def sendMessage(kwargs: dict, msgchain, Quote=True):
 
 
 async def wait_confirm(kwargs: dict):
-    '''
+    """
     一次性模板，用于等待触发对象确认，兼容Group和Friend消息
     :param kwargs: 函数传入的dict
     :return: 若对象发送confirm_command中的其一文本时返回True，反之则返回False
-    '''
+    """
     inc = InterruptControl(bcc)
     confirm_command = ["是", "对", 'yes', 'y']
     if Group in kwargs:
@@ -101,11 +101,11 @@ async def wait_confirm(kwargs: dict):
 
 
 async def revokeMessage(msgchain):
-    '''
+    """
     用于撤回消息。
     :param msgchain: 需要撤回的已发送/接收的消息链
     :return: 无返回
-    '''
+    """
     if isinstance(msgchain, list):
         for msg in msgchain:
             await app.revokeMessage(msg)
@@ -114,11 +114,11 @@ async def revokeMessage(msgchain):
 
 
 def check_permission(kwargs):
-    '''
+    """
     检查对象是否拥有某项权限
     :param kwargs: 从函数传入的dict
     :return: 若对象为群主、管理员或机器人超管则为True
-    '''
+    """
     if Group in kwargs:
         if str(kwargs[Member].permission) in ['MemberPerm.Administrator', 'MemberPerm.Owner'] or check_superuser(
                 kwargs):
