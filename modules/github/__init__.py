9fc7f1f44f59bf441a5c994b36f037e72d493d55
Dianliang233 <dianliang233@gmail.com>
2021-03-06 09:52:36 +0800

    Update __init__.py

--- modules/github/__init__.py
+++ modules/github/__init__.py
@@ -201,8 +201,10 @@ async def forker(kwargs: dict):
         return await repo(kwargs, cmd)
     elif cmd[0] in ['user', 'usr', 'organization', 'org']:
         return await user(kwargs, cmd)
+    else:
+        return await sendMessage(kwargs, '发生错误：指令使用错误，请选择 repo 或 user 工作模式。使用 ~help github 查看详细帮助。')
 
 
 command = {'github': forker}
-help = {'github':{'module': '查询 Github 的指定 repo（仓库）或用户详情。', 'help': '''~github repo <user>/<name> - 获取GitHub仓库信息
-~github <user|usr|organization|org> - 获取GitHub用户或组织信息'''}}
+help = {'github':{'module': '查询 Github 的指定 repo（仓库）或用户详情。', 'help': '''~github repo <user>/<name> - 获取 GitHub 仓库信息。
+~github <user|usr|organization|org> - 获取 GitHub 用户或组织信息。'''}}
