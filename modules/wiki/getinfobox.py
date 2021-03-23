29dec034be906bc751204695350c40ac50ebfdd9
yzhh <2596322644@qq.com>
2021-03-23 23:00:45 +0800

    Update getinfobox.py

--- modules/wiki/getinfobox.py
+++ modules/wiki/getinfobox.py
@@ -55,6 +55,8 @@ async def get_infobox_pic(link, pagelink, headers):
         if find_infobox is None:  # 找
             find_infobox = soup.find(class_='infotemplatebox')  # 找
         if find_infobox is None:  # 找
+            find_infobox = soup.find(class_='skin-infobox')  # 找
+        if find_infobox is None:  # 找
             find_infobox = soup.find(class_='wikitable songtable')  # 找 (arcw)
         if find_infobox is None:  # 找
             return False  # 找你妈，不找了<-咱还是回家吧
