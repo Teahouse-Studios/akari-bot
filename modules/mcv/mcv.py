3ae933717123bfbcbe08e5ca7aa62cd611a28218
yzhh <2596322644@qq.com>
2021-03-31 23:44:44 +0800

    🤡

--- modules/mcv/mcv.py
+++ modules/mcv/mcv.py
@@ -15,9 +15,9 @@ async def get_data(url: str, fmt: str):
 async def mcv():
     try:
         data = await get_data('http://launchermeta.mojang.com/mc/game/version_manifest.json', "json")
-        message1 = f"最新版：{data['latest']['release']}，最新快照：{data['latest']['snapshot']}"
+        message1 = f"■■■■{data['latest']['release']}■■■■■■■{data['latest']['snapshot']}"
     except (ConnectionError, OSError):  # Probably...
-        message1 = "获取manifest.json失败。"
+        message1 = "■■■■■■■■■■■■■■■■■■"
     try:
         mojira = await get_data('https://bugs.mojang.com/rest/api/2/project/10400/versions', 'json')
         release = []
@@ -27,12 +27,12 @@ async def mcv():
                 release.append(v['name'])
         message2 = prefix.join(release)
     except Exception:
-        message2 = "获取Mojira信息失败。"
-    return f"""目前启动器内最新版本为：
-{message1}，
-Mojira上所记录最新版本为：
+        message2 = "■■■■■■■■■■■■■■■■■■"
+    return f"""■■■■■■■■■■■■■■■■■■
+{message1}
+■■■■■■■■■■■■■■■■■■■■■■■■■■■
 {message2}
-（以启动器内最新版本为准，Mojira仅作版本号预览用）"""
+■■■■■■■■■■■■■■■■■■■■■■■■■■■"""
 
 
 async def mcbv():
@@ -49,9 +49,9 @@ async def mcbv():
                 beta.append(match.group(1))
             else:
                 release.append(v["name"])
-    prefix = " | "
-    return f'Beta：{prefix.join(beta)}，Release：{prefix.join(release)}\n' \
-           f'（数据来源于MoJira，可能会比官方发布要早一段时间。信息仅供参考。）'
+    prefix = "■■■■■■■■■"
+    return f'■■■■■■■■■{prefix.join(beta)}■■■■■■■■■{prefix.join(release)}\n' \
+           f'■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■'
 
 
 async def mcdv():
@@ -61,5 +61,5 @@ async def mcdv():
         return "发生错误：土豆熟了"
     for v in data:
         if not v['archived']:
-            return f'最新版：{v.get("name")} \n（数据来源于MoJira，可能会比官方发布要早一段时间。信息仅供参考。）'
+            return f'■■■■■■■■■■■■■■■■■■{v.get("name")} \n■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■'
     return "出了点问题，快去锤develop（"
--- modules/wiki/getinfobox.py
+++ modules/wiki/getinfobox.py
@@ -4,6 +4,7 @@ import os
 import re
 import traceback
 import uuid
+import random
 from urllib.parse import urljoin
 
 import aiohttp
@@ -17,7 +18,16 @@ from config import Config
 infobox_render = Config().config('infobox_render')
 
 
-async def get_infobox_pic(link, pagelink, headers):
+async def get_infobox_pic(link, pagelink, headers, color=False, noise=False, text=False, hueall=False, noiseall=False):
+    if noise or noiseall:
+        peaceful = f'<svg width="0" height="0"><filter id="filter1"><feTurbulence type="fractalNoise" baseFrequency="0.1" numOctaves="{random.uniform(1, 5)}" /><feDisplacementMap in="SourceGraphic" scale="{random.uniform(2, 5)}" /></filter></svg>'
+        normal = f'<svg width="0" height="0"><filter id="filter2"><feTurbulence type="fractalNoise" baseFrequency="{random.uniform(0.1, 0.3)}" numOctaves="{random.uniform(10, 20)}" /><feDisplacementMap in="SourceGraphic" scale="{random.uniform(10, 25)}" /></filter></svg>'
+        hard = f'<svg width="0" height="0"><filter id="filter3"><feTurbulence type="fractalNoise" baseFrequency="{random.uniform(0.1, 0.5)}" numOctaves="{random.uniform(20, 40)}" /><feDisplacementMap in="SourceGraphic" scale="{random.uniform(20, 35)}" /></filter></svg>'
+        extra = f'<svg width="0" height="0"><filter id="filter4"><feTurbulence type="fractalNoise" baseFrequency="{random.uniform(0.2, 0.9)}" numOctaves="{random.uniform(40, 80)}" /><feDisplacementMap in="SourceGraphic" scale="{random.uniform(40, 55)}" /></filter></svg>'
+    if color or hueall:
+        color_hue = f'<svg width="0" height="0"><filter id="color1"><feColorMatrix in="SourceGraphic" type="hueRotate" values="180" /></filter></svg>'
+        color_alpha = f'<svg width="0" height="0"><filter id="color2"><feColorMatrix in="SourceGraphic" type="luminanceToAlpha" /></filter></svg>'
+        color_green = f'<svg width="0" height="0"><filter id="color3"><feColorMatrix in="SourceGraphic" type="matrix" values="0 0 0 0 0 1 1 1 1 0 0 0 0 0 0 0 0 0 1 0" /></filter></svg>'
     try:
         print('hello')
         wlink = re.sub(r'api.php', '', link)
@@ -35,24 +45,42 @@ async def get_infobox_pic(link, pagelink, headers):
         if os.path.exists(url):
             os.remove(url)
         print(222)
-        find_infobox = soup.find(class_='notaninfobox')  # 我
+        find_infobox = soup.find(class_=re.compile('notaninfobox'))  # 我
         if find_infobox is None:  # 找
-            find_infobox = soup.find(class_='portable-infobox')  # 找
+            find_infobox = soup.find(class_=re.compile('portable-infobox'))  # 找
         if find_infobox is None:  # 找
-            find_infobox = soup.find(class_='infobox')  # 找
+            find_infobox = soup.find(class_=re.compile('infobox'))  # 找
         if find_infobox is None:  # 找
-            find_infobox = soup.find(class_='tpl-infobox')  # 找
+            find_infobox = soup.find(class_=re.compile('tpl-infobox'))  # 找
         if find_infobox is None:  # 找
-            find_infobox = soup.find(class_='infoboxtable')  # 找
+            find_infobox = soup.find(class_=re.compile('infoboxtable'))  # 找
         if find_infobox is None:  # 找
-            find_infobox = soup.find(class_='infotemplatebox')  # 找
+            find_infobox = soup.find(class_=re.compile('infotemplatebox'))  # 找
         if find_infobox is None:  # 找
-            find_infobox = soup.find(class_='skin-infobox')  # 找
+            find_infobox = soup.find(class_=re.compile('skin-infobox'))  # 找
         if find_infobox is None:  # 找
-            find_infobox = soup.find(class_='wikitable songtable')  # 找 (arcw)
+            find_infobox = soup.find(class_=re.compile('wikitable songtable'))  # 找 (arcw)
         if find_infobox is None:  # 找
             return False  # 找你妈，不找了<-咱还是回家吧
         print('Find infobox..')
+
+        if noiseall:
+            r = str(random.randint(0, 4))
+            if find_infobox.has_attr('style'):
+                try:
+                    find_infobox.attrs['style'] = find_infobox.attrs['style'] + (';' if find_infobox.attrs['style'][-1] != ';' else '') + f'filter:url(#filter{r});'
+                except:
+                    find_infobox.attrs['style'] = find_infobox.attrs['style'] + f'filter:url(#filter{r});'
+            else:
+                find_infobox.attrs['style'] = f'filter:url(#filter{r});'
+        if hueall:
+            if find_infobox.has_attr('style'):
+                try:
+                    find_infobox.attrs['style'] = find_infobox.attrs['style'] + (';' if find_infobox.attrs['style'][-1] != ';' else '') + f'filter:url(#color1);'
+                except:
+                    find_infobox.attrs['style'] = find_infobox.attrs['style'] + f'filter:url(#color1);'
+            else:
+                find_infobox.attrs['style'] = f'filter:url(#color1);'
 
         if infobox_render is None:
             open_file = open(url, 'a', encoding='utf-8')
@@ -81,14 +109,196 @@ async def get_infobox_pic(link, pagelink, headers):
             return target
 
         for x in find_infobox.find_all(['a', 'img', 'span']):
+            r = str(random.randint(0, 4))
+            cr = str(random.randint(0, 3))
             if x.has_attr('href'):
                 x.attrs['href'] = join_url(link, x.get('href'))
-            if x.has_attr('src'):
+                if x.has_attr('style'):
+                    if r != '0' and noise:
+                        if x.attrs['style'].find('filter:url(#filter') == -1:
+                            try:
+                                x.attrs['style'] = x.attrs['style'] + (
+                                    ';' if x.attrs['style'][-1] != ';' else '') + f'filter:url(#filter{r});'
+                            except:
+                                x.attrs['style'] = x.attrs['style'] + f'filter:url(#filter{r});'
+                    if cr != '0' and color:
+                        if x.attrs['style'].find('filter:url(#color') == -1:
+                            try:
+                                x.attrs['style'] = x.attrs['style'] + (
+                                    ';' if x.attrs['style'][-1] != ';' else '') + f'filter:url(#color{r});'
+                            except:
+                                x.attrs['style'] = x.attrs['style'] + f'filter:url(#color{r});'
+                else:
+                    cont = False
+                    if r != '0' and noise:
+                        x.attrs['style'] = f'filter:url(#filter{r});'
+                        cont = True
+                    if cr != '0' and cont and color:
+                        try:
+                            x.attrs['style'] = x.attrs['style'] + (
+                                ';' if x.attrs['style'][-1] != ';' else '') + f'filter:url(#color{r});'
+                        except:
+                            x.attrs['style'] + f'filter:url(#color{r});'
+                    if cr != '0' and color:
+                        x.attrs['style'] = f'filter:url(#color{r});'
+            elif x.has_attr('src'):
                 x.attrs['src'] = join_url(link, x.get('src'))
-            if x.has_attr('srcset'):
+                if x.has_attr('style'):
+                    if r != '0' and noise:
+                        if x.attrs['style'].find('filter:url(#filter') == -1:
+                            try:
+                                x.attrs['style'] = x.attrs['style'] + (
+                                    ';' if x.attrs['style'][-1] != ';' else '') + f'filter:url(#filter{r});'
+                            except:
+                                x.attrs['style'] = x.attrs['style'] + f'filter:url(#filter{r});'
+                    if cr != '0' and color:
+                        if x.attrs['style'].find('filter:url(#color') == -1:
+                            try:
+                                x.attrs['style'] = x.attrs['style'] + (
+                                    ';' if x.attrs['style'][-1] != ';' else '') + f'filter:url(#color{r});'
+                            except:
+                                x.attrs['style'] = x.attrs['style'] + f'filter:url(#color{r});'
+                else:
+                    cont = False
+                    if r != '0' and noise:
+                        x.attrs['style'] = f'filter:url(#filter{r});'
+                        cont = True
+                    if cr != '0' and cont and color:
+                        try:
+                            x.attrs['style'] = x.attrs['style'] + (
+                                ';' if x.attrs['style'][-1] != ';' else '') + f'filter:url(#color{r});'
+                        except:
+                            x.attrs['style'] + f'filter:url(#color{r});'
+                    if cr != '0' and color:
+                        x.attrs['style'] = f'filter:url(#color{r});'
+            elif x.has_attr('srcset'):
                 x.attrs['srcset'] = join_url(link, x.get('srcset'))
-            if x.has_attr('style'):
+                if x.has_attr('style'):
+                    if r != '0' and noise:
+                        if x.attrs['style'].find('filter:url(#filter') == -1:
+                            try:
+                                x.attrs['style'] = x.attrs['style'] + (
+                                    ';' if x.attrs['style'][-1] != ';' else '') + f'filter:url(#filter{r});'
+                            except:
+                                x.attrs['style'] = x.attrs['style'] + f'filter:url(#filter{r});'
+                    if cr != '0' and color:
+                        if x.attrs['style'].find('filter:url(#color') == -1:
+                            try:
+                                x.attrs['style'] = x.attrs['style'] + (
+                                    ';' if x.attrs['style'][-1] != ';' else '') + f'filter:url(#color{r});'
+                            except:
+                                x.attrs['style'] = x.attrs['style'] + f'filter:url(#color{r});'
+                else:
+                    cont = False
+                    if r != '0' and noise:
+                        x.attrs['style'] = f'filter:url(#filter{r});'
+                        cont = True
+                    if cr != '0' and cont and color:
+                        try:
+                            x.attrs['style'] = x.attrs['style'] + (
+                                ';' if x.attrs['style'][-1] != ';' else '') + f'filter:url(#color{r});'
+                        except:
+                            x.attrs['style'] + f'filter:url(#color{r});'
+                    if cr != '0' and color:
+                        x.attrs['style'] = f'filter:url(#color{r});'
+            elif x.has_attr('style'):
                 x.attrs['style'] = re.sub(r'url\(/(.*)\)', 'url(' + link + '\\1)', x.get('style'))
+                if x.has_attr('style'):
+                    if r != '0' and noise:
+                        if x.attrs['style'].find('filter:url(#filter') == -1:
+                            try:
+                                x.attrs['style'] = x.attrs['style'] + (
+                                    ';' if x.attrs['style'][-1] != ';' else '') + f'filter:url(#filter{r});'
+                            except:
+                                x.attrs['style'] = x.attrs['style'] + f'filter:url(#filter{r});'
+                    if cr != '0' and color:
+                        if x.attrs['style'].find('filter:url(#color') == -1:
+                            try:
+                                x.attrs['style'] = x.attrs['style'] + (
+                                    ';' if x.attrs['style'][-1] != ';' else '') + f'filter:url(#color{r});'
+                            except:
+                                x.attrs['style'] = x.attrs['style'] + f'filter:url(#color{r});'
+                else:
+                    cont = False
+                    if r != '0' and noise:
+                        x.attrs['style'] = f'filter:url(#filter{r});'
+                        cont = True
+                    if cr != '0' and cont and color:
+                        try:
+                            x.attrs['style'] = x.attrs['style'] + (
+                                ';' if x.attrs['style'][-1] != ';' else '') + f'filter:url(#color{r});'
+                        except:
+                            x.attrs['style'] + f'filter:url(#color{r});'
+                    if cr != '0' and color:
+                        x.attrs['style'] = f'filter:url(#color{r});'
+
+        for x in find_infobox.find_all(text=re.compile(r'.*')):
+            r = str(random.randint(0, 4))
+            cr = str(random.randint(0, 3))
+            tr = str(random.randint(0, 2))
+
+            if x not in ['', '\n', None]:
+                if x.parent.has_attr('style') and x.parent.has_attr('style') not in ['', '\n', 'None']:
+                    if r != '0' and noise:
+                        if x.parent.attrs['style'].find('filter:url(#filter') == -1:
+                            try:
+                                x.parent.attrs['style'] = x.parent.attrs['style'] + (
+                                    ';' if x.parent.attrs['style'][-1] != ';' else '') + f'filter:url(#filter{r});'
+                            except:
+                                x.parent.attrs['style'] = x.parent.attrs['style'] + f'filter:url(#filter{r});'
+                    if cr != '0' and color:
+                        if x.parent.attrs['style'].find('filter:url(#color') == -1:
+                            try:
+                                x.parent.attrs['style'] = x.parent.attrs['style'] + (
+                                    ';' if x.parent.attrs['style'][-1] != ';' else '') + f'filter:url(#color{cr});'
+                            except:
+                                x.parent.attrs['style'] = x.parent.attrs['style'] + f'filter:url(#color{r});'
+                    if tr != '0' and text:
+                        trr = random.randint(0, 10)
+                        if trr == 2:
+                            x.replace_with(re.sub(r'.', 'eeeeeee', x))
+                        if trr == 1:
+                            x.replace_with(re.sub(r'.', '■■□■e■□■¿', x))
+                        if trr == 6:
+                            x.replace_with(re.sub(r'.', '↓□□↓□□□□¿', x))
+                        if trr == 5:
+                            x.replace_with(re.sub(r'.', '↓↓↑↓↓↓↑↓↑↓', x))
+                        if trr == 8:
+                            x.replace_with(re.sub(r'.', '¿¿¿¿¿¿¿¿¿', x))
+                        if trr == 7:
+                            x.replace_with(re.sub(r'.', '棍斤拷棍斤拷棍斤拷', x))
+                        if trr == 10:
+                            x.replace_with(re.sub(r'.', '烫烫烫烫烫烫烫烫烫', x))
+                else:
+                    cont = False
+                    if r != '0' and noise:
+                        x.parent.attrs['style'] = f'filter:url(#filter{r});'
+                        cont = True
+                    if cr != '0' and cont and color:
+                        try:
+                            x.parent.attrs['style'] = x.parent.attrs['style'] + (
+                                ';' if x.parent.attrs['style'][-1] != ';' else '') + f'filter:url(#color{cr});'
+                        except:
+                            x.parent.attrs['style'] = x.parent.attrs['style'] + f'filter:url(#color{r});'
+                    if cr != '0' and color:
+                        x.parent.attrs['style'] = f'filter:url(#color{r});'
+                    if tr != '0' and text:
+                        trr = random.randint(0, 10)
+                        if trr == 2:
+                            x.replace_with(re.sub(r'.', 'eeeeeee', x))
+                        if trr == 1:
+                            x.replace_with(re.sub(r'.', '■■■■', x))
+                        if trr == 3:
+                            x.replace_with(re.sub(r'.', '爬', x))
+                        if trr == 4:
+                            x.replace_with(re.sub(r'.', '□¿¿¿¿¿¿□¿¿¿', x))
+                        if trr == 6:
+                            x.replace_with(re.sub(r'.', '□□□□□□□□□', x))
+                        if trr == 7:
+                            x.replace_with(re.sub(r'.', '□■棍斤拷棍斤拷棍斤拷', x))
+                        if trr == 10:
+                            x.replace_with(re.sub(r'.', '■烫烫烫烫烫烫烫烫烫', x))
+
         replace_link = find_infobox
 
         if infobox_render is None:
@@ -96,10 +306,19 @@ async def get_infobox_pic(link, pagelink, headers):
             open_file.close()
         else:
             html_list.append(str(replace_link))
+            if noise or noiseall:
+                html_list.append(peaceful)
+                html_list.append(normal)
+                html_list.append(hard)
+                html_list.append(extra)
+            if color or hueall:
+                html_list.append(color_hue)
+                html_list.append(color_alpha)
+                html_list.append(color_green)
             html = '\n'.join(html_list)
-            print(html)
             html = {'content': html}
-        print(333)
+        print(html)
+        print('getting rendered page..')
 
         path2 = os.path.abspath('./assets/chromedriver.exe')
         picname = os.path.abspath(f'./cache/{pagename}.jpg')
