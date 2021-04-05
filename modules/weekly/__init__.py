26f5edd3a2b4f0e407ac62edc65795cb623fa8c1
yzhh <2596322644@qq.com>
2021-04-05 17:27:25 +0800

    ？

--- modules/weekly/__init__.py
+++ modules/weekly/__init__.py
@@ -1,34 +1,10 @@
 import re
 import json
 from core.utils import get_url
-import hashlib
-import os
-import aiohttp
 from graia.application import MessageChain
 from graia.application.message.elements.internal import Image, Plain
-import traceback
 from core.template import sendMessage
 
-async def download_image(link):
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(link, timeout=aiohttp.ClientTimeout(total=20)) as resp:
-                content = await resp.read()
-                md5 = hashlib.md5().update(content).hexdigest()
-                path = os.path.abspath('./cache/weekly/')
-                if not os.path.exists(path):
-                    os.makedirs(path)
-                path = path + f'/{md5}'
-                if not os.path.exists(path):
-                    os.mkdir(path)
-                if os.path.exists(path):
-                    os.remove(path)
-                with open(path, 'wb+') as image:
-                    image.write(content)
-                    return path
-    except:
-        traceback.print_exc()
-        return False
 
 async def weekly(kwargs: dict):
     try:
@@ -40,8 +16,7 @@ async def weekly(kwargs: dict):
         text = re.sub(r'\n*$', '', text)
         img = re.findall(r'(?<=src=")(.*?)(?=/revision/latest/scale-to-(width|height)-down/\d{3}\?cb=\d{14}?")', html)
         page = re.findall(r'(?<=<b><a href=").*?(?=")', html)
-        img_path = await download_image(img[0][0])
-        sended_img = Image.fromLocalFile(img_path) if img_path != False else Plain('\n（发生错误：图片获取失败）')
+        sended_img = Image.fromNetworkAddress(img[0][0]) if img else Plain('\n（发生错误：图片获取失败）')
         msg = '发生错误：本周页面已过期，请联系中文 Minecraft Wiki 更新。' if page[0] == '/zh/wiki/%E7%8E%BB%E7%92%83' else '本周的每周页面：\n\n' + text + '\n图片：' + img[0][0] + '?format=original\n\n页面链接：https://minecraft.fandom.com' + page[0] + '\n每周页面：https://minecraft.fandom.com/zh/wiki/?oldid=' + str(result['parse']['revid'])
         await sendMessage(kwargs, MessageChain.create([Plain(msg), sended_img]))
 
