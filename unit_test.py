d01514f5cc68d43149f97328f7b2ddc2652427d7
Dianliang233 <dianliang233@gmail.com>
2021-04-10 21:12:32 +0800

    test: now is a console

--- unit_test.py
+++ unit_test.py
@@ -1,8 +1,18 @@
 import asyncio
+import traceback
 
 from core.parser import parser
 
-kwargs = {}
-kwargs['TEST'] = True
-kwargs['command'] = '~weekly'
-asyncio.run(parser(kwargs))
+while True:
+    try:
+        kwargs = {}
+        kwargs['TEST'] = True
+        kwargs['command'] = input('> ')
+        if '~' not in kwargs['command']:
+            kwargs['command'] = '~' + kwargs['command']
+        asyncio.run(parser(kwargs))
+    except KeyboardInterrupt:
+        print('已退出。')
+        break
+    except Exception:
+        traceback.print_exc()
