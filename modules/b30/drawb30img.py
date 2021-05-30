57e8dcb080aff659ccacab021c6e04adf7d70211
yzhh <2596322644@qq.com>
2021-05-30 13:57:32 +0800

    Update drawb30img.py

--- modules/b30/drawb30img.py
+++ modules/b30/drawb30img.py
@@ -47,7 +47,7 @@ def drawb30(Username, b30, r10, ptt, character, path=''):
     # backgroud
     bgimgpath = os.path.abspath('./assets/arcaea/world/')
     bglist = os.listdir(bgimgpath)
-    bgr = random.randint(0, len(bglist))
+    bgr = random.randint(0, len(bglist)-1)
     bg = Image.open(bgimgpath + f'/{bglist[bgr]}').convert("RGBA")
     bg = bg.resize((bg.size[0] * 2, bg.size[1] * 2))
     offset = random.randint(0, 1024)
