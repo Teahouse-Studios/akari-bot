35e0d31af2945d7d8d320d008624d75c5c0b9a64
yzhh <2596322644@qq.com>
2021-05-30 14:33:05 +0800

    wtf I made that shit????????????

--- modules/user/hh.py
+++ modules/user/hh.py
@@ -3,26 +3,14 @@ import re
 
 def hh(w):
     if len(w) > 15:
-        w = re.findall('..?.?.?.?.?.?.?.?.?.?.?.?.?.?', w)
-        w = str(w)
-        w = re.sub('\[', '', w)
-        w = re.sub('\]', '', w)
-        w = re.sub(',', '-\n', w)
-        w = re.sub('\'', '', w)
-        return (w)
-    else:
-        return (w)
+        w = [w[i:i+15] for i in range(0, len(w), 15)]
+        w = '-\n'.join(w)
+    return w
 
 
 def hh1(w):
     w = '理由“' + w + '”'
     if len(w) > 25:
-        w = re.findall('..?.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?', w)
-        w = str(w)
-        w = re.sub('\[', '', w)
-        w = re.sub('\]', '', w)
-        w = re.sub(',', '-\n', w)
-        w = re.sub('\'', '', w)
-        return (w)
-    else:
-        return (w)
+        w = [w[i:i+25] for i in range(0, len(w), 25)]
+        w = '-\n'.join(w)
+    return w
