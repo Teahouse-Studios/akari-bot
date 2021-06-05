77267d2cc34e75f61086cda11664c0c207f56ed6
yzhh <2596322644@qq.com>
2021-06-05 11:52:27 +0800

    bugfix

--- database/updatedatabase.py
+++ database/updatedatabase.py
@@ -4,7 +4,13 @@ import sqlite3
 dbpath = os.path.abspath('./database/save.db')
 conn = sqlite3.connect(dbpath)
 c = conn.cursor()
+a = c.execute(f"ALTER TABLE group_adminuser RENAME TO group_adminuser_old")
 c.execute('''CREATE TABLE group_adminuser
-       (ID INT PRIMARY KEY     NOT NULL,
-       QGROUP  INT);''')
+       (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+       TID INT,
+       TGROUP INT);''')
+t = c.execute(f"SELECT * FROM group_adminuser_old").fetchall()
+for x in t:
+    c.execute(f"INSERT INTO group_adminuser (TID, TGROUP) VALUES (?, ?)", x)
+conn.commit()
 c.close()
