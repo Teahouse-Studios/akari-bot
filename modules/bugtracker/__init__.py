35adc26e1bd89f4192f676585fc9e0fa733b9341
Dianliang233 <dianliang233@gmail.com>
2021-07-09 11:07:45 +0800

    [SECURITY] Fix CWE-20

--- modules/bugtracker/__init__.py
+++ modules/bugtracker/__init__.py
@@ -30,10 +30,10 @@ async def regex_bugtracker(kwargs: dict):
             result = await bugtracker_get(q.group(1) + '-' + q.group(2))
             msgchain = MessageChain.create([Plain(result)])
             await sendMessage(kwargs, msgchain)
-    findlink = re.findall(r'(https://bugs.mojang.com/browse/.*?-\d*)', msg)
+    findlink = re.findall(r'(https://bugs\.mojang\.com/browse/.*?-\d*)', msg)
     for link in findlink:
         print(link)
-        matchbug = re.match(r'https://bugs.mojang.com/browse/(.*?-\d*)', link)
+        matchbug = re.match(r'https://bugs\.mojang\.com/browse/(.*?-\d*)', link)
         if matchbug:
             await sendMessage(kwargs, await bugtracker_get(matchbug.group(1)))
 
