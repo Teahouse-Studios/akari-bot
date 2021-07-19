63440959c472545f8394ec35a72b5bbce5a10c13
yzhh <2596322644@qq.com>
2021-07-19 13:34:29 +0800

    update docopt to docopt-ng 0.7.2

--- core/parser/command.py
+++ core/parser/command.py
@@ -2,14 +2,19 @@ import shlex
 from core.docopt import docopt
 
 
-class InvalidCommandFormatError:
+class InvalidCommandFormatError(BaseException):
     def __init__(self, *args, **kwargs):
         pass
 
 
 class CommandParser:
-    def __init__(self, args: str):
-        self.args = 'Usage:\n' + args
+    def __init__(self, args: [str, list, tuple]):
+        if isinstance(args, str):
+            self.args = f'Usage:\n  {args}'
+        elif isinstance(args, (list, tuple)):
+            self.args = f'Usage:\n  ' + '\n  '.join(x for x in args)
+        else:
+            raise InvalidCommandFormatError
 
     def parse(self, command):
-        return docopt(self.args, argvs=shlex.split(command)[1:], help=False)
+        return docopt(self.args, argvs=shlex.split(command)[1:], default_help=False)
