65597d92031720869c478faf6816be2b2fbb8d4b
Dianliang233 <dianliang233@gmail.com>
2021-08-02 17:29:44 +0800

    More debugging (still not yet usable)

--- core/i18n/__init__.py
+++ core/i18n/__init__.py
@@ -5,35 +5,37 @@ from core.elements import MessageSession
 from database import BotDBUtil
 
 class BotI18n:
+    storage = {}
+    languages = []
+    uselang = 'zh_cn'
+    session = {}
+
     def __init__(self, msg: MessageSession):
-        self.session = msg
+        BotI18n.session = msg
         files = os.listdir(os.path.abspath('./core/i18n/lang/'))
-        self.storage = {}
-        self.languages: list = []
-        self.uselang = 'zh_cn'
         for lang in files:
             obj = json.load(open('./core/i18n/lang/' + lang, 'r'))
             name = lang.split('.')[0]
-            self.languages.append(name)
-            self.storage[name] = obj
+            BotI18n.languages.append(name)
+            BotI18n.storage[name] = obj
 
         for module in BotDBUtil.Module(msg).enable_modules_list:
             if module.startswith('_lang_'):
-                self.uselang = module.strip('_lang_')
+                BotI18n.uselang = module.strip('_lang_')
                 break
             else:
                 continue
 
     @classmethod
     def get_string(self, key: str):
-        strings = self.storage[self.uselang]
+        strings = BotI18n.storage[BotI18n.uselang]
         if key in strings:
             return strings[key]
         else:
             fallbacks = strings['__metadata__']['fallback']
             for fallback in fallbacks:
                 fallbacked = False
-                fallback_strings = self.storage[fallback]
+                fallback_strings = BotI18n.storage[fallback]
                 if key in fallback_strings:
                     fallbacked = True
                     return fallback_strings[key]
@@ -44,5 +46,7 @@ class BotI18n:
 
     @classmethod
     def set_language(self, target):
-        BotDBUtil.Module.disable(f'_lang_{self.uselang}')
-        BotDBUtil.Module.enable(f'_lang_{target}')
+        module_session = BotDBUtil.Module(BotI18n.session)
+        BotI18n.uselang = target
+        module_session.disable(f'_lang_{BotI18n.uselang}')
+        module_session.enable(f'_lang_{target}')
