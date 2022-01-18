ba8b5c9671d8783c62aaccdbdaf3649c67e4cbd4
yzhh <2596322644@qq.com>
2022-01-18 20:46:56 +0800

    Update logger.py

--- core/logger.py
+++ core/logger.py
@@ -1,14 +1,18 @@
 '''基于logging的日志器。'''
 import logging
+import os
+import re
 import sys
 
 factory = logging.getLogRecordFactory()
+
 
 def record_factory(*args, **kwargs):
     record = factory(*args, **kwargs)
     for x in sys.path:
         record.pathname = record.pathname.replace(x, '')
     record.pathname = record.pathname.replace('\\', '.').replace('/', '.')[1:]
+    record.botname = re.split(r'[/\\]', sys.path[0])[-1]
     return record
 
 
@@ -16,7 +20,7 @@ logging.setLogRecordFactory(record_factory)
 
 
 class Logginglogger:
-    def __init__(self, fmt="[%(asctime)s][%(levelname)s][%(pathname)s:%(lineno)d]: %(msg)s", **kwargs):
+    def __init__(self, fmt="[%(asctime)s][%(botname)s][%(levelname)s][%(pathname)s:%(lineno)d]: %(msg)s", **kwargs):
         logging.basicConfig(
             format=fmt,
             level=logging.INFO if not kwargs.get("debug") else logging.DEBUG
