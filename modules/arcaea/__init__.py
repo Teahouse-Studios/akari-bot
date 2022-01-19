3b235a3f6bfb146127711c9b4eb52546fb58e0ab
yzhh <31803608+OasisAkari@users.noreply.github.com>
2022-01-20 01:50:57 +0800

    Update __init__.py

--- modules/arcaea/__init__.py
+++ modules/arcaea/__init__.py
@@ -18,14 +18,13 @@ webrender = Config('web_render')
 assets_path = os.path.abspath('./assets/arcaea')
 
 
-@arc.handle('b30 [<friendcode>] [unofficial] {查询一个Arcaea用户的b30列表}')
+@arc.handle('b30 [<friendcode>] {查询一个Arcaea用户的b30列表}', 'b30 unofficial [<friendcode>] {查询一个Arcaea用户的b30列表（不使用官方API）}')
 async def _(msg: MessageSession):
     if not os.path.exists(assets_path):
         await msg.sendMessage('未找到资源文件！请放置一枚arcaea的apk到机器人的assets目录并重命名为arc.apk后，使用~arcaea initialize初始化资源。')
         return
     query_code = None
     unofficial = msg.parsed_msg['unofficial']
-    onofficial = True if unofficial is not None else False
     friendcode = msg.parsed_msg['<friendcode>']
     if friendcode is not None:
         query_code = friendcode
