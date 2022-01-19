8b4f6b55de071005c8cd907c84ad37238df77a0c
HornCopper <68726147+HornCopper@users.noreply.github.com>
2022-01-19 14:51:09 +0800

    Create __init__.py

---
+++ modules/news/__init__.py
@@ -0,0 +1,14 @@
+from core.component import on_command
+from core.elements import MessageSession, Image
+
+from .world_news import news as n
+
+news = on_command(
+    bind_prefix='news',
+    desc='获取今日世界新闻。',
+    developers=['HornCopper'])
+
+@news.handle()
+async def main(msg: MessageSession):
+    img_url = await n()
+    await msg.sendMessage([Image(path=img_url)])
