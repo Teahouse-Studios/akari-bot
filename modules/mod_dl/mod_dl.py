7bea0b00abe78e26c64c8780eac6d1e1c1d4614a
HornCopper <68726147+HornCopper@users.noreply.github.com>
2022-01-19 23:20:43 +0800

    Create mod_dl.py

---
+++ modules/mod_dl/mod_dl.py
@@ -0,0 +1,40 @@
+'''
+调用了其他API，但是已于CurseForge仓库对接。
+'''
+from bs4 import BeautifulSoup
+
+search_piece_1 = 'https://files.xmdhs.top/curseforge/s?q='
+search_piece_2 = '&type=1'
+search_step_2 = 'https://files.xmdhs.top/curseforge/history?id='
+
+def Chinese(string: str):
+    for word in string:
+        if u'\u4e00' <= word <= u'\u9fff':
+            return True
+    return False
+async def curseforge(mod_name: str,ver: str):
+    if Chinese(mod_name):
+        return '请不要输入中文，CurseForge暂不支持中文搜索。'
+    full_url = search_piece_1 + mod_name + search_piece_2
+    html = await get_url(full_url)
+    if html == '404 page not found':
+        return '未找到搜索结果。'
+    bs = BeautifulSoup(html,'html.parser')
+    information = bs.body.div.div.a
+    more_specific_html = str(information)
+    id = more_specific_html[int(more_specific_html.find('id=')+3):int(more_specific_html.find('\" style='))]
+    final_url = search_step_2 + id + '&ver=' + ver
+    html_2 = await get_url(final_url)
+    bs_2 = BeautifulSoup(html_2,'html.parser')
+    results = bs_2.body.div.div.table.tbody.find_all('tr')
+    information_2 = str(results[1])
+    download_link = information_2[int(information_2.find("\"")+1):int(information_2.find("\" target="))]
+    file_name = information_2[int(information_2.find("_blank\"")+8):int(information_2.find("</a>"))]
+    if bool(information_2.find("Beta")) == True:
+        status = "Release"
+    if bool(information_2.find("Release")) == True:
+        status = "Beta"
+    dict = {"filename":file_name,"download_link":download_link,"status":status}
+    print(dict)
+    return dict
+    
