026759e76db1b39b0f750cdd841abf327797edb8
yzhh <2596322644@qq.com>
2022-01-21 19:40:12 +0800

    bugfix

--- modules/mod_dl/__init__.py
+++ modules/mod_dl/__init__.py
@@ -1,6 +1,5 @@
 from core.component import on_command
 from core.elements import MessageSession
-from core.elements import Url
 
 from .mod_dl import curseforge as d
 
@@ -10,13 +9,14 @@ mod_dl = on_command(
     developers=['HornCopper'],
     recommend_modules=['mcmod'])
 
-@news.handle('<mod_name> <mcversion> {通过模组名获取模组下载链接，CloudFlare CDN支持。')
+
+@mod_dl.handle('<mod_name> <mcversion> {通过模组名获取模组下载链接，CloudFlare CDN支持。}')
 async def main(msg: MessageSession):
-    info = await d(msg.parsed_msg['<mod_name>'],msg.parsed_msg['<mcversion>'])
-    if info['msg'] != '200 OK':
-        await msg.sendMessage(info['msg'])
+    info = await d(msg.parsed_msg['<mod_name>'], msg.parsed_msg['<mcversion>'])
+    if not info['success']:
+        return await msg.sendMessage(info['msg'])
     link = info["download_link"]
     name = info["filename"]
     status = info["status"]
-    message = f'下载链接：{str(Url(link))}\n文件名：{name}\n版本状态：{status}'
+    message = f'下载链接：{link}\n文件名：{name}\n版本状态：{status}'
     await msg.sendMessage(message)
