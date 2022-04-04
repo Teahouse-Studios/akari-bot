0275abb5c416c0d8fec8e17404d51ff0db4a934b
yzhh <2596322644@qq.com>
2022-04-04 14:16:39 +0800

    convert discord embed

--- core/utils/message.py
+++ core/utils/message.py
@@ -1,4 +1,9 @@
 import re
+from typing import Union
+
+from core.elements.message.internal import Embed, EmbedField
+
+from discord import Embed as DiscordEmbed
 
 
 def remove_ineffective_text(prefix: str, lst: list) -> list:
@@ -45,3 +50,39 @@ def RemoveDuplicateSpace(text: str) -> str:
             display_list.append(x)
     text = ' '.join(display_list)
     return text
+
+
+def convertDiscordEmbed(embed: Union[DiscordEmbed, dict]) -> Embed:
+    '''将DiscordEmbed转换为Embed。
+    :param embed: DiscordEmbed。
+    :returns: Embed。'''
+    embed_ = Embed()
+    if isinstance(embed, DiscordEmbed):
+        embed = embed.to_dict()
+    if isinstance(embed, dict):
+        if 'title' in embed:
+            embed_.title = embed['title']
+        if 'description' in embed:
+            embed_.description = embed['description']
+        if 'url' in embed:
+            embed_.url = embed['url']
+        if 'color' in embed:
+            embed_.color = embed['color']
+        if 'timestamp' in embed:
+            embed_.timestamp = embed['timestamp']
+        if 'footer' in embed:
+            embed_.footer = embed['footer']['text']
+        if 'image' in embed:
+            embed_.image = embed['image']
+        if 'thumbnail' in embed:
+            embed_.thumbnail = embed['thumbnail']
+        if 'author' in embed:
+            embed_.author = embed['author']
+        if 'fields' in embed:
+            fields = []
+            for field_value in embed['fields']:
+                fields.append(EmbedField(field_value['name'], field_value['value'], field_value['inline']))
+            embed_.fields = fields
+    return embed_
+
+
