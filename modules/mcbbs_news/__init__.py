6b03af992556f576ab9ea30f0f92596eb1d82c6d
HornCopper <68726147+HornCopper@users.noreply.github.com>
2022-07-15 00:35:35 +0800

    Rewrite module mcbbs_news.

--- modules/mcbbs_news/__init__.py
+++ modules/mcbbs_news/__init__.py
@@ -1,25 +1,44 @@
 from core.component import on_command
 from core.builtins.message import MessageSession
 
-from .mcbbs_news import news
+from .mcbbs_news import get_news as news
+from config import Config
 
 mcbbs_news = on_command(
     bind_prefix='mcbbs_news',
     alias=['mn', 'mcbbsnews'],
-    desc='获得 MCBBS 幻翼快讯版最新新闻（未被版主高亮过的新闻将被忽略）',
-    developers=['Dianliang233']
+    desc='获得 MCBBS 幻翼快讯版最新新闻',
+    developers=['HornCopper']
 )
 
 
 @mcbbs_news.handle()
 async def main(msg: MessageSession):
-    res = await news()
-    print('res' + str(res))
-    if res is None:
-        message = '没有找到任何新闻。'
-    else:
-        lst = []
-        for i in res:
-            lst += [f'{i["count"]}. {i["title"]} - {i["url"]}']
-        message = '\n'.join(lst)
-    await msg.finish(message)
+    def template(sth):
+        return {"type": "node","data": {"name": f"MCBBS · 幻翼快讯","uin": qq_account,"content": [{"type": "text", "data": {"text": sth}}]}}
+    message = ""
+    nodelist = []
+    response = await news()
+    titles = response["titles"]
+    authors = response["authors"]
+    time = response["time"]
+    kinds = response["kinds"]
+    links = response["links"]
+    qq_account = Config("qq_account")
+    legacy = True
+    if msg.Feature.forward and msg.target.targetFrom == 'QQ|Group':
+        try:
+            for i in range(len(titles)):
+                nodelist.append(template(f"标题：{title[i]}\n作者：{author[i]}\n发布日期：{time[i]}\n链接：{links[i]}"))
+            await msg.fake_forward_msg(nodelist)
+            legacy = False
+        except Exception:
+            traceback.print_exc()
+            await msg.finish('无法发送转发消息，已自动回滚至传统样式。')
+            legacy = True
+    if legacy: # 长得过分，所以截取5条
+        message = ""
+        for i in range(4):
+            message = message+f"{kinds[i]}{titles[i]} · {authors[i]}（{time[i]}）\n"
+        await msg.send(message[0:-1]) # 最后会多一个换行符
+        
