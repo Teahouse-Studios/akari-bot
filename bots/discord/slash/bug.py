5f0b7db5fe59deea9516c124860553467b5f3694
yzhh <2596322644@qq.com>
2022-08-17 20:20:01 +0800

    1111

--- bots/discord/slash/bug.py
+++ bots/discord/slash/bug.py
@@ -9,10 +9,7 @@ async def auto_search(ctx: discord.AutocompleteContext):
         return ['MC-', 'MCPE-', 'MCD-', 'MCL-', 'REALMS-', 'WEB-', 'MCCE-']
 
 
-bug = client.create_group("bug", "查询Mojira的相关信息")
-
-
-@bug.command(description="查询一个已记录在Mojira上的bug信息")
+@client.command(description="查询一个已记录在Mojira上的bug信息")
 @discord.option(name="mojiraid", autocomplete=auto_search)
-async def b30(ctx: discord.ApplicationContext, mojiraid: str):
+async def bug(ctx: discord.ApplicationContext, mojiraid: str):
     await slash_parser(ctx, mojiraid)
--- bots/discord/slash/dictionary.py
+++ bots/discord/slash/dictionary.py
@@ -4,10 +4,7 @@ from bots.discord.client import client
 from bots.discord.slash_parser import slash_parser
 
 
-dict_ = client.create_group("dict", "查询柯林斯词典")
-
-
-@dict_.command(description="查询柯林斯词典")
+@client.slash_command(description="查询柯林斯词典", name='dict')
 @discord.option(name="word", description="词汇")
-async def query(ctx: discord.ApplicationContext, word: str):
+async def _(ctx: discord.ApplicationContext, word: str):
     await slash_parser(ctx, word)
