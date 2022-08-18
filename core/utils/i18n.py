e58fdd8c256c25021f7a6ff3a57679d42155e54e
yzhh <2596322644@qq.com>
2022-08-18 23:26:26 +0800

    compatible with py38

--- core/utils/i18n.py
+++ core/utils/i18n.py
@@ -13,6 +13,7 @@ from database import BotDBUtil
 # locale files get too large
 locale_cache = {}
 
+
 # From https://stackoverflow.com/a/6027615
 def flatten(d, parent_key='', sep='.'):
     items = []
@@ -25,12 +26,19 @@ def flatten(d, parent_key='', sep='.'):
     return dict(items)
 
 
+def remove_suffix(string, suffix):
+    return string[:-len(suffix)] if string.endswith(suffix) else string
+
+
 def load_locale_file():
     locales = os.listdir('./locales')
     for l in locales:
         with open(f'./locales/{l}', 'r', encoding='utf-8') as f:
-            locale_cache[l.removesuffix('.json')] = flatten(json.load(f))
+            locale_cache[remove_suffix(l, '.json')] = flatten(json.load(f))
+
+
 load_locale_file()
+
 
 class LocaleFile(TypedDict):
     key: str
@@ -38,9 +46,11 @@ class LocaleFile(TypedDict):
 
 
 class Locale:
-    def __init__(self, locale: str, fallback_lng: List[str] = ['zh_cn', 'en_us']):
-        '''创建一个本地化对象'''
+    def __init__(self, locale: str, fallback_lng=None):
+        """创建一个本地化对象"""
 
+        if fallback_lng is None:
+            fallback_lng = ['zh_cn', 'en_us']
         self.locale = locale
         self.data: LocaleFile = locale_cache[locale]
         self.fallback_lng = fallback_lng
@@ -59,15 +69,16 @@ class Locale:
     def get_string_with_fallback(self, key: str) -> str:
         value = self.data.get(key, None)
         if value is not None:
-            return value # 1. 如果本地化字符串存在，直接返回
+            return value  # 1. 如果本地化字符串存在，直接返回
         fallback_lng = list(self.fallback_lng)
         fallback_lng.insert(0, self.locale)
         for lng in fallback_lng:
             if lng in locale_cache:
                 string = locale_cache[lng].get(key, None)
                 if string is not None:
-                    return string # 2. 如果在 fallback 语言中本地化字符串存在，直接返回
-        return key # 3. 如果在 fallback 语言中本地化字符串不存在，返回 key
+                    return string  # 2. 如果在 fallback 语言中本地化字符串存在，直接返回
+        return key  # 3. 如果在 fallback 语言中本地化字符串不存在，返回 key
+
 
 def get_target_locale_setting(msg: MessageSession):
     return BotDBUtil.TargetInfo(targetId=msg.target.targetId).query.locale
