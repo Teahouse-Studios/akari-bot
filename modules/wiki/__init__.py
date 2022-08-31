bf3df25243733d39bb9720881f569544165780aa
yzhh <31803608+OasisAkari@users.noreply.github.com>
2022-09-01 00:40:06 +0800

    Update __init__.py

--- modules/wiki/__init__.py
+++ modules/wiki/__init__.py
@@ -383,59 +383,54 @@ async def _(msg: MessageSession):
                     query_list.append({x: wiki_.value})
         if query_list:
             for q in query_list:
-                async def infobox():
-                    if msg.Feature.image:
-                        for qq in q:
-                            get_infobox = await get_pic(q[qq].realurl, qq, headers, allow_special_page=q[qq].in_allowlist)
-                            if get_infobox:
-                                await msg.sendMessage(Image(get_infobox), quote=False)
+                img_send = False
+                for qq in q:
+                    articlepath = q[qq].articlepath.replace('$1', '(.*)')
+                    get_title = re.sub(r'' + articlepath, '\\1', qq)
+                    if get_title != '':
+                        title = urllib.parse.unquote(get_title)
+                        if not q[qq].in_allowlist:
+                            for result in await check(title):
+                                if not result['status']:
+                                    return
 
-                async def section():
-                    if msg.Feature.image:
-                        for qq in q:
-                            section_ = []
-                            quote_code = False
-                            for qs in qq:
-                                if qs == '#':
-                                    quote_code = True
-                                if qs == '?':
-                                    quote_code = False
-                                if quote_code:
-                                    section_.append(qs)
-                            if section_:
-                                s = urllib.parse.unquote(''.join(section_)[1:])
-                                if q[qq].realurl:
-                                    get_section = await get_pic(q[qq].realurl, qq, headers, section=s)
-                                    if get_section:
-                                        await msg.sendMessage(Image(get_section))
-
-                async def image_and_voice():
+                        wiki_ = WikiLib(qq)
+                        get_page = await wiki_.parse_page_info(title)
+                        if get_page.status and get_page.file is not None:
+                            dl = await download_to_cache(get_page.file)
+                            guess_type = filetype.guess(dl)
+                            if guess_type is not None:
+                                if guess_type.extension in ["png", "gif", "jpg", "jpeg", "webp", "bmp", "ico"]:
+                                    if msg.Feature.image:
+                                        await msg.sendMessage(Image(dl), quote=False)
+                                        img_send = True
+                                elif guess_type.extension in ["oga", "ogg", "flac", "mp3", "wav"]:
+                                    if msg.Feature.voice:
+                                        await msg.sendMessage(Voice(dl), quote=False)
+                if len(query_list) == 1 and img_send:
+                    return
+                if msg.Feature.image:
                     for qq in q:
-                        articlepath = q[qq].articlepath.replace('$1', '(.*)')
-                        get_title = re.sub(r'' + articlepath, '\\1', qq)
-                        if get_title != '':
-                            title = urllib.parse.unquote(get_title)
-                            if not q[qq].in_allowlist:
-                                for result in await check(title):
-                                    if not result['status']:
-                                        return
+                        get_infobox = await get_pic(q[qq].realurl, qq, headers, allow_special_page=q[qq].in_allowlist)
+                        if get_infobox:
+                            await msg.sendMessage(Image(get_infobox), quote=False)
 
-                            wiki_ = WikiLib(qq)
-                            get_page = await wiki_.parse_page_info(title)
-                            if get_page.status and get_page.file is not None:
-                                dl = await download_to_cache(get_page.file)
-                                guess_type = filetype.guess(dl)
-                                if guess_type is not None:
-                                    if guess_type.extension in ["png", "gif", "jpg", "jpeg", "webp", "bmp", "ico"]:
-                                        if msg.Feature.image:
-                                            await msg.sendMessage(Image(dl), quote=False)
-                                    elif guess_type.extension in ["oga", "ogg", "flac", "mp3", "wav"]:
-                                        if msg.Feature.voice:
-                                            await msg.sendMessage(Voice(dl), quote=False)
-
-                asyncio.create_task(infobox())
-                asyncio.create_task(section())
-                asyncio.create_task(image_and_voice())
+                    for qq in q:
+                        section_ = []
+                        quote_code = False
+                        for qs in qq:
+                            if qs == '#':
+                                quote_code = True
+                            if qs == '?':
+                                quote_code = False
+                            if quote_code:
+                                section_.append(qs)
+                        if section_:
+                            s = urllib.parse.unquote(''.join(section_)[1:])
+                            if q[qq].realurl:
+                                get_section = await get_pic(q[qq].realurl, qq, headers, section=s)
+                                if get_section:
+                                    await msg.sendMessage(Image(get_section))
     asyncio.create_task(bgtask())
 
 
