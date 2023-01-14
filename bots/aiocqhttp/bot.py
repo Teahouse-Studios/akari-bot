c838c9cda83a9bb08f51922f50597aad6a59a631
yzhh <31803608+OasisAkari@users.noreply.github.com>
2023-01-14 13:01:34 +0800

    Update bot.py

--- bots/aiocqhttp/bot.py
+++ bots/aiocqhttp/bot.py
@@ -100,9 +100,12 @@ async def _(event: Event):
 
 @bot.on('request.group.invite')
 async def _(event: Event):
-    await bot.send_private_msg(user_id=event.user_id,
-                               message='你好！本机器人暂时不主动同意入群请求。\n'
-                                       '请至https://github.com/Teahouse-Studios/bot/issues/new?assignees=OasisAkari&labels=New&template=add_new_group.yaml&title=%5BNEW%5D%3A+申请入群。')
+    if BotDBUtil.SenderInfo('QQ|' + str(event.user_id)).query.isSuperUser:
+        return {'approve': True}
+    if Config('invite_join_group_notice'):
+        await bot.send_private_msg(user_id=event.user_id,
+                                   message='你好！本机器人暂时不主动同意入群请求。\n'
+                                           '请至https://github.com/Teahouse-Studios/bot/issues/new?assignees=OasisAkari&labels=New&template=add_new_group.yaml&title=%5BNEW%5D%3A+申请入群。')
 
 
 @bot.on_notice('group_ban')
