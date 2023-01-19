feacf6622168952bcef17055cc69777619585f99
yzhh <31803608+OasisAkari@users.noreply.github.com>
2023-01-19 23:10:06 +0800

    Create calc.py

---
+++ modules/calc/calc.py
@@ -0,0 +1,39 @@
+from constant import consts
+from simpleeval import InvalidExpression, SimpleEval, DEFAULT_FUNCTIONS, DEFAULT_NAMES, DEFAULT_OPERATORS
+import ast
+import sys
+import operator as op
+import math
+import statistics
+
+funcs = {}
+
+
+def add_func(module):
+    for name in dir(math):
+        item = getattr(math, name)
+        if not name.startswith('_') and callable(item):
+            funcs[name] = item
+
+
+add_func(math)
+add_func(statistics)
+
+s_eval = SimpleEval(
+    operators={
+        **DEFAULT_OPERATORS,
+        ast.BitOr: op.or_,
+        ast.BitAnd: op.and_,
+        ast.BitXor: op.xor,
+        ast.Invert: op.invert,
+    },
+    functions={**funcs, **DEFAULT_FUNCTIONS},
+    names={
+        **DEFAULT_NAMES, **consts,
+        'pi': math.pi,
+        'e': math.e,
+        'tau': math.tau,
+        'inf': math.inf, 'nan': math.nan,
+    },)
+
+print(s_eval.eval(' '.join(sys.argv[1:])))
