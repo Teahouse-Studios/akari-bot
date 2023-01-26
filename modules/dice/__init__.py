91c08b40e577f210f80209c5de02f193990b717a
Light Beacon <2682645990@qq.com>
2023-01-26 15:16:11 +0800

    improve code

--- modules/dice/__init__.py
+++ modules/dice/__init__.py
@@ -6,8 +6,8 @@ dice = on_command('dice', alias={'d20': 'dice d20', 'd100': 'dice d100',
                   'd6': 'dice d6'}, developers=['Light-Beacon'], desc='随机骰子',)
 
 
-@dice.handle('[<dices>] [<dc>] {摇动指定骰子,可指定 dc 判断判定。}',)
+@dice.handle('<dices> [<dc>] {摇动指定骰子,可指定 dc 判断判定。}',)
 async def _(msg: MessageSession):
-    dice = msg.parsed_msg.get('<dices>', "D20")
+    dice = msg.parsed_msg['<dices>']
     dc = msg.parsed_msg.get('<dc>', 0)
     await msg.finish(await roll(dice, dc))
