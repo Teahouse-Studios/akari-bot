e7e7b1a175c5eac11c80e1b3e987366ac42abce0
Dianliang233 <dianliang233@gmail.com>
2023-02-07 14:38:31 +0800

    Delete msg after finnish

--- modules/summary/__init__.py
+++ modules/summary/__init__.py
@@ -21,9 +21,10 @@ async def _(msg: Bot.MessageSession):
     text = ''
     for m in msgs:
         text += f'\n{m["sender"]["nickname"]}（ID：{m["sender"]["user_id"]}，Unix时间：{m["time"]}）：{m["content"]}'
-    msg.sendMessage(f'正在生成摘要。您的聊天记录共 {len(text)} 个字符，大约需要 {round(len(text) / 33.5, 1)} 秒请稍候……')
+    wait_msg = await msg.sendMessage(f'正在生成摘要。您的聊天记录共 {len(text)} 个字符，大约需要 {round(len(text) / 33.5, 1)} 秒请稍候……')
     res = await post_url('https://chat-simplifier.imzbb.cc/api/generate', data=json.dumps({'promt': f'''把以下聊天记录概括为一段完整的纪要。当遇到!!!CHATENDS时，聊天记录结束，请在下方续写其摘要：
 
 {text}
 !!!CHATENDS'''}), headers={'Content-Type': 'application/json'})
+    await wait_msg.delete()
     await msg.finish(res.removesuffix('<|im_end|>'), disable_secret_check=True)
