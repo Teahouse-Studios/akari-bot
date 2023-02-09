6754dff311575712c309f1c23354a0a621f7216a
Dianliang233 <dianliang233@gmail.com>
2023-02-09 11:10:34 +0800

    Add natural functionality

--- modules/natural/__init__.py
+++ modules/natural/__init__.py
@@ -4,9 +4,15 @@ from core.builtins import Bot
 from core.component import on_command
 from config import Config
 
-n = on_command('summary', developers=['Dianliang233'], desc='使用 InstructGPT 生成合并转发信息的聊天记录摘要。', required_superuser=True)
+n = on_command('natural', alias=['nl2c'], developers=['Dianliang233'], desc='使用 InstructGPT 把自然语言转换成小可命令。', required_superuser=True)
 
 # Load your API key from an environment variable or secret management service
 openai.api_key = Config('openai_api_key')
+model = Config('nl2c_model')
 
-response = openai.Completion.create(model="text-davinci-003", prompt="Say this is a test", temperature=0, max_tokens=7)
+@n.handle('<text> {使用 InstructGPT 把自然语言转换成小可命令。}')
+async def _(msg: Bot.MessageSession):
+    i = msg.parsed_msg['<text>']
+    response = openai.Completion.create(
+            model=model, prompt=f'{i}\n\n###\n\n', temperature=0, max_tokens=256, stop=['\n'])
+    await msg.finish(response['choices'][0]['text'])
