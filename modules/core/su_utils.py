4ec4a7fb614c95ada9f0d91261f95a75304d3ca3
好耶！ <86039582+cloudw233@users.noreply.github.com>
2023-02-18 21:58:52 +0800

    添加船新模块，大喇叭（雾

--- modules/core/su_utils.py
+++ modules/core/su_utils.py
@@ -327,3 +327,10 @@ if Config('enable_eval'):
     @_eval.handle('<display_msg>')
     async def _(msg: Bot.MessageSession):
         await msg.finish(str(eval(msg.parsed_msg['<display_msg>'], {'msg': msg})))
+
+brc = on_command('broadcast', required_superuser=True, base=True, developers='haoye_qwq')
+
+@brc.handle('<text> {向所有群广播消息}')
+async def broadcast(msg: Bot.MessageSession):
+    get_str = msg.parsed_msg.get('<text>', False)
+    await bot.post_message('broadcast', get_str)
