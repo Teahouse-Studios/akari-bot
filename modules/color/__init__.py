9f78c0889edc5e9676a676c375ddeb414c90dab7
多羅狼 <Dorowolf314@gmail.com>
2023-02-18 15:56:56 +0800

    🤔

--- modules/color/__init__.py
+++ modules/color/__init__.py
@@ -50,11 +50,10 @@ async def _(msg: Bot.MessageSession):
     color_name = ''
     color_name_short = ''
     if color_name_raw[1]:
-        color_name = f'CSS 名称: {color_name_raw[0]}\n'
+        color_name = f'CSS 颜色名称: {color_name_raw[0]}\n'
         color_name_short = f'{color_name_raw[0]}'
     elif color_name_raw[0] is not None:
-        color_name = f'最相似的 CSS 名称: {color_name_raw[0]}\n'
-        color_name_short = f'{color_name_raw[0]} (approx.)'
+        color_name = f'最相似的 CSS 颜色名称: {color_name_raw[0]}\n'
 
     draw.multiline_text((300, 300), f'{color_name_short}\n{color_hex}\n{color_rgb}\n{color_hsl}', font=font, fill=contrast, anchor='mm', align='center', spacing=20)
     await msg.finish([f'{color_name}HEX：{color_hex}\nRGB：{color_rgb}\nHSL：{color_hsl}', BotImage(img)])
