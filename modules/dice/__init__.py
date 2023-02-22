801d59a193db834f10b334564dc4b7307cae945c
多羅狼 <Dorowolf314@gmail.com>
2023-02-22 22:29:36 +0800

    d100

--- modules/dice/__init__.py
+++ modules/dice/__init__.py
@@ -3,7 +3,7 @@ from core.component import on_command
 from .dice import roll
 
 dice = on_command('dice', alias={'d4': 'dice d4', 'd6': 'dice d6',
-                  'd8': 'dice d8', 'd10': 'dice d10', 'd12': 'dice d12', 'd20': 'dice d20'}, developers=['Light-Beacon'], desc='随机骰子',)
+                  'd8': 'dice d8', 'd10': 'dice d10', 'd12': 'dice d12', 'd20': 'dice d20', 'd100': 'dice d100'}, developers=['Light-Beacon'], desc='随机骰子',)
 
 
 @dice.handle('<dices> [<dc>] {摇动指定骰子,可指定 dc 判断判定。}',)
