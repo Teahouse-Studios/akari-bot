9a381a36e0c1456107b63a108499895abc9ba351
Nattiden <2682645990@qq.com>
2023-02-25 22:19:33 +0800

    Update dice.py

--- modules/dice/dice.py
+++ modules/dice/dice.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 import secrets
 import numpy as np
 import re
@@ -8,83 +9,116 @@ MAX_MOD_NUMBER = 10000  # 骰子最大加权值
 MIN_MOD_NUMBER = -10000  # 骰子最小加权值
 MAX_OUTPUT_NUM = 50  # 输出的最多数据量
 MAX_OUTPUT_LEN = 200  # 输出的最大长度
+MAX_DETAIL_CNT = 1  # n次摇动的骰子的总量超过该值时将不再显示详细信息
 
+class DiceSyntaxError(Exception):
+    """骰子语法错误"""
+    def __init__(self, message):
+        self.message = f"语法错误: {message}"
 
-def GetDiceArgs(dice: str):
-    dice.replace(' ', '')
-    dice = dice.upper()  # 便于识别
-    diceCount = '1'  # 骰子数量
-    rollTimes = '1'  # 摇取次数
-    advantage = '0'  # 保留的骰子量
-    mod = '0'  # Modifier
-    if 'D' not in dice:
-        return '发生错误：骰子语法错误'
-    temp = dice.split('D')
-    if len(temp):
-        if '*' in temp[0]:
-            rollTimes = temp[0].split('*')[0]  # 摇动次数
-            diceCount = temp[0].split('*')[1]  # 骰子数量
-            dice = dice[len(rollTimes):]
+class DiceValueError(Exception):
+    """骰子参数值错误"""
+    def __init__(self, message , value):
+        self.message = f"参数错误: {message}: {value}"
+
+class DiceItemBase(object):
+    """骰子项的基类"""
+    def __init__(self,diceCode:str,postive:bool):
+        self.postive = postive
+        self.code = diceCode
+        self.result = None
+        self.detail = ''
+
+    def GetResult(self):
+        return self.result
+
+    def GetDetail(self):
+        return self.detail
+
+class DiceMod(DiceItemBase):
+    """调节值项"""
+    def __init__(self, diceCode: str,postive: bool):
+        super().__init__(diceCode,postive)
+        if not diceCode.isdigit():
+            raise DiceValueError(f'无效的调节值项','+' if self.postive else '-' + diceCode)
         else:
-            diceCount = temp[0]
-    if not len(diceCount):
-        diceCount = '1'
-    back = re.match(r'^(.*)([+-].*)$', temp[1])
-    if back:
-        midstr = back.group(1)
-        mod = back.group(2)
-        if mod[0] == '+':
-            mod = mod[1:]
-    else:
-        midstr = temp[1]
-    midstrs = midstr.partition('K')
-    diceType = midstrs[0]
-    if 'K' in midstrs[1]:
-        advantage = midstrs[2].replace('L', '-')
-        if not len(advantage.removeprefix('-')):
-            advantage += '1' #K/KL后没有值默认为1
-    # 语法合法检定
-    if not diceCount.isdigit():
-        return '无效的骰子数量：' + diceCount
-    if not diceType.isdigit():
-        return '无效的骰子面数：' + diceType
-    if not rollTimes.isdigit():
-        return '无效的投骰次数：' + rollTimes
-    if not (mod.isdigit() or (mod[0] == '-' and mod[1:].isdigit())):
-        return '无效的调节值：' + mod
-    if not (advantage.isdigit() or (advantage[0] == '-' and advantage[1:].isdigit())):
-        return '无效的优劣势：' + advantage
+            self.value = int(diceCode)
+            if self.value > MAX_MOD_NUMBER or self.value < MIN_MOD_NUMBER:
+                raise DiceValueError(f'调节值不得小于 {MIN_MOD_NUMBER} 或大于 {MIN_MOD_NUMBER}')
     
-    return {'times': int(rollTimes), 'cnt': int(diceCount), 'type': int(diceType), 'adv': int(advantage),
-            'mod': int(mod), 'str': dice}
+    def GetDetail(self):
+        return self.value
 
+class Dice(DiceItemBase):
+    """骰子项"""
+    def __init__(self, diceCode: str,postive: bool):
+        diceCode = diceCode.replace(' ', '')
+        super().__init__(diceCode,postive)
+        args = self.GetArgs()
+        self.count = args[0]
+        self.type = args[1]
+        self.adv = args[2]
+        if self.count <= 0 or self.count > MAX_DICE_COUNT:
+            raise DiceValueError(f'骰子数量不得小于 1 或大于 {MAX_DICE_COUNT}',self.count)
+        if self.type  <= 0:
+            DiceValueError(f'骰子面数不得小于 2',self.count)
+        if self.type  == 1:
+            DiceValueError(f'1 ... 1 面的骰子？',self.count)
+        if abs(self.adv) > self.count:
+            DiceValueError(f'优劣势骰数大于总骰子数',self.adv)
+        
+    def GetArgs(self):
+        diceCode = self.code.upper()  # 便于识别
+        diceCount = '1'  # 骰子数量
+        advantage = '0'  # 保留的骰子量
+        if re.search(r'[^0-9DKL\*]',diceCode):
+            raise DiceSyntaxError('骰子语句中存在无法识别的字符')
+        if 'D' not in diceCode:
+            raise DiceSyntaxError('骰子语句缺失字符 D')
+        temp = diceCode.split('D')
+        if len(temp):
+            diceCount = temp[0]
+        else:
+            diceCount = '1'       
+        midstrs = temp[1].partition('K')
+        diceType = midstrs[0]
+        if 'K' in midstrs[1]:
+            advantage = midstrs[2].replace('L', '-')
+            if not len(advantage.removeprefix('-')):
+                advantage += '1' #K/KL后没有值默认为1
+        # 语法合法检定
+        if not diceCount.isdigit():
+            raise DiceValueError(f'无效的骰子数量',diceCount)
+        if not diceType.isdigit():
+            raise DiceValueError(f'无效的骰子面数',diceType)
+        if not (advantage.isdigit() or (advantage[0] == '-' and advantage[1:].isdigit())):
+            raise DiceValueError(f'无效的优劣势',advantage)
+        return (int(diceCount),int(diceType),int(advantage))
 
-def RollDice(args, dc):
-    output = '你掷得的结果是：\n'
-    successNum = 0
-    failNum = 0
-    for times in range(args['times']):
+    def Roll(self):   
+        output = ''
         result = 0
         diceResults = []
-        output += args['str'] + ' = '
+        adv = self.adv
+        output += self.code + ' = '
         # 生成随机序列
-        for i in range(args['cnt']):
-            diceResults.append(secrets.randbelow(int(args['type'])) + 1)
-        if args['adv'] != 0:
+        for i in range(self.count):
+            diceResults.append(secrets.randbelow(int(self.type)) + 1)
+        if adv != 0:
             newResults = []
             indexs = np.array(diceResults).argsort()
-            indexs = indexs[-args['adv']:] if args['adv'] > 0 else indexs[:-args['adv']]
+            indexs = indexs[-adv:] if adv > 0 else indexs[:-adv]
             output += '( '
             outputBuffer = ''
-            for i in range(args['cnt']):
+            for i in range(self.count):
                 outputBuffer += str(diceResults[i])
                 if i in indexs:
                     newResults.append(diceResults[i])
                     outputBuffer += '*'
-                if i < args['cnt'] - 1:
+                if i < self.count - 1:
                     outputBuffer += ','
-            if args['cnt'] >= MAX_OUTPUT_NUM:
-                outputBuffer = f"数量过大，已省略 {args['cnt']} 个数据"
+            if self.count >= MAX_OUTPUT_NUM:
+                outputBuffer = f"数量过大，已省略 {self.count} 个数据"
             output += outputBuffer + ' ) = '
             diceResults = newResults
         # 公用加法
@@ -101,53 +135,68 @@ def RollDice(args, dc):
                         output += '+'
             output += ' ] = '
         else:
-            result = diceResults[0]
-        # Modifier
-        if args['mod'] != 0:
-            output += f"N{result}"
-            if args['mod'] > 0:
-                output += '+'
-            output += f"{args['mod']} = "
-            result += args['mod']
-        output += str(result)
+            result = diceResults[0]      
+        if len(output) > MAX_OUTPUT_LEN:
+            output = '输出过长...'
+        self.detail = output
+        self.result = result
+
+async def GenerateMessage(dices: str,times: int, dc:int):
+    if re.search(r'[^0-9DKL\*]',dices):
+        return DiceSyntaxError('骰子语句中存在无法识别的字符').message
+    if times > MAX_ROLL_TIMES or times < 1:
+        return DiceValueError('投骰次数不得小于 1 或 大于 {MAX_ROLL_TIMES}').message
+    diceCodeList = re.compile(r'([+-][^+-]*)?(^[^+-]*)?').findall(dices)
+    diceList = []
+    haveErr = False
+    output = ""
+    diceCount = 0
+    i = 0
+    #初始化骰子序列
+    for item in diceCodeList:
+        i += 1
+        isAdd = True
+        if item[0] == '-':
+            isAdd = False
+            item = item[1:]
+        if item[0] == '+':
+            item = item[1:]
+        try:
+            if 'D' in item or 'd' in item:
+                diceList.append(Dice(item,isAdd))
+                diceCount += 1
+            elif item.isdigit():
+                diceList.append(DiceMod(item,isAdd))
+        except (DiceSyntaxError,DiceValueError) as ex:
+            output += f'\n在第{i}项时发生{ex.message}'
+            haveErr = True
+    if haveErr:
+        return '解析骰子多项式时存在以下错误：' + output
+    successNum = 0
+    failNum = 1
+    output = '你掷得的结果是：\n'
+    #开始摇动并输出
+    for i in range(times):
+        outputLine = ''
+        result = 0
+        for dice in diceList:
+            dice.Roll()
+            outputLine += '+' if dice.postive else '-'
+            if dice is Dice and times * diceCount < MAX_DETAIL_CNT:
+                outputLine += f'({dice.GetDetail()})'
+            else:
+                outputLine += str(dice.GetResult())
+            result += dice.GetResult()
+        outputLine = outputLine.removeprefix('+') #移除多项式首个+
+        outputLine += ' = ' + str(result)
         if dc != 0:
             if result > dc:
-                output += '，判定成功！'
+                outputLine += '，判定成功！'
                 successNum += 1
             else:
-                output += '，判定失败！'
+                outputLine += '，判定失败！'
                 failNum += 1
-        if (args['cnt'] == 1 or (args['cnt'] == 2 and (args['adv'] != 0))) and (
-            args['type'] == 20 or args['type'] == 100) and args['mod'] >= 0:
-            temp = result - args['mod']
-            if temp >= args['type']:
-                output += '大成功！'
-            if temp == 1:
-                output += '大失败！'
-        if times != args['times'] - 1:
-            output += '\n'
-    if len(output) > MAX_OUTPUT_LEN:
-        output = '输出过长...'
-    if dc != 0 and args['times'] > 1:
-        output += '\n▷ 判定成功数量：' + str(successNum) + '  判定失败数量：' + str(failNum)
+        output += f'{dices} = {outputLine}\n'
+    if dc != 0 and times > 1:
+        output += '▷ 判定成功数量：' + str(successNum) + '  判定失败数量：' + str(failNum)
     return output
-
-
-async def roll(dice: str, dc: int):
-    diceArgs = GetDiceArgs(dice)  # 语法识别
-    if type(diceArgs) is str:
-        return diceArgs  # 报错输出
-    if diceArgs['times'] <= 0 or diceArgs['times'] > MAX_ROLL_TIMES:
-        return f'发生错误：投骰次数不得小于 1 或 大于 {MAX_ROLL_TIMES}'
-    if diceArgs['cnt'] <= 0 or diceArgs['cnt'] > MAX_DICE_COUNT:
-        return f'发生错误：骰子数量不得小于 1 或大于 {MAX_DICE_COUNT}'
-    if diceArgs['type'] <= 0:
-        return '发生错误：骰子面数不得小于 2'
-    if diceArgs['type'] == 1:
-        return '发生错误：1 ... 1 面的骰子？'
-    if abs(diceArgs['adv']) > diceArgs['cnt']:
-        return '发生错误：优劣势骰数大于总骰子数'
-    if diceArgs['mod'] > MAX_MOD_NUMBER or diceArgs['mod'] < MIN_MOD_NUMBER:
-        return f'发生错误：调节值不得小于 {MIN_MOD_NUMBER} 或大于 {MIN_MOD_NUMBER} '
-    # 开始随机生成
-    return RollDice(diceArgs, dc)
