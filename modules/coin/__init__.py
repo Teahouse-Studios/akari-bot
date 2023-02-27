47f41d084641516afbf12fa181ed98546744ecfe
Light Beacon <2682645990@qq.com>
2023-02-27 21:24:30 +0800

    text changes

--- modules/coin/__init__.py
+++ modules/coin/__init__.py
@@ -2,8 +2,8 @@ import secrets
 from core.builtins.message import MessageSession
 from core.component import on_command,on_regex
 
-MAX_COIN_NUM = 20
-FACE_UP_RATE = 4975  # n/10000
+MAX_COIN_NUM = 100
+FACE_UP_RATE = 4975  # n/10000 
 FACE_DOWN_RATE = 4975
 COIN_DROP_PLACES = ["地上","桌子上","桌子底下","门口","窗户外","月球"]  # 硬币可能掉落的位置
 
@@ -32,9 +32,9 @@ async def flipCoins(count:int):
     if count > MAX_COIN_NUM:
         return f"发生错误：你最多只能抛 {MAX_COIN_NUM} 个硬币"
     if count <= 0:
-        return f"你投了个空气，什么也没发生..."
-    if FACE_UP_RATE + FACE_DOWN_RATE > 10000:
-        raise OverflowError("硬币概率总和超过最大概率")
+        return f"你抛了个空气，什么也没发生..."
+    if FACE_UP_RATE + FACE_DOWN_RATE > 10000 or FACE_UP_RATE < 0 or FACE_DOWN_RATE < 0:
+        raise OverflowError("硬币概率错误")
     faceUp = 0
     faceDown = 0
     stand = 0
@@ -46,10 +46,10 @@ async def flipCoins(count:int):
             faceDown += 1
         else:
             stand += 1
-    head = f"你投了 {count} 个硬币，"
+    head = f"你抛了 {count} 枚硬币，"
     if count == 1:
         drop_place = COIN_DROP_PLACES[secrets.randbelow(len(COIN_DROP_PLACES))]
-        head += f"它掉到了{drop_place}...\n"
+        head += f"它掉到了{drop_place}......\n"
         if faceUp:
             return head + "...是正面！"
         elif faceDown:
@@ -58,16 +58,16 @@ async def flipCoins(count:int):
             return head + "...它...它立起来了！"
     else:
         if not (stand or faceDown):
-            return "它们...\n...全是正面!"
+            return head + "它们...\n...全是正面!"
         if not (stand or faceUp):
-            return "它们...\n...全是反面!"
+            return head + "它们...\n...全是反面!"
         if not (faceUp or faceDown):
-            return "它们...\n...全都立起来了！？！？天哪！"
-        output = "其中...\n"
+            return head + "它们......\n...全都立起来了！？！？天哪！"
+        output = head + "其中......\n"
         if faceUp:
-            output += f"{faceUp}个是正面,"
+            output += f"{faceUp}枚是正面，"
         if faceDown:
-            output += f"{faceDown}个是反面"
+            output += f"{faceDown}枚是反面"
         if stand:
             output += f"...还有{stand}个立起来了！"
         return output
