58142d63c7d36c235d49304bfa2168d9a9d5e106
好耶！ <86039582+cloudw233@users.noreply.github.com>
2023-03-04 22:09:21 +0800

    答辩写法

--- modules/zhongli/__init__.py
+++ modules/zhongli/__init__.py
@@ -6,8 +6,15 @@ zl = on_command('zhongli', alias='zl',desc='对接钟离（ 万 物 互 联 ）'
 @zl.handle('<command> {向钟离发送命令}')
 async def zlc(send:Bot.MessageSession):
     cmd = send.parsed_msg['<command>']
-    if 'bind' or 'load' or 'stats' or 'code' or 'check-in' or 'mute' or 'save' or 'load' or 'permission' in cmd:
-        await send.sendMessage('因对接方式问题，暂不支持该命令')
+    if '-bind' in cmd:await send.sendMessage('因对接方式问题，暂不支持该命令')
+    elif '-load' in cmd:await send.sendMessage('因对接方式问题，暂不支持该命令')
+    elif '-stats' in cmd:await send.sendMessage('因对接方式问题，暂不支持该命令')
+    elif '-code' in cmd:await send.sendMessage('因对接方式问题，暂不支持该命令')
+    elif '-check-in' in cmd:await send.sendMessage('因对接方式问题，暂不支持该命令')
+    elif '-mute' in cmd:await send.sendMessage('因对接方式问题，暂不支持该命令')
+    elif '-save' in cmd:await send.sendMessage('因对接方式问题，暂不支持该命令')
+    elif '-load' in cmd:await send.sendMessage('因对接方式问题，暂不支持该命令')
+    elif '-permission' in cmd:await send.sendMessage('因对接方式问题，暂不支持该命令')
     else:
         await Bot.FetchTarget.post_message('zhongli-probe', cmd)
         sender = 'QQ|2376834381'
