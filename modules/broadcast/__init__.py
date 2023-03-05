7df24005edc1a17436a04f613e8309ae3ba0cd5e
好耶！ <86039582+cloudw233@users.noreply.github.com>
2023-03-05 20:59:27 +0800

    modified:   modules/broadcast/__init__.py

--- modules/broadcast/__init__.py
+++ modules/broadcast/__init__.py
@@ -1,9 +1,9 @@
 from core.builtins import Bot
 from core.component import on_command
 
-brc = on_command('broadcast', required_superuser=True, base=True, developers='haoye_qwq')
+brc = on_command('broadcast', required_superuser=True, developers='haoye_qwq')
 
 @brc.handle('<text> {向所有群广播消息}')
 async def broadcast(msg: Bot.MessageSession):
     get_str = msg.parsed_msg.get('<text>', False)
-    await Bot.FetchTarget.post_message('broadcast', get_str)
+    await Bot.FetchTarget.post_message('alias', get_str)
