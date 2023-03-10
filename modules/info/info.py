6573ec8448bd030d9612abc2e1c09b5156a4f55a
好耶！ <86039582+cloudw233@users.noreply.github.com>
2023-03-10 19:54:57 +0800

    modified:   modules/info/info.py

--- modules/info/info.py
+++ modules/info/info.py
@@ -1,6 +1,7 @@
 from core.builtins import Bot, Image
 from core.component import on_command
 from core.utils.image_table import image_table_render, ImageTable
+import time
 
 inf = on_command('info')
 
@@ -13,9 +14,6 @@ async def inf_helps(msg: Bot.MessageSession):
         if len(inf) == 0:
             await msg.sendMessage('自定义命令别名列表为空。')
         else:
-            table = ImageTable([[k, inf[k]] for k in inf], ['别名', '命令'])
-            img = await image_table_render(table)
-            if img:
-                await msg.sendMessage(['自定义命令别名列表：', Image(img)])
-            else:
-                await msg.sendMessage(f'自定义命令别名列表：\n' + '\n'.join([f'{k} -> {inf[k]}' for k in inf]))
+                send = await msg.sendMessage(f'[90秒后撤回消息]自定义命令别名列表：\n' + '\n'.join([f'{k} -> {inf[k]}' for k in inf]))
+                time.sleep(90)
+                send.delete
