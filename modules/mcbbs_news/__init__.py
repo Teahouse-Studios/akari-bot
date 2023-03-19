7191511cfe56fccb20b0f002a74aa402d438fbff
多羅狼 <Dorowolf314@gmail.com>
2023-03-19 13:00:32 +0800

    sync

--- modules/mcbbs_news/__init__.py
+++ modules/mcbbs_news/__init__.py
@@ -20,6 +20,6 @@ async def main(msg: Bot.MessageSession):
         lst = []
         for i in res:
             lst += [f'{i["count"]}. [{i["category"]}] {i["title"]} - {i["author"]} @ {i["time"]}\n{i["url"]}']
-        message = '\n'.join(lst) + msg.locale.t('mcbbs_news.message.more') + \
+        message = '\n'.join(lst) + '\n' + msg.locale.t('mcbbs_news.message.more') + \
                   Url('https://www.mcbbs.net/forum-news-1.html').url
     await msg.finish(message)
