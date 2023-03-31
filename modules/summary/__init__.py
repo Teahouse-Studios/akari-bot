677cd791882dbda926d80dd5df19482ff0979d5f
多羅狼 <Dorowolf314@gmail.com>
2023-03-31 12:18:25 +0800

    Update __init__.py

--- modules/summary/__init__.py
+++ modules/summary/__init__.py
@@ -31,7 +31,7 @@ async def _(msg: Bot.MessageSession):
     prev = ''
     while nth < len(texts):
         prompt = f'请总结下列聊天内容。要求语言简练，但必须含有所有要点，以一段话的形式输出。除了聊天记录的摘要以外，不要输出其他任何内容。' \
-                 f'''{f"""同时<ctx_start>与<|ctx_end|>之间记录了聊天内容的上下文，请你同时结合这段上下文和聊天记录，并以这段聊天记录的原语言形式输出。
+                 f'''{f"""同时<ctx_start>与<|ctx_end|>之间记录了聊天内容的上下文，请你同时结合这段上下文和聊天记录，必须以这段聊天记录的原语言输出。
 
 <|ctx_start|>{prev}<|ctx_end|>""" if nth != 0 else ""}'''
         len_prompt = len(prompt)
