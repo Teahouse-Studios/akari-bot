ead7d2287712a139a8040027ac8bee2f70afb412
多羅狼 <Dorowolf314@gmail.com>
2023-04-04 23:09:36 +0800

    i18n calc

--- modules/calc/__init__.py
+++ modules/calc/__init__.py
@@ -9,34 +9,34 @@ from core.exceptions import NoReportException
 from core.logger import Logger
 
 c = module('calc', developers=[
-    'Dianliang233'], desc='安全地计算 Python ast 表达式。')
+    'Dianliang233'], desc='{calc.help}')
 
 
-@c.command('<math_expression>', options_desc={'+': '和/正数：1 + 2 -> 3',
-                                             '-': '差/负数：3 - 1 -> 2',
-                                             '/': '商：6 / 3 -> 2',
-                                             '*': '积：2 * 3 -> 6',
-                                             '**': 'x 的 y 次幂（由于性能问题，指数不得超过 4e+6）：2 ** 3 -> 8',
-                                             '%': '取模：5 % 2 -> 1',
-                                             '==': '等于：1 == 1 -> True',
-                                             '<=': '小于等于：1 <= 2 -> True',
-                                             '>=': '大于等于：1 >= 2 -> False',
-                                             '>>': 'x 右移 y 位（相当于 x / (2 ** y)，y < 10000）：32 >> 5 -> 1',
-                                             '<<': 'x 左移 y 位（相当于 x * (2 ** y)，y < 10000）：1 << 5 -> 32',
-                                             '^': '按位异或：1 ^ 1 -> 0',
-                                             'not': '非：not True -> False',
-                                             'is': 'x 与 y 是同一个对象：1 is 1 -> True',
-                                             'randint(x)': '小于 x 的随机整数：randint(6) -> 5',
-                                             'rand()': '0 与 1 之间的随机浮点数：rand() -> 0.5789015836448923',
-                                             'int()': '转换为整数：int(1.5) -> 1',
-                                             'float()': '转换为浮点数：float(1) -> 1.0',
-                                             'str()': '转换为字符串：str(1) -> "1"',
-                                             'complex()': '转换为复数：complex(1) -> (1 + 0j)',
-                                             'bool()': '转换为布尔值：bool(1) -> True',
-                                             'bin()': '转换为二进制：bin(268) -> 0b100001100',
-                                             'oct()': '转换为八进制：oct(268) ->  0o414',
-                                             'hex()': '转换为十六进制：hex(268) -> 0x10c',
-                                             '更多可用运算符和函数': 'https://bot.teahouse.team/-/340',
+@c.command('<math_expression>', options_desc={'+': '{calc.help.plus}',
+                                             '-': '{calc.help.minus}',
+                                             '/': '{calc.help.multiply}',
+                                             '*': '{calc.help.divide}',
+                                             '**': '{calc.help.power}',
+                                             '%': '{calc.help.modulo}',
+                                             '==': '{calc.help.equal}',
+                                             '<=': '{calc.help.less_equal}',
+                                             '>=': '{calc.help.greater_equal}',
+                                             '>>': '{calc.help.move_right}',
+                                             '<<': '{calc.help.move_left}',
+                                             '^': '{calc.help.xor}',
+                                             'not': '{calc.help.not}',
+                                             'is': '{calc.help.is}',
+                                             'randint(x)': '{calc.help.randint}',
+                                             'rand()': '{calc.help.rand}',
+                                             'int()': '{calc.help.int}',
+                                             'float()': '{calc.help.float}',
+                                             'str()': '{calc.help.str}',
+                                             'complex()': '{calc.help.complex}',
+                                             'bool()': '{calc.help.bool}',
+                                             'bin()': '{calc.help.bin}',
+                                             'oct()': '{calc.help.oct}',
+                                             'hex()': '{calc.help.hex}',
+                                             '{calc.help.more}': 'https://bot.teahouse.team/-/340',
                                              })
 async def _(msg: Bot.MessageSession):
     expr = msg.asDisplay().split(' ', 1)[1]
@@ -47,9 +47,9 @@ async def _(msg: Bot.MessageSession):
             if res[0:6] == 'Result':
                 await msg.finish(f'{(expr)} = {res[7:]}')
             else:
-                await msg.finish(f'表达式无效：{res[7:]}')
+                await msg.finish(msg.locale.t("calc.message.invalid", exp={res[7:]}))
         except subprocess.TimeoutExpired:
-            raise NoReportException('计算超时。')
+            raise NoReportException(msg.locale.t("calc.message.time_out"))
     else:
         try:
             p = await asyncio.create_subprocess_exec('python', os.path.abspath("./modules/calc/calc.py"), expr,
@@ -60,7 +60,7 @@ async def _(msg: Bot.MessageSession):
                 await asyncio.wait_for(p.wait(), timeout=10)
             except asyncio.TimeoutError:
                 p.kill()
-                raise NoReportException('计算超时。')
+                raise NoReportException(msg.locale.t("calc.message.time_out"))
             stdout_data, stderr_data = await p.communicate()
             if p.returncode == 0:
                 res = stdout_data.decode('utf-8')
@@ -68,7 +68,7 @@ async def _(msg: Bot.MessageSession):
                 if res[0:6] == 'Result':
                     await msg.finish(f'{(expr)} = {res[7:]}')
                 else:
-                    await msg.finish(f'表达式无效：{res[7:]}')
+                    await msg.finish(msg.locale.t("calc.message.invalid", exp={res[7:]}))
             else:
                 Logger.error(f'calc.py exited with code {p.returncode}')
                 try:
