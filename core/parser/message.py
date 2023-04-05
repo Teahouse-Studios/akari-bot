71c8fc23ae465bb369724c2fdaecd71bd679f989
yzhh <31803608+OasisAkari@users.noreply.github.com>
2023-04-05 13:41:03 +0800

    Update message.py

--- core/parser/message.py
+++ core/parser/message.py
@@ -295,7 +295,7 @@ async def parser(msg: MessageSession, require_enable_modules: bool = True, prefi
                 except Exception as e:
                     Logger.error(traceback.format_exc())
                     await msg.sendMessage(msg.locale.t('error.prompt.report', err_msg=str(e)) +
-                                          Url(Config('bug_report_url')))
+                                          str(Url(Config('bug_report_url'))))
             return msg
         if running_mention:
             if display.find('小可') != -1:
