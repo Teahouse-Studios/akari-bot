ce5d669ad0195687c8d07a0d63e3549c6d58786b
好耶！ <86039582+cloudw233@users.noreply.github.com>
2023-04-05 09:46:31 +0800

    Update __init__.py

--- modules/zhongli-probe/__init__.py
+++ modules/zhongli-probe/__init__.py
@@ -9,8 +9,7 @@ zlp = on_regex('zhongli-probe', desc='对接钟离挂钩', developers='haoye_qwq
             show_typing=False
             )
 async def zl_probe(send: Bot.MessageSession):
-    msg = send.matched_msg
-#     send_m = '[钟离]\n' + msg['text']
-#     f = await Bot.FetchTarget.fetch_target(f"QQ|Group|{msg['return_to']}")
-#     await f.sendDirectMessage(send_m)
-    await send.sendMessage(str(msg))
+    msg = eval(send.matched_msg[0]+'}')
+    send_m = '[钟离]\n' + msg['text']
+    f = await Bot.FetchTarget.fetch_target(f"QQ|Group|{msg['return_to']}")
+    await f.sendDirectMessage(send_m)
