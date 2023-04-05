70187a2e33a69c1b2783f1614100aa23fcf99cbe
好耶！ <86039582+cloudw233@users.noreply.github.com>
2023-04-05 19:59:30 +0800

    Update info.py

--- modules/info/info.py
+++ modules/info/info.py
@@ -51,7 +51,7 @@ async def __(msg: Bot.MessageSession):
 @inf.handle('url <ServerUrl> {查询任意服务器信息}')
 async def ___(msg: Bot.MessageSession):
     info = await server(msg.parsed_msg.get('<ServerUrl>')[0])
-    send = await msg.sendMessage(info + '[90秒后撤回]')
+    send = await msg.sendMessage(info + '\n[90秒后撤回]')
     await msg.sleep(90)
     await send.delete()
 
@@ -61,7 +61,7 @@ async def ____(msg: Bot.MessageSession):
     name = msg.parsed_msg.get('<name>')[0]
     group_id = msg.target.targetId
     if db.exists(f"{group_id}_{name}"):
-        send = await msg.sendMessage(await server(db.get(f"{group_id}_{name}")) + '\n[90秒后撤回]')
+        send = await msg.sendMessage(await server(str(db.get(f"{group_id}_{name}"))) + '\n[90秒后撤回]')
         await msg.sleep(90)
         await send.delete()
     else:
