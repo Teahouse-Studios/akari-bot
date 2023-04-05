80d695aebce76acd24cb962fb5903d677c5dc133
好耶！ <86039582+cloudw233@users.noreply.github.com>
2023-04-05 13:13:46 +0800

    应该好了？

--- modules/zhongli-probe/__init__.py
+++ modules/zhongli-probe/__init__.py
@@ -1,16 +1,19 @@
 from core.component import on_regex
 from core.builtins import Bot
 import json
+import re
 
 zlp = on_regex('zhongli-probe', desc='对接钟离挂钩', developers='haoye_qwq', required_admin=True)
 
 
-@zlp.handle(pattern=r'^{[\s\S]*}',
+@zlp.handle(pattern=r'^[\s\S]*',
             mode='A',
             show_typing=False
             )
 async def zl_probe(send: Bot.MessageSession):
-    msg = json.loads(send.matched_msg[0].replace('\'', '\"'))
+    msg = send.matched_msg[0].replace('\'', '\"')
+    p = re.compile('(?<!\\\\)\'')
+    msg = p.sub('\"', msg)
     send_m = '[钟离]\n' + msg['text']
     f = await Bot.FetchTarget.fetch_target(msg['return_to'])
     await f.sendDirectMessage(send_m)
