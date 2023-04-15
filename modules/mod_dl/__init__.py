290849577f25eabb48e7f779f39cd4c1e60819fd
z0z0r4 <z0z0r4@outlook.com>
2023-04-16 00:27:19 +0800

    [moddl] cf无key支持、修复 Bug以及调整输出顺序 (#546)
    * feat: use mirror api without curseforge_api_key
    
    * fix: ver_list error
    
    * feat: moddl i18n

--- modules/mod_dl/__init__.py
+++ modules/mod_dl/__init__.py
@@ -9,12 +9,17 @@ from config import Config
 
 mod_dl = module(
     bind_prefix='mod_dl',
-    desc='下载 CurseForge 和 Modrinth 上的 Mod。',
+    desc='{moddl.help.desc}',
     developers=['HornCopper', 'OasisAkari', 'z0z0r4'],
     recommend_modules=['mcmod'],
     alias='moddl')
 
 x_api_key = Config("curseforge_api_key")
+if not x_api_key:
+    # CurseForge API Key 未配置，使用镜像 https://mcim.z0z0r4.top ...(z0z0r4 不想解析网页)
+    MCIM_MODE = True
+else:
+    MCIM_MODE = False
 
 def cn_chk(string: str):
     for word in string:
@@ -22,7 +27,7 @@ def cn_chk(string: str):
             return True
     return False
 
-@mod_dl.handle('<mod_name> [<version>] {通过模组名获取模组下载链接}')
+@mod_dl.handle('<mod_name> [<version>] {{moddl.help}}')
 async def main(msg: Bot.MessageSession):
     mod_name = msg.parsed_msg['<mod_name>']
     ver = msg.parsed_msg.get('<version>', None)
@@ -50,19 +55,26 @@ async def main(msg: Bot.MessageSession):
             return results
     
     async def search_curseforge(name: str, ver: str):
-        headers = {
-            'Accept': 'application/json',
-            'x-api-key': x_api_key
-        }
-        url = f'https://api.curseforge.com/v1/mods/search?gameId=432&searchFilter={name}&sortField=2&sortOrder=desc&pageSize=10&classId=6'
+        if MCIM_MODE:
+            # https://mcim.z0z0r4.top/docs#/Curseforge/curseforge_search_curseforge_search_get
+            url = f'https://mcim.z0z0r4.top/curseforge/search?gameId=432&searchFilter={name}&sortField=2&sortOrder=desc&pageSize=10&classId=6'
+            headers = None
+        else:
+            headers = {
+                'Accept': 'application/json',
+                'x-api-key': x_api_key
+            }
+            url = f'https://api.curseforge.com/v1/mods/search?gameId=432&searchFilter={name}&sortField=2&sortOrder=desc&pageSize=10&classId=6'
+
         if ver:
             url += f'&gameVersion={ver}'
         results = []
         try:
             resp = await get_url(url, 200, fmt="json", timeout=5, attempt=3, headers=headers)
             if resp is not None:
-                if resp["pagination"]["resultCount"] == 0:
-                    return None
+                if not MCIM_MODE: # 没提供 pagination
+                    if resp["pagination"]["resultCount"] == 0:
+                        return None
                 for mod in resp["data"]:
                     results.append(("curseforge", mod["name"], mod["id"], None))
         except Exception:
@@ -76,21 +88,32 @@ async def main(msg: Bot.MessageSession):
             return resp
 
     async def get_curseforge_mod_version_index(modid: str):
-        headers = {
-            'Accept': 'application/json',
-            'x-api-key': x_api_key
-        }
-        url = f'https://api.curseforge.com/v1/mods/{modid}'
+        if MCIM_MODE:
+            # https://mcim.z0z0r4.top/docs#/Curseforge/get_mod_curseforge_mod__modid_slug__get
+            url = f'https://mcim.z0z0r4.top/curseforge/mod/{modid}'
+            headers = None
+        else:
+            headers = {
+                'Accept': 'application/json',
+                'x-api-key': x_api_key
+            }
+            url = f'https://api.curseforge.com/v1/mods/{modid}'
         resp = await get_url(url, 200, fmt="json", timeout=5, attempt=3, headers=headers)
         if resp is not None:
             return resp["data"]['latestFilesIndexes']
         
     async def get_curseforge_mod_file(modid: str, ver: str):
-        headers = {
-            'Accept': 'application/json',
-            'x-api-key': x_api_key
-        }
-        url = f'https://api.curseforge.com/v1/mods/{modid}/files?gameVersion={ver}'
+        if MCIM_MODE:
+            url = f'https://mcim.z0z0r4.top/curseforge/mod/{modid}/files?gameVersion={ver}'
+            headers = None
+        else:
+            headers = {
+                'Accept': 'application/json',
+                'x-api-key': x_api_key
+            }
+            url = f'https://api.curseforge.com/v1/mods/{modid}/files?gameVersion={ver}'
+        
+            
         try:
             resp = await get_url(url, 200, fmt="json", timeout=5, attempt=3, headers=headers)
             if resp is not None:
@@ -102,73 +125,77 @@ async def main(msg: Bot.MessageSession):
     result = await asyncio.gather(*(search_modrinth(mod_name, ver), search_curseforge(mod_name, ver)))
     cache_result = []
     if result[0] is None and result[1] is None:
-        await msg.finish("搜索失败，无结果。")
+        await msg.finish(msg.locale.t("moddl.message.search_failed"))
     else:
         # 合并搜索结果
         reply_text, count = [], 0
+
+        # 先显示 CurseForge 的结果
+        if result[1] is None:
+            reply_text.append(msg.locale.t("moddl.message.curseforge.nothing"))
+        else:
+            reply_text.append(msg.locale.t("moddl.message.curseforge.found"))
+            for mod in result[1]:
+                count += 1
+                reply_text.append(f"{count}. {mod[1]}")
+                cache_result.append(mod)
+
         if result[0] is None:
-            reply_text.append("Modrinth 结果：无")
-        reply_text.append("Modrinth 结果：")
+            reply_text.append(msg.locale.t("moddl.message.modrinth.nothing"))
+        reply_text.append(msg.locale.t("moddl.message.modrinth.found"))
         for mod in result[0]:
             count += 1
             reply_text.append(f"{count}. {mod[1]}")
             cache_result.append(mod)
 
-        if result[1] is None:
-            reply_text.append("CurseForge 结果：无")
-        else:
-            reply_text.append("CurseForge 结果：")
-            for mod in result[1]:
-                count += 1
-                reply_text.append(f"{count}. {mod[1]}")
-                cache_result.append(mod)
-    
-        reply = await msg.waitReply('\n'.join(reply_text) + '\n请回复编号来选择mod。')
+        reply = await msg.waitReply('\n'.join(reply_text) + '\n' + msg.locale.t("moddl.message.choose_mod"))
         replied = reply.asDisplay(text_only=True)
 
         # 查找 Mod
         if replied.isdigit():
             replied = int(replied)
             if replied > len(cache_result):
-                return await msg.finish('编号超出范围。')
+                return await msg.finish(msg.locale.locale("moddl.message.out_of_index"))
             else:
                 mod_info = cache_result[replied - 1]
         else:
-            return await msg.finish('无效的编号，必须为纯数字。')
+            return await msg.finish(msg.locale.locale("moddl.message.illegal_index"))
         
         if mod_info[0] == "modrinth": # modrinth mod
             if ver is None:
-                reply2 = await msg.waitReply('此mod拥有如下版本：\n' + '\n'.join(mod_info[3]) + '\n请回复版本号来选择版本。')
+                reply2 = await msg.waitReply(f'{msg.locale.t("moddl.message.reply_version_1")}\n'
+                                              + "\n".join(mod_info[3]) 
+                                              + f'\n{msg.locale.t("moddl.message.reply_version_2")}')
                 replied2 = reply2.asDisplay(text_only=True)
                 if replied2 in mod_info[3]:
                     version_info = await get_modrinth_project_version(mod_info[2], replied2)
                     if version_info is not None:
-                        await msg.finish(f'{" ".join(version_info["loaders"])}\n下载链接：{version_info["files"][0]["url"]}\n文件名：{version_info["files"][0]["filename"]}')
+                        await msg.finish(f'{" ".join(version_info["loaders"])}\n{msg.locale.t("moddl.message.download_url")}{version_info["files"][0]["url"]}\n{msg.locale.t("moddl.message.filename")}{version_info["files"][0]["filename"]}')
             elif ver not in mod_info[3]:
-                await msg.finish("未找到指定版本。")
+                await msg.finish(msg.locale.t("moddl.message.no_such_version"))
             elif ver in mod_info[3]:
                 version_info = await get_modrinth_project_version(mod_info[2], ver)
                 if version_info is not None:
-                    await msg.finish(f'{" ".join(version_info["loaders"])}\n下载链接：{version_info["files"][0]["url"]}\n文件名：{version_info["files"][0]["filename"]}')
+                    await msg.finish(f'{" ".join(version_info["loaders"])}\n{msg.locale.t("moddl.message.download_url")}{version_info["files"][0]["url"]}\n{msg.locale.t("moddl.message.filename")}{version_info["files"][0]["filename"]}')
         else: # curseforge mod
-            version_index = await get_curseforge_mod_version_index(mod_info[2])
+            version_index, ver_list = await get_curseforge_mod_version_index(mod_info[2]), []
+            for version in version_index:
+                if version["gameVersion"] not in ver_list:
+                    ver_list.append(version["gameVersion"])
             if version_index is not None:
                 if ver is None:
-                    reply_text = []
-                    for version in version_index:
-                        reply_text.append(version["gameVersion"])
-                    reply2 = await msg.waitReply('此mod拥有如下版本：\n' + 
-                                                 '\n'.join(reply_text) + 
-                                                 '\n请回复版本号来选择版本。')
+                    reply2 = await msg.waitReply(f'{msg.locale.t("moddl.message.ask_version_1")}\n' + 
+                                                 '\n'.join(ver_list) + 
+                                                 f'\n{msg.locale.t("moddl.message.ask_version_2")}')
                     ver = reply2.asDisplay(text_only=True)
-                elif ver not in version_index:
-                    await msg.finish("未找到指定版本。")
+                elif ver not in ver_list:
+                    await msg.finish(msg.locale.t("moddl.message.no_such_version"))
 
-                if ver in version_index:
+                if ver in ver_list:
                     file_info = await get_curseforge_mod_file(mod_info[2], ver)
                     if file_info is not None:
                         await msg.finish(f'{" ".join(file_info["gameVersions"])} \
-                                         \n下载链接：{file_info["downloadUrl"]} \
-                                         \n文件名：{file_info["fileName"]}')
+                                         \n{msg.locale.t("moddl.message.download_url")}{file_info["downloadUrl"]} \
+                                         \n{msg.locale.t("moddl.message.filename")}{file_info["fileName"]}')
                 else:
-                    await msg.finish("未找到指定版本。")
+                    await msg.finish(msg.locale.t("moddl.message.no_such_version"))
