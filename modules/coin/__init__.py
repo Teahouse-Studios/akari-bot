5068d270663ffb382254c3b420b8e099ae2855ef
多羅狼 <Dorowolf314@gmail.com>
2023-04-16 09:39:50 +0800

    i18n coin

--- modules/coin/__init__.py
+++ modules/coin/__init__.py
@@ -8,14 +8,14 @@ FACE_UP_RATE = 4994  # n/10000
 FACE_DOWN_RATE = 4994
 COIN_DROP_PLACES = ["地上", "桌子上", "桌子底下", "门口", "窗户外", "月球"]  # 硬币可能掉落的位置
 
-coin = module('coin', developers=['Light-Beacon'], desc='抛n枚硬币')
+coin = module('coin', developers=['Light-Beacon'], desc='{coin.help.desc}')
 
 
-@coin.command('<amount> {抛n枚硬币}')
+@coin.command('<amount> {{coin.help}}')
 async def _(msg: MessageSession):
     amount = msg.parsed_msg.get('<amount>', '1')
     if not amount.isdigit():
-        await msg.finish('发生错误：无效的硬币个数：' + amount)
+        await msg.finish(msg.locale.t('coin.message.error.amount') + amount)
     else:
         await msg.finish(await flipCoins(int(amount)))
 
@@ -30,17 +30,17 @@ async def _(message: MessageSession):
         try:
             count = Zh2Int(count)
         except ValueError as ex:
-            await message.finish("发生错误：" + str(ex))
+            await message.finish(msg.locale.t("error") + str(ex))
     await message.finish(await flipCoins(count))
 
 
 async def flipCoins(count: int):
     if count > MAX_COIN_NUM:
-        return f"发生错误：你最多只能抛 {MAX_COIN_NUM} 个硬币"
+        return msg.locale.t("coin.message.error.max", max=MAX_COIN_NUM)
     if count <= 0:
-        return f"发生错误：你抛了个空气，什么也没发生..."
+        return msg.locale.t("coin.message.error.nocoin")
     if FACE_UP_RATE + FACE_DOWN_RATE > 10000 or FACE_UP_RATE < 0 or FACE_DOWN_RATE < 0:
-        raise OverflowError("发生错误：硬币概率错误")
+        raise OverflowError(msg.locale.t("coin.message.error.rate"))
     faceUp = 0
     faceDown = 0
     stand = 0
@@ -52,30 +52,30 @@ async def flipCoins(count: int):
             faceDown += 1
         else:
             stand += 1
-    head = f"你抛了 {count} 枚硬币，"
+    head = msg.locale.t("coin.message")
     if count == 1:
         drop_place = COIN_DROP_PLACES[secrets.randbelow(len(COIN_DROP_PLACES))]
-        head += f"它掉到了{drop_place}...\n"
+        head += msg.locale.t("coin.message.drop_place", drop_place=drop_place) + '\n'
         if faceUp:
-            return head + "...是正面！"
+            return head + msg.locale.t("coin.message.head")
         elif faceDown:
-            return head + "...是反面！"
+            return head + msg.locale.t("coin.message.tail")
         else:
-            return head + "...它立起来了！"
+            return head + msg.locale.t("coin.message.stand")
     else:
         if not (stand or faceDown):
-            return head + "它们...\n...全是正面！"
+            return head + msg.locale.t("coin.message.head.all")
         if not (stand or faceUp):
-            return head + "它们...\n...全是反面！"
+            return head + msg.locale.t("coin.message.tail.all")
         if not (faceUp or faceDown):
-            return head + "它们...\n...全都立起来了？！"
-        output = head + "其中...\n有"
+            return head + msg.locale.t("coin.message.stand.all")
+        output = head + msg.locale.t("coin.message.mix")
         if faceUp:
-            output += f" {faceUp} 枚是正面，"
+            output += msg.locale.t("coin.message.mix.head", head=faceUp)
         if faceDown:
-            output += f"{faceDown} 枚是反面"
+            output += msg.locale.t("coin.message.mix.tail", tail=faceDown)
         if stand:
-            output += f"...还有 {stand} 枚立起来了！"
+            output += msg.locale.t("coin.message.mix.stand", stand=stand)
         else:
-            output += f"。"
+            output += msg.locale.t("coin.message.mix.end")
         return output
