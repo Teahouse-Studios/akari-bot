367c369694c93846c97d737ee3ada1783ac473ab
yzhh <31803608+OasisAkari@users.noreply.github.com>
2023-04-16 15:44:17 +0800

    ELEGANT

--- core/builtins/__init__.py
+++ core/builtins/__init__.py
@@ -1,4 +1,6 @@
-from core.types.message import FetchTarget
+from typing import Union
+
+from core.types.message import FetchTarget, FetchedSession
 from .message import *
 from .message.chain import *
 from .message.internal import *
@@ -7,6 +9,16 @@ from .temp import *
 from .utils import *
 
 
+
 class Bot:
     MessageSession = MessageSession
     FetchTarget = FetchTarget
+
+    async def sendMessage(self, target: Union[FetchedSession, str], msg: Union[MessageChain, list],
+                          disable_secret_check=False,
+                          allow_split_image=True):
+        if isinstance(target, str):
+            target = FetchTarget.fetch_target(target)
+        if isinstance(msg, list):
+            msg = MessageChain(msg)
+        await target.sendDirectMessage(msg, disable_secret_check, allow_split_image)
