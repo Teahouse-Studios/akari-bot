4b9f4dd1394ee6c4bee21116756af8d18a7f64b3
yzhh <31803608+OasisAkari@users.noreply.github.com>
2023-04-17 16:55:55 +0800

    Update __init__.py

--- core/builtins/__init__.py
+++ core/builtins/__init__.py
@@ -1,6 +1,7 @@
-from typing import Union
+from typing import Union, List
 
 from core.types.message import FetchTarget, FetchedSession
+from database import BotDBUtil
 from .message import *
 from .message.chain import *
 from .message.internal import *
@@ -19,7 +20,23 @@ class Bot:
                           disable_secret_check=False,
                           allow_split_image=True):
         if isinstance(target, str):
-            target = FetchTarget.fetch_target(target)
+            target = Bot.FetchTarget.fetch_target(target)
+            if not target:
+                raise ValueError("Target not found")
         if isinstance(msg, list):
             msg = MessageChain(msg)
         await target.sendDirectMessage(msg, disable_secret_check, allow_split_image)
+
+    @staticmethod
+    async def fetch_target(target: str):
+        return Bot.FetchTarget.fetch_target(target)
+
+    @staticmethod
+    async def get_enabled_this_module(module: str) -> List[FetchedSession]:
+        lst = BotDBUtil.TargetInfo.get_enabled_this(module)
+        fetched = []
+        for x in lst:
+            x = Bot.FetchTarget.fetch_target(x)
+            if isinstance(x, FetchedSession):
+                fetched.append(x)
+        return fetched
