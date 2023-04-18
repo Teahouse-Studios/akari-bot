ce81833a480554e6e05e4f1cfd561f873f43e531
多羅狼 <Dorowolf314@gmail.com>
2023-04-18 14:12:07 +0800

    Update __init__.py

--- modules/exchange_rate/__init__.py
+++ modules/exchange_rate/__init__.py
@@ -13,7 +13,7 @@ api_key = 'd31697e581d5c35b038c625c'
 async def _(msg: Bot.MessageSession):
     base_currency = msg.parsed_msg['<base>'].upper()
     target_currency = msg.parsed_msg['<target>'].upper()
-
+"""
     url = f'https://v6.exchangerate-api.com/v6/{api_key}/codes'
     response = requests.get(url)
     if response.status_code == 200:
@@ -28,7 +28,7 @@ async def _(msg: Bot.MessageSession):
                 await msg.finish(f"发生错误：无效的货币单位：{' '.join(unsupported_currencies)}")
     else:
             raise NoReportException(f"{response.text}")
-
+"""
     amount = None
     while amount is None:
         try:
