80194e61bf49b3aea82237f61fa666c066b6e4b8
yzhh <31803608+OasisAkari@users.noreply.github.com>
2023-04-21 18:11:04 +0800

    fix fallback loop

--- core/utils/i18n.py
+++ core/utils/i18n.py
@@ -95,7 +95,8 @@ class Locale:
                 if string is not None:
                     return string  # 2. 如果在 fallback 语言中本地化字符串存在，直接返回
         if fallback_failed_prompt:
-            return f'{{{key}}}' + self.t("i18n.prompt.fallback.failed", url=Config('bug_report_url'))
+            return f'{{{key}}}' + self.t("i18n.prompt.fallback.failed", url=Config('bug_report_url'),
+                                         fallback_failed_prompt=False)
         else:
             return key
         # 3. 如果在 fallback 语言中本地化字符串不存在，返回 key
