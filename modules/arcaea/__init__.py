5411bb4c330edafb92bdfd33b5e5db36ac8e6dc4
多羅狼 <Dorowolf314@gmail.com>
2023-04-28 21:31:37 +0800

    Update __init__.py

--- modules/arcaea/__init__.py
+++ modules/arcaea/__init__.py
@@ -8,11 +8,12 @@ from core.component import module
 
 webrender = Config('web_render')
 arc = module('arcaea', developers=['OasisAkari'], desc='{arcaea.help.desc}',
-             alias={'b30': 'arcaea', 'a': 'arcaea', 'arc': 'arcaea'})
+             alias={'b30': 'arcaea b30', 'a': 'arcaea', 'arc': 'arcaea'})
 assets_path = os.path.abspath('./assets/')
 
 
 @arc.handle()
+@arc.handle('<no_one_cares>')
 async def _(msg: Bot.MessageSession):
     await msg.finish([msg.locale.t("arcaea.message.sb616"), Image(assets_path + '/noc.jpg'), Image(assets_path + '/aof.jpg')])
 
