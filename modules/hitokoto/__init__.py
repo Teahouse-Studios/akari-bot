e8a16ab181203bd24a14c4f7969622c94ed517f1
多羅狼 <Dorowolf314@gmail.com>
2023-05-04 20:07:38 +0800

    Update __init__.py

--- modules/hitokoto/__init__.py
+++ modules/hitokoto/__init__.py
@@ -4,7 +4,7 @@ from core.builtins.message import MessageSession
 from core.dirty_check import check
 from core.utils.http import get_url
 
-hitokoto = module('hitokoto', developers=['bugungu'], desc='{hitokoto.help.desc}')
+hitokoto = module('hitokoto', developers=['bugungu', 'DoroWolf'], desc='{hitokoto.help.desc}')
 
 @hitokoto.handle('{{hitokoto.help}}')
 
@@ -12,6 +12,6 @@ async def _(msg: Bot.MessageSession):
     url = 'https://v1.hitokoto.cn/?encode=json'
     data = await get_url(url, 200, fmt = 'json')
     from_who = data["from_who"] or ""
-    types = msg.locale.t('hitokoto.message.type') + msg.locale.t('hitokoto.message.type.' + responce['type'])
+    types = msg.locale.t('hitokoto.message.type') + msg.locale.t('hitokoto.message.type.' + data['type'])
     hitokoto_url = 'https://hitokoto.cn?id={data["id"]}'
     await msg.sendMessage(f'''{data["hitokoto"]}\n——{from_who}「{data["from"]}」\n{types}\n{hitokoto_url}''')
