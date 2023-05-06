e9339e1be8ee0672e24a8df6b4b90301642f20bd
SilianZ <silian_zheng@outlook.com>
2023-05-06 23:18:29 +0800

    更新代码

--- modules/netease_cloud_music/__init__.py
+++ modules/netease_cloud_music/__init__.py
@@ -8,7 +8,7 @@ import ujson as json
 
 netease_cloud_music = module(bind_prefix = 'music', developers = ['bugungu'])
 
-@netease_cloud_music.handle('search <value> {{ncmusic.help_doc.search}}', 
+@netease_cloud_music.handle('search <value> {{ncmusic.msg.help.search}}')
 
 async def search(msg: Bot.MessageSession):
     api_address = Config('netease_cloud_music_api')
@@ -16,22 +16,22 @@ async def search(msg: Bot.MessageSession):
         api_address += 'search?limit=10&keywords=' + msg.parsed_msg['<value>']
         result = await get_url(api_address, 200, fmt = 'text', request_private_ip = True)
         result_json = json.loads(result)
-        send_msg = msg.locale.t('ncmusic.search_result') + '\n'
+        send_msg = msg.locale.t('ncmusic.msg.search_result') + '\n'
         cnt = 1
         for k in result_json['result']['songs']:
             #歌名（如果有翻译则加上翻译）
             send_msg += str(cnt) + '. ' + k['name']
             if 'transNames' in k:
-                send_msg += msg.locale.t('ncmusic.character.(')
                 time = len(k['transNames'])
+                value_trans = ''
                 for i in range(time):
                     if i == time - 1:
-                        send_msg += k['transNames'][i]
+                        value_trans += k['transNames'][i]
                     else:
-                        send_msg += k['transNames'][i] + ' / '
-                send_msg += msg.locale.t('ncmusic.character.)')
+                        value_trans += k['transNames'][i] + ' / '
+                send_msg += msg.locale.t('ncmusic.msg.character', value = value_trans)
             #歌手名
-            send_msg += ' ' + msg.locale.t('ncmusic.come_from.character') + ' '
+            send_msg += ' ' + msg.locale.t('ncmusic.msg.come_from') + ' '
             time = len(k['artists'])
             for i in range(time):
                 if i == time - 1:
@@ -39,28 +39,28 @@ async def search(msg: Bot.MessageSession):
                 else:
                     send_msg += k['artists'][i]['name'] + ' & '
             #专辑名（如果有翻译则加上翻译）
-            send_msg += ' ' + msg.locale.t('ncmusic.come_from.character') + ' ' + k['album']['name']
+            send_msg += ' ' + msg.locale.t('ncmusic.msg.come_from') + ' ' + k['album']['name']
             if 'transNames' in k['album']:
-                send_msg += msg.locale.t('ncmusic.character.(')
                 time = len(k['album']['transNames'])
+                value_trans = ''
                 for i in range(time):
                     if i == time - 1:
-                        send_msg += k['album']['transNames'][i]
+                        value_trans += k['album']['transNames'][i]
                     else:
-                        send_msg += k['album']['transNames'][i] + ' / '
-                send_msg += msg.locale.t('ncmusic.character.)')
+                        value_trans += k['album']['transNames'][i] + ' / '
+                send_msg += msg.locale.t('ncmusic.msg.character', value = value_trans)
             #歌曲号
-            send_msg += msg.locale.t('ncmusic.character.(') + str(k['id']) + msg.locale.t('ncmusic.character.)')
+            send_msg += msg.locale.t('ncmusic.msg.character', value = str(k['id']))
             send_msg += '\n'
             cnt += 1
-        send_msg += '\n' + msg.locale.t('ncmusic.message.delete')
+        send_msg += '\n' + msg.locale.t('ncmusic.msg.recall')
         img_path = await msgchain2image([Plain(send_msg)])
         send = await msg.sendMessage(Image(img_path))
         await msg.sleep(90)
         await send.delete()
         await msg.finish()
 
-@netease_cloud_music.handle('info <ids> {{ncmusic.help_doc.info}}', 
+@netease_cloud_music.handle('info <ids> {{ncmusic.msg.help.info}}', 
                             required_admin = False, required_superuser = False,
                             available_for = '*')
 
@@ -75,15 +75,13 @@ async def info(msg: Bot.MessageSession):
             send_msg.append(Image(k['al']['picUrl']))
             send_msg_plain = ''
             #歌名
-            send_msg_plain += msg.locale.t('ncmusic.info.name') + k['name'] + \
-                              msg.locale.t('ncmusic.character.(') + \
-                              str(k['id']) + msg.locale.t('ncmusic.character.)') + '\n'
+            send_msg_plain += msg.locale.t('ncmusic.msg.name') + k['name'] + \
+                              msg.locale.t('ncmusic.msg.character', value = str(k['id'])) + '\n'
             #专辑名
-            send_msg_plain += msg.locale.t('ncmusic.info.album') + k['al']['name'] + \
-                              msg.locale.t('ncmusic.character.(') + str(k['al']['id']) + \
-                              msg.locale.t('ncmusic.character.)') + '\n'
+            send_msg_plain += msg.locale.t('ncmusic.msg.album') + k['al']['name'] + \
+                              msg.locale.t('ncmusic.msg.character', value = str(k['al']['id'])) + '\n'
             #歌手
-            send_msg_plain += msg.locale.t('ncmusic.info.artists')
+            send_msg_plain += msg.locale.t('ncmusic.msg.artists')
             time = len(k['ar'])
             for i in range(time):
                 if i == time - 1:
@@ -93,14 +91,14 @@ async def info(msg: Bot.MessageSession):
             send_msg_plain += '\n'
             #歌曲详情页
             song_page = 'https://music.163.com/#/song?id=' + str(k['id'])
-            send_msg_plain += msg.locale.t('ncmusic.info.song_page') + song_page + '\n'
+            send_msg_plain += msg.locale.t('ncmusic.msg.song_page') + song_page + '\n'
             #歌曲链接
             url = api_address + 'song/url?id=' + str(k['id'])
             song = await get_url(url, 200, fmt = 'text', request_private_ip = True)
             song_url = json.loads(song)
-            send_msg_plain += msg.locale.t('ncmusic.info.song_url') + song_url['data'][0]['url'] + '\n'
+            send_msg_plain += msg.locale.t('ncmusic.msg.song_url') + song_url['data'][0]['url'] + '\n'
             send_msg.append(Plain(send_msg_plain))
-        send_msg.append(Plain('\n' + msg.locale.t('ncmusic.message.delete')))
+        send_msg.append(Plain('\n' + msg.locale.t('ncmusic.msg.recall')))
         send = await msg.sendMessage(send_msg)
         await msg.sleep(90)
         await send.delete()
