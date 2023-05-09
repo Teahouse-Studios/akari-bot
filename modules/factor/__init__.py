6ff3844d727ee9df246a58d0b571c79765668503
多羅狼 <Dorowolf314@gmail.com>
2023-05-09 22:51:07 +0800

    Update __init__.py

--- modules/factor/__init__.py
+++ modules/factor/__init__.py
@@ -1,22 +1,25 @@
 from core.builtins import Bot
 from core.component import module
 
-f = module('factor', developers=[
+factor = module('factor', developers=[
     'DoroWolf'], desc='{factor.help.desc}', required_superuser=True)
 
-@f.handle('prime <number> {{factor.help.prime}}')
+@factor.handle('prime <number> {{factor.prime.help}}')
 async def prime(msg: Bot.MessageSession):
     number = int(msg.parsed_msg.get('<number>'))
-    n=number
-    i=2
-    primes_list=[]
-    if number<=1:
-        await msg.finish('{factor.message.prime.invalid}')
-    while i<=n:
-        if n%i==0:
-            list.append(str(i))
-            n=n//i
+    n = number
+    i = 2
+    primes_list = []
+    if number <= 1:
+        await msg.finish(msg.locale.t('factor.prime.message.error'))
+    while i <= n:
+        if n % i == 0:
+            primes_list.append(str(i))
+            n = n // i
         else:
-            i+=1
+            i += 1
     prime="*".join(primes_list)
-    await msg.finish(f"{number}={prime}")
+    if len(primes_list) == 1:
+        await msg.finish(msg.locale.t('factor.prime.message.is_prime'))
+    else:
+        await msg.finish(f"{number}={prime}")
