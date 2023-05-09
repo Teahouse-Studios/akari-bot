107165192fba3d6ccbeb2624deb3f49b028878b1
多羅狼 <Dorowolf314@gmail.com>
2023-05-10 00:24:10 +0800

    fix?

--- modules/factor/__init__.py
+++ modules/factor/__init__.py
@@ -16,7 +16,7 @@ async def prime(msg: Bot.MessageSession):
     if number <= 1:
         await msg.finish(msg.locale.t('factor.prime.message.error'))
     while i <= n:
-        if time.time() - start_time >= 10:
+        if running_time >= 10:
             await msg.finish(msg.locale.t('factor.message.time_out'))
         if n % i:
             i += 1
@@ -24,11 +24,13 @@ async def prime(msg: Bot.MessageSession):
             n //= i
             primes_list.append(str(i))
     prime="*".join(primes_list)
+    end_time = time.time()
+    running_time = end_time - start_time
     if len(primes_list) == 1:
         result = msg.locale.t("factor.prime.message.is_prime", num=number)
     else:
         result = f"{number} = {prime}"
     checkpermisson = msg.checkSuperUser()
     if checkpermisson:
-        result += '\n' + msg.locale.t("factor.message.running_time", time=f"{time.time() - start_time:.2f}")
+        result += '\n' + msg.locale.t("factor.message.running_time", time=f"{running_time:.2f}")
     await msg.finish(result)
