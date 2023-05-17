7eacdf17ae53eb68f6e96fc2b58d2f8877641b85
yzhh <31803608+OasisAkari@users.noreply.github.com>
2023-05-17 17:30:26 +0800

    zzz

--- modules/bugtracker/__init__.py
+++ modules/bugtracker/__init__.py
@@ -1,8 +1,8 @@
 import re
 
-from core.builtins import Bot
+from core.builtins import Bot, Image
 from core.component import module
-from .bugtracker import bugtracker_get
+from .bugtracker import bugtracker_get, make_screenshot
 
 bug = module('bug', alias='b', developers=['OasisAkari'])
 
@@ -14,7 +14,11 @@ async def bugtracker(msg: Bot.MessageSession):
         q = re.match(r'(.*-.*)', mojira_id)
         if q:
             result = await bugtracker_get(msg, q.group(1))
-            await msg.finish(result)
+            await msg.sendMessage(result[0])
+            if result[1] is not None:
+                screenshot = await make_screenshot(result[1])
+                if screenshot:
+                    await msg.sendMessage(Image(screenshot))
 
 
 @bug.regex(pattern=r'^\!(?:bug |)(.*)-(.*)', mode='M')
@@ -22,7 +26,11 @@ async def regex_bugtracker(msg: Bot.MessageSession):
     matched_msg = msg.matched_msg
     if len(matched_msg.group(1)) < 10 and len(matched_msg.group(2)) < 10:
         result = await bugtracker_get(msg, matched_msg.group(1) + '-' + matched_msg.group(2))
-        await msg.finish(result)
+        await msg.sendMessage(result[0])
+        if result[1] is not None:
+            screenshot = await make_screenshot(result[1])
+            if screenshot:
+                await msg.sendMessage(Image(screenshot))
 
 
 @bug.regex(re.compile(r'https://bugs\.mojang\.com/(?:browse/(.*?-\d*)|projects/.*?/issues/(.*?-\d*))'), mode='A')
@@ -31,6 +39,11 @@ async def _(msg: Bot.MessageSession):
         for title in msg.matched_msg:
             for t in title:
                 if t != '':
-                    await msg.sendMessage(await bugtracker_get(msg, t.split('?')[0], nolink=True))
+                    get_ = await bugtracker_get(msg, t.split('?')[0], nolink=True)
+                    await msg.sendMessage(get_[0])
+                    if get_[1] is not None:
+                        screenshot = await make_screenshot(get_[1])
+                        if screenshot:
+                            await msg.sendMessage(Image(screenshot))
 
     await bgtask(msg)
