42c129684e82bd05f50a4bb49b233ac2bea310f6
多羅狼 <Dorowolf314@gmail.com>
2023-05-24 22:30:47 +0800

    fx

--- modules/maimai/__init__.py
+++ modules/maimai/__init__.py
@@ -151,7 +151,7 @@ async def _(msg: Bot.MessageSession):
         if username is None:
             await msg.finish(msg.locale.t("maimai.message.no_username"))
         payload = {'username': username, 'b50': True}
-        await session.sendMessage(msg.locale.t("maimai.message.b50.waiting"))
+    await msg.sendMessage(msg.locale.t("maimai.message.b50.waiting"))
     img, success = await generate50(payload)
     if success == 400:
         await msg.finish(msg.locale.t("maimai.message.not_found"))
