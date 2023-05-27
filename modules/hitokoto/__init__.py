ecd74ba8684d8cacf6f4cce32b5f77dd1d3fe7a4
多羅狼 <Dorowolf314@gmail.com>
2023-05-27 18:43:31 +0800

    fix htkt

--- modules/hitokoto/__init__.py
+++ modules/hitokoto/__init__.py
@@ -8,19 +8,21 @@ hitokoto_types = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"]
 
 hitokoto = module('hitokoto', developers=['bugungu', 'DoroWolf'], desc='{hitokoto.help.desc}', alias=['htkt'], support_languages=['zh_cn'])
 
-@hitokoto.handle()
 @hitokoto.handle('[<type>] {{hitokoto.help.type}}')
 
 async def _(msg: Bot.MessageSession):
-    set_type = msg.parsed_msg.get('<type>')
+    try:
+        set_type = msg.parsed_msg.get('<type>')
+    except AttributeError:
+        set_type = None
     url = 'https://v1.hitokoto.cn/?'
-    if nset_type is not None:
+    if set_type is not None:
         if set_type not in hitokoto_types:
             await msg.finish(msg.locale.t('hitokoto.message.error.type'))
         else:
             url += "c=" + set_type
     data = await get_url(url, 200, fmt = 'json')
     from_who = data["from_who"] or ""
-    type = msg.locale.t('hitokoto.message.type') + msg.locale.t('hitokoto.message.type.' + data['type'])
+    _type = msg.locale.t('hitokoto.message.type') + msg.locale.t('hitokoto.message.type.' + data['type'])
     link = 'https://hitokoto.cn?id='
-    await msg.finish(f'''{data["hitokoto"]}\n——{from_who}「{data["from"]}」\n{type}\n{link}{data["id"]}''')
+    await msg.finish(f'''{data["hitokoto"]}\n——{from_who}「{data["from"]}」\n{_type}\n{link}{data["id"]}''')
