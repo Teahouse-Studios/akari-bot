ad6e99cedffbce64c303004c2a552188b7a7e1e1
好耶！ <86039582+cloudw233@users.noreply.github.com>
2023-06-06 21:06:50 +0800

    ¿

--- modules/embed_test/__init__.py
+++ modules/embed_test/__init__.py
@@ -1,7 +1,15 @@
 import datetime
+import traceback
 
-from core.builtins import Bot, Embed, Image, EmbedField
+from core.builtins import Bot, Embed, Image, EmbedField, Plain
 from core.component import on_command
+from core.exceptions import InvalidHelpDocTypeError
+from core.extra import pir
+from core.loader import ModulesManager
+from core.parser.command import CommandParser
+from core.types import Command
+from core.utils.image_table import ImageTable, image_table_render
+from database import BotDBUtil
 
 t = on_command('embed_test', required_superuser=True)
 
@@ -16,3 +24,10 @@ async def _(session: Bot.MessageSession):
                                     image=Image('https://avatars.githubusercontent.com/u/68471503?s=200&v=4'),
                                     fields=[EmbedField('oaoa', 'aaaaa', inline=True),
                                             EmbedField('oaoa', 'aaaaa', inline=True)]))
+
+
+@t.handle('new_help {测试罢了}')
+async def __(msg: Bot.MessageSession):
+    module_list = ModulesManager.return_modules_list_as_dict(
+        targetFrom=msg.target.targetFrom)
+    msg.sendMessage(module_list)
