12f22cc8a912370cdc9171884ea2ee2a61f3a702
好耶！ <86039582+cloudw233@users.noreply.github.com>
2023-06-06 22:49:12 +0800

    Update ImageProcess.py

--- core/extra/ImageProcess.py
+++ core/extra/ImageProcess.py
@@ -10,7 +10,7 @@ def pir(text: str, line_length: int = 28):
     image_size = draw.textsize(text, font=font)  # 得到文本占用像素大小
     image = Image.new("RGB", image_size, 'white')  # 创建等大的图像
     draw = ImageDraw.Draw(image)  # 建立一个绘图的对象
-    draw.text((0, 0), fill(text, line_length*2), font=font, fill="black")  # 在图像上打印文字
+    draw.text((0, 0), fill(text, line_length), font=font, fill="black")  # 在图像上打印文字
     return image
 
 
