05664464cb875065615a7e679510e23f51394b92
好耶！ <86039582+cloudw233@users.noreply.github.com>
2023-06-07 16:29:17 +0800

    Update __init__.py

--- modules/hash/__init__.py
+++ modules/hash/__init__.py
@@ -8,8 +8,8 @@ hsh = on_command('hash', desc='生成对应字符串的哈希值', developers='h
 @hsh.handle('<algorithm> <string>')
 async def _(msg: Bot.MessageSession):
     try:
-        hash_ = hashlib.new(msg.parsed_msg['algorithm'], msg.parsed_msg['<string>'])
-        await msg.sendMessage(f"该字符串的 {msg.parsed_msg['algorithm']} 哈希值为:{hash_.hexdigest()}")
+        hash_ = hashlib.new(msg.parsed_msg['<algorithm>'], msg.parsed_msg['<string>'])
+        await msg.sendMessage(f"该字符串的 {msg.parsed_msg['<algorithm>']} 哈希值为:{hash_.hexdigest()}")
 
     except ValueError:
         await msg.sendMessage(f"不支持该\"{msg.parsed_msg['<algorithm>']}\"算法")
