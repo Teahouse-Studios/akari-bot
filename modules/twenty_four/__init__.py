1c5c72801ace5f965bc7ef51744e82161becefb4
多羅狼 <Dorowolf314@gmail.com>
2023-06-08 00:19:25 +0800

    #646

--- modules/twenty_four/__init__.py
+++ modules/twenty_four/__init__.py
@@ -92,16 +92,16 @@ play_state = {}
 
 @tf.command('{{twenty_four.help}}')
 async def _(msg: Bot.MessageSession):
-    if msg.target.targetId in play_state and play_state[msg.target.targetId]['24']['active']: 
+    if msg.target.targetId in play_state and play_state[msg.target.targetId]['active']: 
         await msg.finish(msg.locale.t('twenty_four.message.running'))
-    play_state.update({msg.target.targetId: {'24': {'active': True}}})
+    play_state.update({msg.target.targetId: {'game': '24', 'active': True}})
 
     numbers = [random.randint(1, 13) for _ in range(4)]
     has_solution_flag = has_solution(numbers)
 
     answer = await msg.waitNextMessage(msg.locale.t('twenty_four.message', numbers=numbers))
     expression = answer.asDisplay(text_only=True)
-    if play_state[msg.target.targetId]['24']['active']:
+    if play_state[msg.target.targetId]['active']:
         if expression.lower() in ['无解', '無解', 'none', 'n/a']:
             if has_solution_flag:
                 await answer.sendMessage(msg.locale.t('twenty_four.message.incorrect.have_solution'))
@@ -115,13 +115,13 @@ async def _(msg: Bot.MessageSession):
                 await answer.sendMessage(msg.locale.t('twenty_four.message.incorrect'))
         else:
             await answer.sendMessage(msg.locale.t('twenty_four.message.incorrect.error'))
-        play_state[msg.target.targetId]['24']['active'] = False
+        play_state[msg.target.targetId]['active'] = False
 
 
 
 @tf.command('stop {{twenty_four.stop.help}}')
 async def s(msg: Bot.MessageSession):
-    state = play_state.get(msg.target.targetId, {}).get('24', False)
+    state = play_state.get(msg.target.targetId, False)
     if state:
         if state['active']:
             play_state[msg.target.targetId]['24']['active'] = False
