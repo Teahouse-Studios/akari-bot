e9d32b59ea6319b00d44c07333670d4ab30842ef
好耶！ <86039582+cloudw233@users.noreply.github.com>
2023-06-07 16:42:31 +0800

    Update modules.py

--- modules/core/modules.py
+++ modules/core/modules.py
@@ -386,7 +386,7 @@ async def modules_help(msg: Bot.MessageSession):
         help_msg_t = '使用~help <模块名>查看详细信息。\n你也可以通过查阅文档获取帮助：\nhttps://bot.teahouse.team/wiki/'
         if msg.Feature.delete:
             help_msg_t += '\n[本消息将在一分钟后撤回]'
-        send = await msg.sendMessage([Image(pir('\n'.join(help_msg))), Plain(help_msg_t)])
+        send = await msg.sendMessage([Image(pir('\n'.join(help_msg), word_wrap=True)), Plain(help_msg_t)])
         await msg.sleep(60)
         await send.delete()
 
