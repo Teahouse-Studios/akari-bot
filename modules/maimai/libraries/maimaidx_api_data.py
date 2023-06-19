888bd467a3f1089e8e49a4b05165e6146854a281
多羅狼 <Dorowolf314@gmail.com>
2023-06-19 20:07:31 +0800

    Add get_record & get_plate

--- modules/maimai/libraries/maimaidx_api_data.py
+++ modules/maimai/libraries/maimaidx_api_data.py
@@ -22,22 +22,12 @@ async def get_alias(input, get_music=False):
 
 
 async def get_rank(msg, payload):
-    try:
-        userdata = await post_url('https://www.diving-fish.com/api/maimaidxprober/query/player',
-                                  data=json.dumps(payload),
-                                  status_code=200,
-                                  headers={'Content-Type': 'application/json', 'accept': '*/*'}, fmt='json')
-        username = userdata['username']
-        rating = userdata['rating']
-    except ValueError as e:
-        if str(e).startswith('400'):
-            await msg.finish(msg.locale.t("maimai.message.user_not_found"))
-        if str(e).startswith('403'):
-            await msg.finish(msg.locale.t("maimai.message.forbidden"))
-        else:
-            await msg.finish(ErrorMessage(str(e)))
+    player_data = await get_record(msg, payload)
 
-    rank_data = await get_url('https://www.diving-fish.com/api/maimaidxprober/rating_ranking', 200, fmt='json')
+    username = player_data['username']
+    rating = player_data['rating']
+    url = 'https://www.diving-fish.com/api/maimaidxprober/rating_ranking'
+    rank_data = await get_url(url, 200, fmt='json')
     sorted_data = sorted(rank_data, key=lambda x: x['ra'], reverse=True)
 
     rank = None
@@ -57,3 +47,39 @@ async def get_rank(msg, payload):
     time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     return time, total_rank, average_rating, username, rating, rank, surpassing_rate
+
+
+async def get_record(msg, payload):
+    url = 'https://www.diving-fish.com/api/maimaidxprober/query/player'
+    try:
+        data = await post_url(url,
+                              data=json.dumps(payload),
+                              status_code=200,
+                              headers={'Content-Type': 'application/json', 'accept': '*/*'}, fmt='json')
+    except ValueError as e:
+        if str(e).startswith('400'):
+            await msg.finish(msg.locale.t("maimai.message.user_not_found"))
+        if str(e).startswith('403'):
+            await msg.finish(msg.locale.t("maimai.message.forbidden"))
+        else:
+            await msg.finish(ErrorMessage(str(e)))
+
+    return data
+
+
+async def get_plate(msg, payload):
+    url = 'https://www.diving-fish.com/api/maimaidxprober/query/plate'
+    try:
+        data = await post_url(url,
+                              data=json.dumps(payload),
+                              status_code=200,
+                              headers={'Content-Type': 'application/json', 'accept': '*/*'}, fmt='json')
+    except ValueError as e:
+        if str(e).startswith('400'):
+            await msg.finish(msg.locale.t("maimai.message.user_not_found"))
+        if str(e).startswith('403'):
+            await msg.finish(msg.locale.t("maimai.message.forbidden"))
+        else:
+            await msg.finish(ErrorMessage(str(e)))
+
+    return data
