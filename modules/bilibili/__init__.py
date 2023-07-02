9b146b2bc5782a4c7fe052a14bc441aa41669332
多羅狼 <Dorowolf314@gmail.com>
2023-07-02 14:25:49 +0800

    add bilibili module

---
+++ modules/bilibili/__init__.py
@@ -0,0 +1,44 @@
+import re
+
+from core.builtins import Bot
+from core.component import module
+from .bilibili import get_info
+
+
+api_url = f'https://api.bilibili.com/x/web-interface/view/detail'
+
+
+bili = module('bilibili', alias='bili', developers=['DoroWolf'],
+              desc='{bilibili.help.desc}', support_languages=['zh_cn'])
+
+
+@bili.command('<video> [-i] {{bilibili.help}}',
+          options_desc={'-i': '{bilibili.help.option.i}'})
+async def _(msg: Bot.MessageSession, video: str):
+    if msg.parsed_msg.get('-i', False):
+        get_detail = True
+    if video[:2].upper() == "BV":
+        url = f"{api_url}?bvid={video}"
+    elif video[:2].lower() == "av":
+        url = f"{api_url}?aid={video[2:]}"
+    else:
+        msg.finish(msg.locale.t('bilibili.message.error.invalid'))
+    get_info(msg, url, get_detail)
+
+
+@bili.handle(re.compile(r"([aA][vV])(\d+)"),
+            desc="{bilibili.help.regex.av}")
+async def _(msg: Bot.MessageSession):
+    res = msg.matched_msg
+    if res:
+        url = f"{api_url}?aid={res.groups()[1]}"
+    get_info(msg, url)
+
+
+@bili.handle(re.compile(r"(BV1[a-zA-Z0-9]{9})"),
+            desc="{bilibili.help.regex.bv}")
+async def _(msg: Bot.MessageSession):
+    res = msg.matched_msg
+    if res:
+        url = f"{api_url}?bvid={res}"
+    get_info(msg, url)
---
+++ modules/bilibili/bilibili.py
@@ -0,0 +1,68 @@
+from datetime import datetime
+
+from core.builtins import Bot
+from core.builtins import Image, Plain, ErrorMessage
+from core.utils.http import get_url
+
+
+async def get_info(message: Bot.MessageSession, url, get_detail=False):
+    try:
+        data = await get_url(url, 200, fmt='json')
+    except ValueError as e:
+        if str(e).startswith('404'):
+            await msg.finish(msg.locale.t("bilibili.message.error"))
+        else:
+           await msg.sendMessage(ErrorMessage(str(e)))
+        
+
+    view = data['data']['View']
+    stat = view['stat']
+    
+    pic = view['pic']
+    video_url = f"https://www.bilibili.com/video/{view['bvid']}"
+    title = view['title']
+    tname = view['tname']
+
+    timestamp = view['ctime']
+    dt_object = datetime.fromtimestamp(timestamp)
+    time = dt_object.strftime('%Y-%m-%d %H:%M:%S')
+
+    if len(view['pages']) > 1:
+        pages = f" ({len(view['pages'])} P)"
+    else:
+        pages = ''
+
+    stat_view = format_num(stat['view'])
+    stat_danmaku = format_num(stat['danmaku'])
+    stat_reply = format_num(stat['reply'])
+    stat_favorite = format_num(stat['favorite'])
+    stat_coin = format_num(stat['coin'])
+    stat_share = format_num(stat['share'])
+    stat_like = format_num(stat['like'])
+
+    owner = view['owner']['name']
+    fans = format_num(data['data']['Card']['card']['fans'])
+
+    if get_detail:
+        msg = video_url + message.locale.t('bilibili.message', title=title, tname=tname, owner=owner, time=time)
+    else:
+        msg = video_url + message.locale.t('bilibili.message.detail', title=title, pages=pages, tname=tname,
+                                                            owner=owner, fans=fans, view=stat_view, danmaku=stat_danmaku, reply=stat_reply,
+                                                            like=stat_like, coin=stat_coin, favorite=stat_favorite, share=stat_share)
+        
+    await msg.finish([Image(pic), Plain(msg)])
+
+
+async def format_num(number):
+    if number >= 1000000000:
+        return f'{number/1000000000:.1f}B'
+    elif number >= 1000000:
+        return f'{number/1000000:.1f}M'
+    elif number >= 1000:
+        return f'{number/1000:.1f}k'
+    else:
+        return str(number)
+
+
+
+    
