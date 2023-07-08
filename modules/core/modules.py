e5f9520e670b92009abe1a10d1130fbe857ed97d
yzhh <31803608+OasisAkari@users.noreply.github.com>
2023-07-08 15:11:58 +0800

    Update modules.py

--- modules/core/modules.py
+++ modules/core/modules.py
@@ -226,8 +226,9 @@ async def config_modules(msg: Bot.MessageSession):
                             unloaded_list = CFG.get('unloaded_modules')
                             if not unloaded_list:
                                 unloaded_list = []
-                            unloaded_list.append(module_)
-                            CFG.write('unloaded_modules', unloaded_list)
+                            if module_ not in unloaded_list:
+                                unloaded_list.append(module_)
+                                CFG.write('unloaded_modules', unloaded_list)
                             msglist.append(msg.locale.t("core.message.module.unload.success", module=module_))
                     else:
                         msglist.append(msg.locale.t("core.message.module.unload.error"))
