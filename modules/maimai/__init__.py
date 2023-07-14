8a6dd6bf6fdf8ff150af039386d4db30ebe5de98
多羅狼 <Dorowolf314@gmail.com>
2023-07-14 14:19:25 +0800

    fix

--- modules/maimai/__init__.py
+++ modules/maimai/__init__.py
@@ -3,7 +3,7 @@ from core.component import module
 from core.logger import Logger
 from core.utils.image import msgchain2image
 from modules.maimai.libraries.maimai_best_50 import generate
-from modules.maimai.libraries.maimaidx_api_data import get_alias, get_cover
+from modules.maimai.libraries.maimaidx_api_data import update_alias, get_alias, get_cover
 from modules.maimai.libraries.maimaidx_project import get_level_process, get_plate_process, get_player_score, get_rank, get_score_list
 from modules.maimai.libraries.maimaidx_music import Music, TotalList
 from .regex import *
