fbaa87f2af2bc4792f163af15c811cd0199ee2e2
多羅狼 <Dorowolf314@gmail.com>
2023-07-24 11:33:26 +0800

    Update modules.py

--- modules/core/modules.py
+++ modules/core/modules.py
@@ -185,20 +185,17 @@ async def config_modules(msg: Bot.MessageSession):
                 elif module_ not in modules_:
                     msglist.append(msg.locale.t("core.message.module.reload.unbound", module=module_))
                 else:
-                    if modules_[module_].base:
-                        msglist.append(msg.locale.t("core.message.module.reload.base", module=module_))
-                    else:
-                        extra_reload_modules = ModulesManager.search_related_module(module_, False)
-                        if len(extra_reload_modules):
-                            confirm = await msg.waitConfirm(msg.locale.t("core.message.module.reload.confirm",
-                                                                         modules='\n'.join(extra_reload_modules)))
-                            if not confirm:
-                                continue
-                        unloaded_list = CFG.get('unloaded_modules')
-                        if module_ in unloaded_list:
-                            unloaded_list.remove(module_)
-                            CFG.write('unloaded_modules', unloaded_list)
-                        msglist.append(module_reload(module_, extra_reload_modules))
+                    extra_reload_modules = ModulesManager.search_related_module(module_, False)
+                    if len(extra_reload_modules):
+                        confirm = await msg.waitConfirm(msg.locale.t("core.message.module.reload.confirm",
+                                                                     modules='\n'.join(extra_reload_modules)))
+                        if not confirm:
+                            continue
+                    unloaded_list = CFG.get('unloaded_modules')
+                    if module_ in unloaded_list:
+                        unloaded_list.remove(module_)
+                        CFG.write('unloaded_modules', unloaded_list)
+                    msglist.append(module_reload(module_, extra_reload_modules))
         else:
             msglist.append(msg.locale.t("parser.superuser.permission.denied"))
     elif msg.parsed_msg.get('load', False):
