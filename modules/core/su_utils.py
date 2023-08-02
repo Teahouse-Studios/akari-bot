c3e347b2819a2b6acda62871a6bd82fd6685d6af
多羅狼 <Dorowolf314@gmail.com>
2023-08-02 22:48:11 +0800

    Update su_utils.py

--- modules/core/su_utils.py
+++ modules/core/su_utils.py
@@ -474,8 +474,8 @@ if Config('openai_api_key'):
             group = msg.parsed_msg['<target>']
             target = BotDBUtil.TargetInfo(group)
             target.modify_petal(int(petal))
-            await msg.finish(msg.locale.t('core.message.petal.modify.self', group=group, add_petal=petal, petal=msg.data.petal))
+            await msg.finish(msg.locale.t('core.message.petal.modify', group=group, add_petal=petal, petal=msg.data.petal))
         else:
             target = msg.data
             target.modify_petal(int(petal))
-            await msg.finish(msg.locale.t('core.message.petal.modify', add_petal=petal, petal=msg.data.petal))
+            await msg.finish(msg.locale.t('core.message.petal.modify.self', add_petal=petal, petal=msg.data.petal))
