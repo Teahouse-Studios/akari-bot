5d1c53c2cf81f2c4c22557d14b01d7cff04768b2
多羅狼 <Dorowolf314@gmail.com>
2023-08-24 13:09:07 +0800

    Update

--- modules/whois/__init__.py
+++ modules/whois/__init__.py
@@ -7,10 +7,16 @@ from core.utils.image import msgchain2image
 
 
 def process(input):
-        if isinstance(input, list) and len(input) > 0:
-            return input[0]
-        else:
-            return input
+    if isinstance(input, list) and len(input) > 0:
+        return input[0]
+    else:
+        return input
+
+def get_value(dict, key):
+    if key in dict:
+        return dict[key]
+    else:
+        return None
 
 
 w = module('whois', desc='{whois.help.desc}',
@@ -29,23 +35,36 @@ async def _(msg: Bot.MessageSession, domain: str):
 async def get_whois(msg, domain):
     try:
         info = whois(domain)
-        if not info['domain_name']:
-            await msg.finish(msg.locale.t("whois.message.error.get_failed"))
     except:
         await msg.finish(msg.locale.t("whois.message.error.get_failed"))
 
-    name_servers = list(set([i.lower() for i in info['name_servers']]))
+    domain_name = get_value(info, domain_name)
+    registrar = get_value(info, registrar)
+    whois_server = get_value(info, whois_server)
+    updated_date = get_value(info, updated_date)
+    creation_date = get_value(info, creation_date)
+    expiration_date = get_value(info, expiration_date)
+    name_servers = get_value(info, name_servers)
+    name_servers_list = list(set([i.lower() for i in info['name_servers']]))
+    dnssec = get_value(info, dnssec)
+    name = get_value(info, name)
+    org = get_value(info, org)
+    address = get_value(info, address)
+    city = get_value(info, city)
+    state = get_value(info, state)
+    country = get_value(info, country)
+    registrant_postal_code = get_value(info, registrant_postal_code)
 
     return f'''\
-{msg.locale.t('whois.message.domain_name')}{info['domain_name'][1]}{f"""
-{msg.locale.t('whois.message.registrar')}{info['registrar']}""" if info['registrar'] else ''}{f"""
-{msg.locale.t('whois.message.whois_server')}{info['whois_server']}""" if info['whois_server'] else ''}{f"""
-{msg.locale.t('whois.message.updated_date')}{str(process(info['updated_date']))}""" if info['updated_date'] else ''}{f"""
-{msg.locale.t('whois.message.creation_date')}{str(process(info['creation_date']))}""" if info['creation_date'] else ''}{f"""
-{msg.locale.t('whois.message.expiration_date')}{str(process(info['expiration_date']))}""" if info['expiration_date'] else ''}{f"""
-{msg.locale.t('whois.message.name_servers')}{', '.join(name_servers)}""" if info['name_servers'] else ''}{f"""
-{msg.locale.t('whois.message.dnssec')}{process(info['dnssec'])}""" if info['dnssec'] else ''}{f"""
-{msg.locale.t('whois.message.name')}{process(info['name'])}""" if info['name'] else ''}{f"""
-{msg.locale.t('whois.message.organization')}{process(info['org'])}""" if info['org'] else ''}{f"""
-{msg.locale.t('ip.message.location')}{f"{process(info['address'])}, " if info['address'] else ''}{f"{process(info['city'])}, " if info['city'] else ''}{f"{process(info['state'])}, " if info['state'] else ''}{process(info['country'])}""" if info['country'] else ''}{f"""
-{msg.locale.t('whois.message.postal_code')}{process(info['registrant_postal_code'])}""" if info['registrant_postal_code'] else ''}'''
+{msg.locale.t('whois.message.domain_name')}{domain_name[1]}{f"""
+{msg.locale.t('whois.message.registrar')}{registrar}""" if registrar else ''}{f"""
+{msg.locale.t('whois.message.whois_server')}{whois_server""" if whois_server else ''}{f"""
+{msg.locale.t('whois.message.updated_date')}{str(process(updated_date))}""" if updated_date else ''}{f"""
+{msg.locale.t('whois.message.creation_date')}{str(process(creation_date))}""" if creation_date else ''}{f"""
+{msg.locale.t('whois.message.expiration_date')}{str(process(expiration_date))}""" if expiration_date else ''}{f"""
+{msg.locale.t('whois.message.name_servers')}{', '.join(name_servers_list)}""" if name_servers else ''}{f"""
+{msg.locale.t('whois.message.dnssec')}{process(dnssec)}""" if dnssec else ''}{f"""
+{msg.locale.t('whois.message.name')}{process(name)}""" if name else ''}{f"""
+{msg.locale.t('whois.message.organization')}{process(org)}""" if org else ''}{f"""
+{msg.locale.t('ip.message.location')}{f"{process(address)}, " if address else ''}{f"{process(city)}, " if city else ''}{f"{process(state)}, " if state else ''}{process(country)}""" if country else ''}{f"""
+{msg.locale.t('whois.message.postal_code')}{process(registrant_postal_code)}""" if registrant_postal_code else ''}'''
