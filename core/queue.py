fc587d53c80ee18d207b33df1fcb3b71e745689a
yzhh <31803608+OasisAkari@users.noreply.github.com>
2023-08-29 21:04:36 +0800

    Update queue.py

--- core/queue.py
+++ core/queue.py
@@ -1,4 +1,6 @@
 import asyncio
+import traceback
+
 import ujson as json
 
 from core.logger import Logger
@@ -29,6 +31,11 @@ class JobQueue:
                                   {'target_id': target_id, 'sender_id': sender_id}))['value']
 
 
+def return_val(tsk, value: dict, status=True):
+    value = value.update({'status': status})
+    BotDBUtil.JobQueue.return_val(tsk, value)
+
+
 async def check_job_queue():
     for tskid in _queue_tasks:
         tsk = BotDBUtil.JobQueue.get(tskid)
@@ -39,7 +46,10 @@ async def check_job_queue():
     for tsk in get_all:
         Logger.debug(f'Received job queue task {tsk.taskid}, action: {tsk.action}')
         args = json.loads(tsk.args)
-        if tsk.action == 'validate_permission':
-            fetch = await Bot.FetchTarget.fetch_target(args['target_id'], args['sender_id'])
-            if fetch:
-                BotDBUtil.JobQueue.return_val(tsk, json.dumps({'value': await fetch.parent.checkPermission()}))
+        try:
+            if tsk.action == 'validate_permission':
+                fetch = await Bot.FetchTarget.fetch_target(args['target_id'], args['sender_id'])
+                if fetch:
+                    return_val(tsk, {'value': await fetch.parent.checkPermission()})
+        except Exception as e:
+            return_val(tsk, {'traceback': traceback.format_exc()}, status=False)
