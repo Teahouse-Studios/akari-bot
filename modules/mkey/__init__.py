2614972574c8a4a26fba3c96f992d6fcd5bfa295
yzhh <31803608+OasisAkari@users.noreply.github.com>
2023-09-02 19:00:02 +0800

    Update __init__.py

--- modules/mkey/__init__.py
+++ modules/mkey/__init__.py
@@ -20,8 +20,13 @@ async def mkey(msg: Bot.MessageSession, device: str, month: int, day: int, inqui
         await msg.finish('无效的月份。')
     if day < 1 or day > 31:
         await msg.finish('无效的日期。')
-
+    if device.lower() not in device_codes:
+        await msg.finish('无效的主机。')
+    if len(inquiry_num) not in [6, 8, 10]:
+        await msg.finish('无效的询问码。')
     device_code = device_codes[device.lower()]
+    if device_id is None and device_code == "HAC":
+        await msg.finish('Switch需要指定主机ID。')
 
     result = get_mkey(inquiry_num, month, day, device_id, device_code)
     await msg.finish(f'您的重置密码是：{result}。')
