2643c66184884e652437782a02bf5585bc1b6e92
yzhh <31803608+OasisAkari@users.noreply.github.com>
2023-09-13 09:56:55 +0800

    Update cooldown.py

--- core/utils/cooldown.py
+++ core/utils/cooldown.py
@@ -4,7 +4,7 @@ from core.types import MessageSession
 from typing import Union
 
 
-_cd_lst = {}
+cd_lst = {}
 
 
 class CoolDown:
@@ -17,15 +17,15 @@ class CoolDown:
             self.sender_id = self.sender_id.target.sender_id
 
     def add(self):
-        if self.k not in _cd_lst:
-            _cd_lst[self.k] = {}
-        _cd_lst[self.k].update({self.sender_id: datetime.datetime.now().timestamp()})
+        if self.k not in cd_lst:
+            cd_lst[self.k] = {}
+        cd_lst[self.k][self.sender_id] = datetime.datetime.now().timestamp()
 
     def check(self, delay):
-        if self.k not in _cd_lst:
+        if self.k not in cd_lst:
             return 0
-        if self.sender_id in _cd_lst:
-            if (d := (datetime.datetime.now().timestamp() - _cd_lst[self.k][self.sender_id])) > delay:
+        if self.sender_id in cd_lst[self.k]:
+            if (d := (datetime.datetime.now().timestamp() - cd_lst[self.k][self.sender_id])) > delay:
                 return 0
             else:
                 return d
@@ -33,7 +33,7 @@ class CoolDown:
             return 0
 
     def reset(self):
-        if self.k in _cd_lst:
-            if self.sender_id in _cd_lst[self.k]:
-                _cd_lst[self.k].pop(self.sender_id)
+        if self.k in cd_lst:
+            if self.sender_id in cd_lst[self.k]:
+                cd_lst[self.k].pop(self.sender_id)
         self.add()
