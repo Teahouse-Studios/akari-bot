98aac1735009cc60a3c37a7f3b15431380172645
多羅狼 <Dorowolf314@gmail.com>
2023-09-28 12:36:43 +0800

    .

--- modules/core/su_utils.py
+++ modules/core/su_utils.py
@@ -472,7 +472,7 @@ async def _(msg: Bot.MessageSession):
 
 
 if Config('openai_api_key'):
-    petal = module('petal', developers=['Dianliang233'], base=True, alias='petals', base=True)
+    petal = module('petal', developers=['Dianliang233'], base=True, alias='petals')
 
     @petal.handle()
     async def _(msg: Bot.MessageSession):
