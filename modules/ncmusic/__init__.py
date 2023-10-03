de0eb942a4a06cc1bc9a19fbda8af37ebb18aa51
SilianZ <silian_zheng@outlook.com>
2023-10-03 13:57:56 +0800

    :bug: 修复了在未配置Webrender时无法进行交互式搜索

--- modules/ncmusic/__init__.py
+++ modules/ncmusic/__init__.py
@@ -2,6 +2,7 @@ from core.builtins import Bot, Plain, Image
 from core.component import module
 from core.utils.image_table import image_table_render, ImageTable
 from core.utils.http import get_url
+from core.logger import logger
 
 ncmusic = module('ncmusic',
                  developers=['bugungu', 'DoroWolf'],
@@ -73,7 +74,7 @@ async def search(msg: Bot.MessageSession, keyword: str):
         send_msg = msg.locale.t('ncmusic.message.search.result') + '\n'
 
         for i, song in enumerate(songs, start=1):
-            send_msg += f"{i}\u200B. {song['name']}"
+            send_msg += f"{i}. {song['name']}"
             if 'transNames' in song:
                 send_msg += f"（{' / '.join(song['transNames'])}）"
             send_msg += f"——{' / '.join(artist['name'] for artist in song['artists'])}"
@@ -84,8 +85,29 @@ async def search(msg: Bot.MessageSession, keyword: str):
 
         if len(result['result']['songs']) > 10:
             send_msg += msg.locale.t('ncmusic.message.search.collapse')
-        
-        await msg.finish(send_msg.strip())
+        send_msg += '\n'
+        send_msg += msg.locale.t('ncmusic.message.search.prompt')
+        query = await msg.wait_next_message(send_msg)
+        query = query.as_display(text_only=True)
+        try:
+            query = int(query)
+            if query > 10:
+                await msg.finish(msg.locale.t('ncmusic.message.search.invalid.out_of_range'))
+            sid = result['result']['songs'][query - 1]['id']
+            url = f"https://ncmusic.akari-bot.top/song/detail?ids={sid}"
+            logger.info(url)
+            info = await get_url(url, 200, fmt='json')
+            info = info['songs'][0]
+            artist = ' / '.join([ar['name'] for ar in info['ar']])
+            song_page = f"https://music.163.com/#/song?id={info['id']}"
+
+            send_msg = msg.locale.t('ncmusic.message.info',
+                                    name=info['name'], id=info['id'],
+                                    album=info['al']['name'], album_id=info['al']['id'],
+                                    artists=artist, detail=song_page)
+            await msg.finish([Image(info['al']['picUrl']), Plain(send_msg)])
+        except Exception:
+            await msg.finish(msg.locale.t('ncmusic.message.search.invalid.non_digital'))
         
 
 @ncmusic.handle('info <sid> {{ncmusic.help.info}}')
