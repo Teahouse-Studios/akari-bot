070338d9862954a1b5deea8ba6160e9d4639ff1a
Dianliang233 <dianliang233@gmail.com>
2023-10-23 16:55:07 +0800

    Refactor expert mode with minimax algorithm

--- modules/tic_tac_toe/__init__.py
+++ modules/tic_tac_toe/__init__.py
@@ -1,3 +1,4 @@
+import math
 import random
 from typing import Awaitable, Callable, List
 from core.builtins import Bot
@@ -13,24 +14,24 @@ tic_tac_toe = module('tic_tac_toe',
 play_state = {}
 
 
-def check_winner(table: List[List[int]]):
+def check_winner(board: List[List[int]]):
     # left-right diagonal
-    if table[0][0] == table[1][1] == table[2][2] != 0:
-        return table[0][0]
+    if board[0][0] == board[1][1] == board[2][2] != 0:
+        return board[0][0]
     # right-left diagonal
-    elif table[0][2] == table[1][1] == table[2][0] != 0:
-        return table[0][2]
+    elif board[0][2] == board[1][1] == board[2][0] != 0:
+        return board[0][2]
     for i in range(3):
         # vertical
-        if table[i][0] == table[i][1] == table[i][2] != 0:
-            return table[i][0]
+        if board[i][0] == board[i][1] == board[i][2] != 0:
+            return board[i][0]
         # horizontal
-        elif table[0][i] == table[1][i] == table[2][i] != 0:
-            return table[0][i]
+        elif board[0][i] == board[1][i] == board[2][i] != 0:
+            return board[0][i]
     return None
 
 
-GameTable = List[List[int]]
+GameBoard = List[List[int]]
 GameCallback = Callable[[List[List[int]]], Awaitable[tuple[int]]]
 
 
@@ -42,42 +43,42 @@ async def game(msg: Bot.MessageSession,
                x_callback: GameCallback,
                o_callback: GameCallback) -> 0 | 1 | 2:
     # 0 = empty; 1 = x; 2 = o
-    table = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
     while True:
         if not play_state[msg.target.target_id]['active']:
             raise TerminationError
-        x = await x_callback(table)
-        table[x[0]][x[1]] = 1
-        if winner := check_winner(table):
-            return winner, table
+        x = await x_callback(board)
+        board[x[0]][x[1]] = 1
+        if winner := check_winner(board):
+            return winner, board
         spaces = []
         for i in range(3):
             for j in range(3):
-                if table[i][j] == 0:
+                if board[i][j] == 0:
                     spaces.append((i, j))
         if not spaces:
-            return 0, table
+            return 0, board
 
-        o = await o_callback(table)
-        table[o[0]][o[1]] = 2
-        if winner := check_winner(table):
-            return winner, table
+        o = await o_callback(board)
+        board[o[0]][o[1]] = 2
+        if winner := check_winner(board):
+            return winner, board
         spaces = []
         for i in range(3):
             for j in range(3):
-                if table[i][j] == 0:
+                if board[i][j] == 0:
                     spaces.append((i, j))
         if not spaces:
-            return 0, table
+            return 0, board
 
 
-def format_table(table: GameTable):
-    return '\n'.join([' '.join(['Ｘ' if i == 1 else 'Ｏ' if i == 2 else '．' for i in row]) for row in table])
+def format_board(board: GameBoard):
+    return '\n'.join([' '.join(['Ｘ' if i == 1 else 'Ｏ' if i == 2 else '．' for i in row]) for row in board])
 
 
 def generate_human_callback(msg: Bot.MessageSession, player: str):
-    async def callback(table: List[List[int]]):
-        await msg.send_message(format_table(table) + f'\n{msg.locale.t("ttt.turn.message", player=player)}', quote=False)
+    async def callback(board: List[List[int]]):
+        await msg.send_message(format_board(board) + f'\n{msg.locale.t("ttt.turn.message", player=player)}', quote=False)
         while True:
             if not play_state[msg.target.target_id]['active']:
                 raise TerminationError
@@ -95,7 +96,7 @@ def generate_human_callback(msg: Bot.MessageSession, player: str):
                     continue
                 x = (digit - 1) // 3
                 y = (digit - 1) % 3
-                if table[x][y] == 0:
+                if board[x][y] == 0:
                     return x, y
                 else:
                     continue
@@ -105,7 +106,7 @@ def generate_human_callback(msg: Bot.MessageSession, player: str):
                     y = int(text[1]) - 1
                 except ValueError:
                     continue
-                if table[x][y] == 0:
+                if board[x][y] == 0:
                     return x, y
                 else:
                     continue
@@ -115,74 +116,123 @@ def generate_human_callback(msg: Bot.MessageSession, player: str):
     return callback
 
 
-async def expert_bot_callback(table: GameTable):
-    # Win: If the player has two in a row, they can place a third to get three in a row.
-    # Block: If the opponent has two in a row, the player must play the third themselves to block the opponent.
-    for i in range(3):
-        if table[i][0] == table[i][1] and table[i][2] == 0:
-            return i, 2
-        elif table[i][1] == table[i][2] and table[i][0] == 0:
-            return i, 0
-        elif table[i][0] == table[i][2] and table[i][1] == 0:
-            return i, 1
-    for i in range(3):
-        if table[0][i] == table[1][i] and table[2][i] == 0:
-            return 2, i
-        elif table[1][i] == table[2][i] and table[0][i] == 0:
-            return 0, i
-        elif table[0][i] == table[2][i] and table[1][i] == 0:
-            return 1, i
-    if table[0][0] == table[1][1] and table[2][2] == 0:
-        return 2, 2
-    elif table[1][1] == table[2][2] and table[0][0] == 0:
-        return 0, 0
-    elif table[0][0] == table[2][2] and table[1][1] == 0:
-        return 1, 1
-    elif table[0][2] == table[1][1] and table[2][0] == 0:
-        return 2, 0
-    elif table[1][1] == table[2][0] and table[0][2] == 0:
-        return 0, 2
-    elif table[0][2] == table[2][0] and table[1][1] == 0:
-        return 1, 1
-
-    spaces = []
+def is_move_left(board: GameBoard):
     for i in range(3):
         for j in range(3):
-            if table[i][j] == 0:
-                spaces.append((i, j))
-
-    # first move
-    if len(spaces) == 8:
-        # 1 goes center
-        if table[1][1] == 1:
-            return random.choice([(0, 0), (0, 2), (2, 0), (2, 2)])
-        # 1 goes corner/edge
-        else:
-            return 1, 1
-
-    # second move
-    if len(spaces) == 6:
-        # 1 goes corner and 2 goes center and 1 goes corner, then 2 goes to any edge
-        if table[1][1] == 2 and any([table[0][0] == table[2][2] == 1, table[0][2] == table[2][0] == 1]):
-            return random.choice([(0, 1), (1, 0), (1, 2), (2, 1)])
-
-    # prefer corner to edge
-    corners = (0, 0), (0, 2), (2, 0), (2, 2)
-    for corner in random.shuffle(corners):
-        if corner in spaces:
-            return corner
-    edges = (0, 1), (1, 0), (1, 2), (2, 1)
-    for edge in random.shuffle(edges):
-        if edge in spaces:
-            return edge
-    return random.choice(spaces)
+            if (board[i][j] == 0):
+                return True
+    return False
 
 
-async def random_bot_callback(table: GameTable):
+def evaluate(b: GameBoard) -> int:
+    return 10 if check_winner(b) == 2 else -10 if check_winner(b) == 1 else 0
+
+
+def minimax(board: GameBoard, depth: int, is_max: bool):
+    score = evaluate(board)
+    player = 2
+    opponent = 1
+
+    if (score == 10):
+        return score
+
+    if (score == -10):
+        return score
+
+    if (not is_move_left(board)):
+        return 0
+
+    # If this maximizer's move
+    if (is_max):
+        best = -1000
+
+        # Traverse all cells
+        for i in range(3):
+            for j in range(3):
+
+                # Check if cell is empty
+                if (board[i][j] == 0):
+
+                    # Make the move
+                    board[i][j] = player
+
+                    # Call minimax recursively and choose
+                    # the maximum value
+                    best = max(best, minimax(board,
+                                             depth + 1,
+                                             not is_max))
+
+                    # Undo the move
+                    board[i][j] = 0
+        return best
+
+    # If this minimizer's move
+    else:
+        best = 1000
+
+        # Traverse all cells
+        for i in range(3):
+            for j in range(3):
+
+                # Check if cell is empty
+                if (board[i][j] == 0):
+
+                    # Make the move
+                    board[i][j] = opponent
+
+                    # Call minimax recursively and choose
+                    # the minimum value
+                    best = min(best, minimax(board, depth + 1, not is_max))
+
+                    # Undo the move
+                    board[i][j] = 0
+        return best
+
+
+def find_best_move(board):
+    player = 2
+    # opponent = 1
+    best_val = -1000
+    best_move = (-1, -1)
+
+    # Traverse all cells, evaluate minimax function for
+    # all empty cells. And return the cell with optimal
+    # value.
+    for i in range(3):
+        for j in range(3):
+
+            # Check if cell is empty
+            if (board[i][j] == 0):
+
+                # Make the move
+                board[i][j] = player
+                # compute evaluation function for this
+                # move.
+                move_val = minimax(board, 0, False)
+
+                # Undo the move
+                board[i][j] = 0
+
+                # If the value of the current move is
+                # more than the best value, then update
+                # best/
+                if (move_val > best_val):
+                    best_move = (i, j)
+                    best_val = move_val
+
+    return best_move
+
+
+async def expert_bot_callback(board: GameBoard):
+    print(find_best_move(board))
+    return find_best_move(board)
+
+
+async def random_bot_callback(board: GameBoard):
     random_spaces = []
     for i in range(3):
         for j in range(3):
-            if table[i][j] == 0:
+            if board[i][j] == 0:
                 random_spaces.append((i, j))
     return random.choice(random_spaces)
 
@@ -213,7 +263,7 @@ async def ttt_with_bot(msg: Bot.MessageSession):
     play_state.update({msg.target.target_id: {'active': True}})
 
     try:
-        winner, table = await game(msg, generate_human_callback(msg, 'X'), expert_bot_callback if msg.parsed_msg else random_bot_callback)
+        winner, board = await game(msg, generate_human_callback(msg, 'X'), expert_bot_callback if msg.parsed_msg else random_bot_callback)
     except TerminationError:
         return
 
@@ -221,7 +271,7 @@ async def ttt_with_bot(msg: Bot.MessageSession):
     if winner == 0:
         await msg.finish(msg.locale.t('ttt.draw'), quote=False)
     g_msg = '\n' + gained_petal(msg, 2) if winner == 1 and msg.parsed_msg else ''
-    await msg.finish(format_table(table) + '\n' + msg.locale.t('ttt.winner', winner='X' if winner == 1 else 'O') + g_msg, quote=False)
+    await msg.finish(format_board(board) + '\n' + msg.locale.t('ttt.winner', winner='X' if winner == 1 else 'O') + g_msg, quote=False)
 
 
 @tic_tac_toe.command('duo {{ttt.duo.help}}')
@@ -236,11 +286,11 @@ async def ttt_multiplayer(msg: Bot.MessageSession):
     play_state.update({msg.target.target_id: {'active': True}})
 
     try:
-        winner, table = await game(msg, generate_human_callback(msg, 'X'), generate_human_callback(msg, 'O'))
+        winner, board = await game(msg, generate_human_callback(msg, 'X'), generate_human_callback(msg, 'O'))
     except TerminationError:
         return
 
     play_state[msg.target.target_id]['active'] = False
     if winner == 0:
         await msg.finish(msg.locale.t('ttt.draw'), quote=False)
-    await msg.finish(format_table(table) + '\n' + msg.locale.t('ttt.winner', winner='X' if winner == 1 else 'O'), quote=False)
+    await msg.finish(format_board(board) + '\n' + msg.locale.t('ttt.winner', winner='X' if winner == 1 else 'O'), quote=False)
