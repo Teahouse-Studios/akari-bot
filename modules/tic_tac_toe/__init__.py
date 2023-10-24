13cb7ac366f1adb360fc5b9086a0c38c30909cd5
Dianliang233 <dianliang233@gmail.com>
2023-10-24 14:30:25 +0800

    Fix help

--- modules/tic_tac_toe/__init__.py
+++ modules/tic_tac_toe/__init__.py
@@ -264,7 +264,7 @@ async def terminate(msg: Bot.MessageSession):
 @tic_tac_toe.command('{{ttt.bot.help}}')
 @tic_tac_toe.command('noob {{ttt.noob.help}}')
 @tic_tac_toe.command('expert {{ttt.expert.help}}')
-@tic_tac_toe.command('master {{ttt.expert.help}}')
+@tic_tac_toe.command('master {{ttt.master.help}}')
 async def ttt_with_bot(msg: Bot.MessageSession):
     if msg.parsed_msg:
         if 'expert' in msg.parsed_msg:
