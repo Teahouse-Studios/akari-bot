2d046a4518520c7c065094cdccace4a6dbf7de42
海屿有燕 <zorua@vip.qq.com>
2023-10-28 03:24:57 +0800

    Update __init__.py

--- modules/genshin/__init__.py
+++ modules/genshin/__init__.py
@@ -7,10 +7,8 @@ genshin = module('genshin', alias='yuanshen', desc='原神角色信息查询。'
 
 client = Enka()
 
-@genshin.command('test <UID> 只是测试，什么都没有',
-                 'placeholder <ph> Placeholder',
-)
-async def _(msg: Bot.MessageSession, genshin):
+@genshin.command()
+async def _(msg: Bot.MessageSession):
     user = await client.fetch_user(193588293)
     player_level = {user.player.level}
     await msg.send_message(player_level)
