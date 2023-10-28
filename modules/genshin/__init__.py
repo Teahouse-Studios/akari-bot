57bf4f38ab954bf2d1c4199c154c550d99424fdf
海屿有燕 <zorua@vip.qq.com>
2023-10-28 19:14:40 +0800

    Update __init__.py

--- modules/genshin/__init__.py
+++ modules/genshin/__init__.py
@@ -18,9 +18,10 @@ async def _(msg: Bot.MessageSession):
     data = await client.fetch_user(msg.parsed_msg['<number>'])
     player_level = {data.player.level}
     await msg.send_message(
-        f"测试用玩家昵称：{data.player.nickname}\n"
+        f"玩家昵称：{data.player.nickname}\n"
         f"玩家签名: {data.player.signature}\n"
-        f"测试用玩家等级：{data.player.level}\n"
+        f"玩家等级：{data.player.level}\n"
         f"深境螺旋: {data.player.abyss_floor} 层 {data.player.abyss_room} 间"
+        f"数据缓存时间：{data.ttl}\n"
         )
     
