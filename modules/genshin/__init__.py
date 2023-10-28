48b18faf56e2e74872690d3a7f945cc19ade346b
海屿有燕 <zorua@vip.qq.com>
2023-10-28 16:26:20 +0800

    refactor

--- modules/genshin/__init__.py
+++ modules/genshin/__init__.py
@@ -1,15 +1,18 @@
 from core.component import module
 from core.builtins import Bot
-from enkapy import Enka
+from enkanetwork import EnkaNetworkAPI
 from core.utils.cooldown import CoolDown
+from config import Config
 
 genshin = module('genshin', alias='yuanshen', desc='原神角色信息查询。', developers=['ZoruaFox'])
 
-client = Enka()
+client = EnkaNetworkAPI()
+
+ENKA_URL = Config('enka_url')
 
 @genshin.command()
 async def _(msg: Bot.MessageSession):
-    user = await client.fetch_user(193588293)
-    player_level = {user.player.level}
-    await msg.send_message('测试用玩家等级：' + str(player_level))
+    data = await client.fetch_user(193588293)
+    player_level = {data.player.level}
+    await msg.send_message(f'测试用玩家等级：{player_level}')
     
