b1112e5dbca34a1811659d7396ca00bd615cc5b2
海屿有燕 <zorua@vip.qq.com>
2023-10-28 19:22:50 +0800

    feat: add Genshin module

---
+++ modules/genshin/__init__.py
@@ -0,0 +1,27 @@
+import asyncio
+from core.component import module
+from core.builtins import Bot
+from enkanetwork import EnkaNetworkAPI
+from core.utils.cooldown import CoolDown
+from config import Config
+from enkanetwork import Assets
+
+
+genshin = module('genshin', alias='yuanshen', desc='原神角色信息查询。', developers=['ZoruaFox'])
+
+client = EnkaNetworkAPI()
+
+ENKA_URL = Config('enka_url') #预引入enka节点自定义
+
+@genshin.handle('uid <number> {{genshin.help.uid}}')
+async def _(msg: Bot.MessageSession):
+    data = await client.fetch_user(msg.parsed_msg['<number>'])
+    player_level = {data.player.level}
+    await msg.send_message(
+        f"玩家昵称：{data.player.nickname}\n"
+        f"玩家签名: {data.player.signature}\n"
+        f"玩家等级：{data.player.level}\n"
+        f"深境螺旋: {data.player.abyss_floor} 层 {data.player.abyss_room} 间"
+        f"数据缓存时间：{data.ttl}\n"
+        )
+    
