07c79106e4e37c227c56571a14f1cd8fd4cf422f
yzhh <31803608+OasisAkari@users.noreply.github.com>
2023-11-01 10:50:28 +0800

    update

--- core/builtins/utils/__init__.py
+++ core/builtins/utils/__init__.py
@@ -1,11 +1,9 @@
 from config import Config
 from core.types import PrivateAssets, Secret
 
-if not (confirm_command := Config('confirm_command')):
-    confirm_command = ["是", "对", "對", "yes", "Yes", "YES", "y", "Y"]
 
-if not (command_prefix := Config('command_prefix')):
-    command_prefix = ['~', '～']  # 消息前缀
+confirm_command = Config('confirm_command', default=["是", "对", "對", "yes", "Yes", "YES", "y", "Y"])
+command_prefix = Config('command_prefix', default=['~', '～'])  # 消息前缀
 
 
 class EnableDirtyWordCheck:
