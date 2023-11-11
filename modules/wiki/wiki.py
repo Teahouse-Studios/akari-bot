51952a58de875140ec5dfd96412244ee9441f6e7
多羅狼 <Dorowolf314@gmail.com>
2023-11-11 14:03:00 +0800

    continue if not t

--- modules/wiki/wiki.py
+++ modules/wiki/wiki.py
@@ -83,7 +83,9 @@ async def query_pages(session: Union[Bot.MessageSession, QueryInfo], title: Unio
         for t in title:
             if prefix is not None and use_prefix:
                 t = prefix + t
-            if t and t[0] == ':':
+            if not t:
+                continue
+            if t[0] == ':':
                 if len(t) > 1:
                     query_task[start_wiki]['query'].append(t[1:])
             else:
