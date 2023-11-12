791f4a22f873da55cbf767ba891f5e8b5e864c52
多羅狼 <Dorowolf314@gmail.com>
2023-11-12 18:30:47 +0800

    Update modules.py
    sb缩进

--- modules/core/modules.py
+++ modules/core/modules.py
@@ -167,19 +167,14 @@ async def config_modules(msg: Bot.MessageSession):
         if msg.check_super_user():
             def module_reload(module, extra_modules, base_mode=False):
                 reload_count = ModulesManager.reload_module(module)
-                if base_mode:
+                if base_mode and reload_count:
                     return msg.locale.t("core.message.module.reload.success.base")
                 elif reload_count > 1:
-                    return f"{msg.locale.t('core.message.module.reload.success', module=module)}{(\
-                        '\n' if len(extra_modules) != 0 else '')}{\
-                        '\n'.join(extra_modules)}\n{msg.locale.t('core.message.module.reload.with',
-                                                                reloadCnt=reload_count - 1)}"
+                    return f"{msg.locale.t('core.message.module.reload.success', module=module)}{('\n' if len(extra_modules) != 0 else '')}{'\n'.join(extra_modules)}\n{msg.locale.t('core.message.module.reload.with', reloadCnt=reload_count - 1)}"
                 elif reload_count == 1:
-                    return f"{msg.locale.t('core.message.module.reload.success', module=module)}{(\
-                        '\n' if len(extra_modules) != 0 else '')}{\
-                        '\n'.join(extra_modules)}\n{msg.locale.t('core.message.module.reload.no_more')}"
+                    return f"{msg.locale.t('core.message.module.reload.success', module=module)}{('\n' if len(extra_modules) != 0 else '')}{'\n'.join(extra_modules)}\n{msg.locale.t('core.message.module.reload.no_more')}"
                 else:
-                    return f'{msg.locale.t("core.message.module.reload.failed")}'
+                    return msg.locale.t("core.message.module.reload.failed")
 
             for module_ in wait_config_list:
                 base_mode = False
