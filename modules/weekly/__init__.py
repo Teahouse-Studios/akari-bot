207680477a899fa2230abda19e9e513d67089858
yzhh <31803608+OasisAkari@users.noreply.github.com>
2023-11-23 15:09:18 +0800

    fix weekly

--- modules/weekly/__init__.py
+++ modules/weekly/__init__.py
@@ -1,11 +1,13 @@
 import re
 from html import unescape
+from bs4 import BeautifulSoup
 
 import ujson as json
 
 from core.builtins import Bot
 from core.builtins import Plain, Image, Url
 from core.component import module
+from core.logger import Logger
 from core.utils.http import get_url
 from core.utils.i18n import Locale
 from core.utils.image import msgchain2image
@@ -17,23 +19,28 @@ from .teahouse import get_rss as get_teahouse_rss
 async def get_weekly(with_img=False, zh_tw=False):
     locale = Locale('zh_cn' if not zh_tw else 'zh_tw')
     result = json.loads(await get_url(
-        'https://zh.minecraft.wiki/api.php?action=parse&page=Minecraft_Wiki/weekly&prop=text|revid|images&format=json' +
+        'https://zh.minecraft.wiki/api.php?action=parse&page=Minecraft_Wiki&prop=text|revid|images&format=json' +
         ('&variant=zh-tw' if zh_tw else ''),
         200))
-    html = result['parse']['text']['*']
-    text = re.sub(r'<p>', '\n', html)  # 分段
+    b_result = BeautifulSoup(result['parse']['text']['*'], 'html.parser')
+    html = b_result.find('div', id='fp-section-weekly')
+
+    content = html.find('div', class_='weekly-content')
+    text = re.sub(r'<p>', '\n', str(content))  # 分段
     text = re.sub(r'<(.*?)>', '', text, flags=re.DOTALL)  # 移除所有 HTML 标签
     text = re.sub(r'\n\n\n', '\n\n', text)  # 移除不必要的空行
     text = re.sub(r'\n*$', '', text)
     text = unescape(text)
-    img = result['parse']['images']
-    page = re.findall(r'(?<=<b><a href=").*?(?=")', html)
+    img = html.find('div', class_='weekly-image').find(class_='image')
+
+    img_filename = re.match(r'/w/(.*)', img.attrs['href'])
+    page = re.findall(r'(?<=<b><a href=").*?(?=")', str(content))
     msg_list = [Plain(locale.t("weekly.message.error.expired") if page[
-        0] == '/zh/wiki/%E7%8E%BB%E7%92%83' else locale.t(
+        0] == '/w/%E7%8E%BB%E7%92%83' else locale.t(
         "weekly.message", text=text))]
     imglink = None
-    if img:
-        get_image = await (WikiLib('https://zh.minecraft.wiki/wiki/')).parse_page_info('File:' + img[0])
+    if img_filename:
+        get_image = await (WikiLib('https://zh.minecraft.wiki/')).parse_page_info(img_filename.group(1))
         if get_image.status:
             imglink = get_image.file
     msg_list.append(
