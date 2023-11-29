3366735c312d42da2140c42e2ec673cd1a18a645
多羅狼 <Dorowolf314@gmail.com>
2023-11-29 18:33:29 +0800

    Update

--- modules/maimai/__init__.py
+++ modules/maimai/__init__.py
@@ -41,8 +41,9 @@ def get_diff(diff):
     return level
 
 
-mai = module('maimai', developers=['mai-bot', 'OasisAkari', 'DoroWolf'], alias='mai',
-             desc='{maimai.help.desc}')
+mai = module('maimai', 
+             recommend_modules='maimai_regex', developers=['mai-bot', 'OasisAkari', 'DoroWolf'], 
+             alias='mai', desc='{maimai.help.desc}')
 
 
 @mai.handle('base <constant> [<constant_max>] {{maimai.help.base}}')
