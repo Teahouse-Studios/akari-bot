d7a5c6f617b0b633fe712e88c35e822c91240337
多羅狼 <Dorowolf314@gmail.com>
2023-12-17 23:05:23 +0800

    upd tweet

--- bots/discord/slash/tweet.py
+++ bots/discord/slash/tweet.py
@@ -3,10 +3,7 @@ import discord
 from bots.discord.client import client
 from bots.discord.slash_parser import slash_parser
 
-tweet = client.create_group("tweet", "Get tweet image from tweet ID or link.")
-
-
-@tweet.command()
+@client.slash_command(description="Get tweet image from tweet ID or link.")
 @discord.option(name="tweet", description="The tweet ID or tweet link.")
-async def get(ctx: discord.ApplicationContext, tweet: str):
+async def tweet(ctx: discord.ApplicationContext, tweet: str):
     await slash_parser(ctx, tweet)
