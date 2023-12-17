8b15fe4d7bf813c7b8b74eee7f687957a146f751
多羅狼 <Dorowolf314@gmail.com>
2023-12-17 14:22:16 +0800

    .

--- modules/maimai/__init__.py
+++ modules/maimai/__init__.py
@@ -459,7 +459,7 @@ async def _(msg: Bot.MessageSession, diff: str, sid: str, score: float):
 
 @mai.command('rating <base> <score> {{maimai.help.rating}}')
 async def _(msg: Bot.MessageSession, base: float, score: float):
-    if base > 15 or base < 0:
+    if base > 15 or base < 1:
         await msg.finish(msg.locale.t('maimai.message.base_invalid'))
     if score:
         await msg.finish([Plain(computeRa(base, score))])
