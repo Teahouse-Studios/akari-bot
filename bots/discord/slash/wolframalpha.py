c48452ed0cb74b0514c71d672050aaab6035b710
多羅狼 <Dorowolf314@gmail.com>
2023-12-18 20:17:50 +0800

    fix

--- bots/discord/slash/wolframalpha.py
+++ bots/discord/slash/wolframalpha.py
@@ -3,12 +3,13 @@ import discord
 from bots.discord.client import client
 from bots.discord.slash_parser import slash_parser
 
-@client.slash_command(name="wolframalpha", description="Input a question or formula to search for WolframAlpha.")
-@discord.option(name="query", description="Enter what you want to calculate.")
-async def query(ctx: discord.ApplicationContext, query: str):
-    await slash_parser(ctx, query)
 
 wolframalpha = client.create_group("wolframalpha", "Use WolframAlpha.")
+
+@wolframalpha.command(description="Input a question or formula to search for WolframAlpha.")
+@discord.option(name="query", description="Enter what you want to calculate.")
+async def _(ctx: discord.ApplicationContext, query: str):
+    await slash_parser(ctx, query)
 
 
 @wolframalpha.command(description="Answer the question via WolframAlpha.")
