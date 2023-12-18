1d79c2360a2706009b386458800e127cae14b1e2
多羅狼 <Dorowolf314@gmail.com>
2023-12-18 20:02:39 +0800

    upd

--- bots/discord/slash/exchange_rate.py
+++ bots/discord/slash/exchange_rate.py
@@ -4,7 +4,7 @@ from bots.discord.client import client
 from bots.discord.slash_parser import slash_parser
 
 @client.slash_command(description="Convert currency prices according to the exchange rate of the day.")
-@discord.option(name="amount", description="The amount of base currency.")
+@discord.option(name="amount", default="1", description="The amount of base currency.")
 @discord.option(name="base", description="The base currency unit.")
 @discord.option(name="target", description="The target currency unit.")
 async def exchange_rate(ctx: discord.ApplicationContext, amount: float, base: str, target: str):
