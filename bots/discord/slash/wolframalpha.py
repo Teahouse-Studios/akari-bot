747683e0581de00059853bc7d7fd73a0ee17def8
多羅狼 <Dorowolf314@gmail.com>
2023-12-18 21:01:21 +0800

    Add ask

--- bots/discord/slash/wolframalpha.py
+++ bots/discord/slash/wolframalpha.py
@@ -8,7 +8,7 @@ wolframalpha = client.create_group("wolframalpha", "Use WolframAlpha.")
 
 @wolframalpha.command(description="Input a question or formula to search for WolframAlpha.")
 @discord.option(name="query", description="Enter what you want to calculate.")
-async def _(ctx: discord.ApplicationContext, query: str):
+async def query(ctx: discord.ApplicationContext, query: str):
     await slash_parser(ctx, query)
 
 
