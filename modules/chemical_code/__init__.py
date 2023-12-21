124da4bfe7ba788aeb4797c7d2f97d28b7c1d006
多羅狼 <Dorowolf314@gmail.com>
2023-12-21 21:50:25 +0800

    Update __init__.py

--- modules/chemical_code/__init__.py
+++ modules/chemical_code/__init__.py
@@ -221,7 +221,7 @@ async def chemical_code(msg: Bot.MessageSession, id=None, random_mode=True, capt
             else:
                 send_ = wait.locale.t('chemical_code.message.correct')
                 if random_mode:
-                    if (g_msg := await gained_petal(wait, 2)):
+                    if (g_msg := await gained_petal(wait, 1)):
                         send_ += '\n' + g_msg
                 await wait.send_message(send_)
                 play_state[msg.target.target_id]['active'] = False
@@ -249,7 +249,7 @@ async def chemical_code(msg: Bot.MessageSession, id=None, random_mode=True, capt
         if play_state[msg.target.target_id]['active']:
             if result.as_display(text_only=True) == csr['name']:
                 send_ = msg.locale.t('chemical_code.message.correct')
-                if (g_msg := await gained_petal(msg, 1)):
+                if (g_msg := await gained_petal(msg, 2)):
                     send_ += '\n' + g_msg
                 await result.send_message(send_)
             else:
