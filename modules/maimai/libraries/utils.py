2901dc9697833499d30a34867ecfaa78268539d2
多羅狼 <Dorowolf314@gmail.com>
2023-12-30 18:50:05 +0800

    Update utils.py

--- modules/maimai/libraries/utils.py
+++ modules/maimai/libraries/utils.py
@@ -527,9 +527,9 @@ async def get_grade_info(msg, grade):
         life = grade_data["life"]
 
         for chart in charts:
-            music = (await total_list.get()).by_id(chart['song_id'])
+            music = (await total_list.get()).by_id(str(chart['song_id']))
             level = chart['level_index']
-            chart_info.append(f"{music.title}{' (DX)' if music['type'] == 'DX' else ''} {diffs[level]} {music['level'][level]}")
+            chart_info.append(f"{music['title']}{' (DX)' if music['type'] == 'DX' else ''} {diffs[level]} {music['level'][level]}")
             
     else:
         grade_data = data[grade_type][grade_key]
@@ -540,7 +540,7 @@ async def get_grade_info(msg, grade):
 
         for i in range(4):
             music = (await total_list.get()).filter(ds=(base[0], base[1]))
-            chart_info.append(f"{music.title}{' (DX)' if music['type'] == 'DX' else ''} {diffs[level]} {music['level'][level]}")
+            chart_info.append(f"{music['title']}{' (DX)' if music['type'] == 'DX' else ''} {diffs[level]} {music['level'][level]}")
 
     output_lines = '\n'.join(chart_info)
     condition_info = f"GREAT{condition[0]}/GOOD{condition[1]}/MISS{condition[2]}/CLEAR{condition[3]}"
