5c526651259cec64ac7c7babeaddd8f6edeb8d2c
多羅狼 <Dorowolf314@gmail.com>
2024-01-10 10:58:01 +0800

    fix format

--- modules/chunithm/libraries/utils.py
+++ modules/chunithm/libraries/utils.py
@@ -44,7 +44,7 @@ async def generate_best30_text(msg, payload):
             )
         title = chart["title"]
         title = title[:17] + '...' if len(title) > 20 else title
-        line = "#{:<2} {:<4} {:<3} {:<7} {:<4} {:<2} {:>4}->{:<5.2f} {:<20}\n".format(
+        line = "#{:<2} {:>4} {:<3} {:>7} {:<4} {:<2} {:>4}->{:<5.2f} {:<20}\n".format(
             idx,
             chart["mid"],
             level,
@@ -56,7 +56,7 @@ async def generate_best30_text(msg, payload):
             title
         )
         html += line
-    html += "\n"
+    html += "Recent10\n"
     for idx, chart in enumerate(r10_records, start=1):
         level = ''.join(filter(str.isalpha, chart["level_label"]))[:3].upper()
         rank = next(
@@ -64,7 +64,7 @@ async def generate_best30_text(msg, payload):
             )
         title = chart["title"]
         title = title[:17] + '...' if len(title) > 20 else title
-        line = "#{:<2} {:<4} {:<3} {:<7} {:<4} {:<2} {:>4}->{:<5.2f} {:<20}\n".format(
+        line = "#{:<2} {:>4} {:<3} {:>7} {:<4} {:<2} {:>4}->{:<5.2f} {:<20}\n".format(
             idx,
             chart["mid"],
             level,
