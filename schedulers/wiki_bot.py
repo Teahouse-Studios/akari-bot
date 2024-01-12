ef867d2ba04671a0350615782272d67c1308c9c7
yzhh <31803608+OasisAkari@users.noreply.github.com>
2024-01-12 17:02:02 +0800

    update

---
+++ schedulers/wiki_bot.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timedelta
+
+from core.logger import Logger
+from core.queue import JobQueue
+from core.scheduler import DateTrigger, Scheduler
+from modules.wiki.utils.bot import BotAccount
+
+
+@Scheduler.scheduled_job(DateTrigger(datetime.now() + timedelta(seconds=30)))
+async def login_bots():
+    Logger.info('Start login wiki bot account...')
+    await BotAccount.login()
+    await JobQueue.trigger_hook_all('wiki_audit.login_wiki_bots', cookies=BotAccount.cookies)
+    Logger.info('Login wiki bot account done')
