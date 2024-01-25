300a7473af690194a23b0673b2ad757d6e1c0220
多羅狼 <Dorowolf314@gmail.com>
2024-01-25 13:10:05 +0800

    fix

--- modules/ip/__init__.py
+++ modules/ip/__init__.py
@@ -91,7 +91,7 @@ async def check_ip(ip: str):
             reverse = socket.getnameinfo((ip, 0), 0)
             res['reverse'] = reverse[0]
         except:
-            Logger.warning("Unable to fetch reverse DNS.")
+            Logger.warn("Unable to fetch reverse DNS.")
 
     return res
 
