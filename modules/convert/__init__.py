dda819135d0d3be72ddbac4be624b8b818a99ccc
多羅狼 <Dorowolf314@gmail.com>
2024-02-08 16:22:45 +0800

    Update i18n

--- modules/convert/__init__.py
+++ modules/convert/__init__.py
@@ -18,6 +18,6 @@ async def _(msg: Bot.MessageSession, from_val: str, to_unit: str):
         ori = ureg.parse_expression(from_val)
         res = ureg.parse_expression(from_val).to(to_unit)
     except BaseException:
-        await msg.finish(msg.locale.t("convert.message.error"))
+        await msg.finish(msg.locale.t("convert.message.invalid"))
 
     await msg.finish(f"{ori:~Pg} = {res:~Pg}")
