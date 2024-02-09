b997e0d744e5e32be76969b242375f962fc9a0b2
多羅狼 <Dorowolf314@gmail.com>
2024-02-10 01:34:38 +0800

    Update&fix

--- bots/discord/slash/user.py
+++ bots/discord/slash/user.py
@@ -4,7 +4,9 @@ from bots.discord.client import client
 from bots.discord.slash_parser import slash_parser
 
 
-@client.slash_command(description="查询一个已记录在Mediawiki上的用户信息")
-@discord.option(name="username", description="用户名")
-async def user(ctx: discord.ApplicationContext, username: str):
-    await slash_parser(ctx, username)
+@client.slash_command(name="user", description="Get information about a MediaWiki user.")
+@discord.option(name="username", description="The username, support Interwiki.")
+@discord.option(name="gamepedia_mode", choices=['false', 'true'], description="Whether to get extra information from Gamepedia site.")
+async def user(ctx: discord.ApplicationContext, username: str, gamepedia_mode: str):
+    extra = "-r" if gamepedia_mode == "true" else ""
+    await slash_parser(ctx, f"{username} {extra}")
