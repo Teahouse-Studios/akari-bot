4814d891fb6d4ae2ade85c2747b8ab9faaa10cce
yzhh <31803608+OasisAkari@users.noreply.github.com>
2024-02-21 13:41:28 +0800

    Update __init__.py

--- modules/mc_crowdin/__init__.py
+++ modules/mc_crowdin/__init__.py
@@ -2,9 +2,18 @@ from core.builtins import Bot
 from core.component import module
 
 mcr = module('mc_crowdin', developers=['OasisAkari'],
-             desc='', alias='mccrowdin')
+             desc='', alias='mccrowdin', exclude_from=['QQ', 'QQ|Group', 'QQ|Guild'])
 
 
 @mcr.hook()
+async def _(fetch: Bot.FetchTarget, ctx: Bot.ModuleHookContext):
+    await fetch.post_message('mc_crowdin', **ctx.args)
+
+
+mcrs = module('mc_crowdin', developers=['OasisAkari'],
+              desc='', alias='mccrowdin', available_for=['QQ', 'QQ|Group', 'QQ|Guild'], required_superuser=True)
+
+
+@mcrs.hook()
 async def _(fetch: Bot.FetchTarget, ctx: Bot.ModuleHookContext):
     await fetch.post_message('mc_crowdin', **ctx.args)
