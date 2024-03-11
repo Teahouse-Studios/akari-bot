fc754a7edfc79b2cc7d3e20b9aee4edfe5898176
Lynx <159797095+dicoderin@users.noreply.github.com>
2024-03-11 12:37:51 +0700

    Update __init__.py

--- modules/minecraft_news/__init__.py
+++ modules/minecraft_news/__init__.py
@@ -1,20 +1,27 @@
 from core.builtins import Bot
 from core.component import module
 
-minecraft_news = module('minecraft_news', developers=['_LittleC_', 'OasisAkari', 'Dianliang233'],
-                        recommend_modules=['feedback_news'],
-                        desc='{minecraft_news.help.minecraft_news}', alias='minecraftnews')
-
+# Modul untuk berita Minecraft
+minecraft_news = module(
+    'minecraft_news', 
+    developers=['_LittleC_', 'OasisAkari', 'Dianliang233'],
+    recommend_modules=['feedback_news'],
+    desc='{minecraft_news.help.minecraft_news}', 
+    alias='minecraftnews'
+)
 
 @minecraft_news.hook()
 async def start_check_news(fetch: Bot.FetchTarget, ctx: Bot.ModuleHookContext):
     await fetch.post_message('minecraft_news', **ctx.args)
 
-
-feedback_news = module('feedback_news', developers=['Dianliang233'], recommend_modules=['minecraft_news'],
-                       desc='{minecraft_news.help.feedback_news}',
-                       alias='feedbacknews')
-
+# Modul untuk umpan balik berita
+feedback_news = module(
+    'feedback_news', 
+    developers=['Dianliang233'], 
+    recommend_modules=['minecraft_news'],
+    desc='{minecraft_news.help.feedback_news}',
+    alias='feedbacknews'
+)
 
 @feedback_news.hook()
 async def feedback_news(fetch: Bot.FetchTarget, ctx: Bot.ModuleHookContext):
