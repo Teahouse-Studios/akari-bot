2990f92c582a3ddc55f0380419e5858d64ac1629
多羅狼 <Dorowolf314@gmail.com>
2024-03-12 22:40:27 +0800

    Tos report (#1136)

--- bots/aiocqhttp/bot.py
+++ bots/aiocqhttp/bot.py
@@ -15,9 +15,11 @@ from config import Config
 from core.builtins import EnableDirtyWordCheck, PrivateAssets, Url, Temp
 from core.scheduler import Scheduler, IntervalTrigger
 from core.parser.message import parser
+from core.tos import tos_report
 from core.types import MsgInfo, Session
 from core.utils.bot import load_prompt, init_async
 from core.utils.info import Info
+from core.utils.i18n import Locale
 from database import BotDBUtil
 
 PrivateAssets.set(os.path.abspath(os.path.dirname(__file__) + '/assets'))
@@ -157,6 +159,8 @@ async def _(event: Event):
         if event.duration >= 259200:
             result = True
         if result:
+            reason = Locale(lang).t('tos.message.reason.mute')
+            await tos_report('QQ|' + str(event.operator_id), 'QQ|Group|' + str(event.group_id), reason, banned=True)
             await bot.call_action('set_group_leave', group_id=event.group_id)
             BotDBUtil.SenderInfo('QQ|' + str(event.operator_id)).edit('isInBlockList', True)
             await bot.call_action('delete_friend', friend_id=event.operator_id)
@@ -167,6 +171,8 @@ async def _(event: Event):
     if event.user_id == int(qq_account):
         result = True
         if result:
+            reason = Locale(lang).t('tos.message.reason.kick')
+            await tos_report('QQ|' + str(event.operator_id), 'QQ|Group|' + str(event.group_id), reason, banned=True)
             BotDBUtil.SenderInfo('QQ|' + str(event.operator_id)).edit('isInBlockList', True)
             await bot.call_action('delete_friend', friend_id=event.operator_id)
 
--- core/tos.py
+++ core/tos.py
@@ -1,34 +1,38 @@
 from config import Config
 from core.builtins import Bot
+from core.utils.i18n import Locale
 from database import BotDBUtil
 
+lang = Config('locale')
+report_targets = Config('report_targets')
 WARNING_COUNTS = Config('tos_warning_counts', 5)
 
 
-async def warn_target(msg: Bot.MessageSession, reason=None):
-    if msg.check_super_user():
-        return None
-    if WARNING_COUNTS >= 1:
+async def warn_target(msg: Bot.MessageSession, reason = None):
+    if WARNING_COUNTS >= 1 and msg.check_super_user():
         current_warns = int(msg.target.sender_info.query.warns) + 1
         msg.target.sender_info.edit('warns', current_warns)
-        warn_template = [msg.locale.t("tos.warning")]
+        warn_template = [msg.locale.t("tos.message.warning")]
         if reason:
-            warn_template.append(msg.locale.t("tos.reason") + reason)
+            warn_template.append(msg.locale.t("tos.message.reason") + reason)
         if current_warns < WARNING_COUNTS:
+            await tos_report(msg.target.sender_id, msg.target.target_id, reason)
             warn_template.append(
                 msg.locale.t(
-                    'tos.warning.count',
+                    'tos.message.warning.count',
                     current_warns=current_warns,
                     warn_counts=WARNING_COUNTS))
-        if current_warns <= 2 and Config('issue_url'):
-            warn_template.append(msg.locale.t('tos.appeal', issue_url=Config('issue_url')))
-        if current_warns == WARNING_COUNTS:
-            warn_template.append(msg.locale.t('tos.warning.last'))
-        if current_warns > WARNING_COUNTS:
+            if current_warns <= 2 and Config('issue_url'):
+                warn_template.append(msg.locale.t('tos.message.appeal', issue_url=Config('issue_url')))
+        elif current_warns == WARNING_COUNTS:
+            await tos_report(msg.target.sender_id, msg.target.target_id, reason)
+            warn_template.append(msg.locale.t('tos.message.warning.last'))
+        elif current_warns > WARNING_COUNTS:
             msg.target.sender_info.edit('isInBlockList', True)
-            warn_template.append(msg.locale.t('tos.banned'))
+            await tos_report(msg.target.sender_id, msg.target.target_id, reason, banned=True)
+            warn_template.append(msg.locale.t('tos.message.banned'))
             if Config('issue_url'):
-                warn_template.append(msg.locale.t('tos.appeal', issue_url=Config('issue_url')))
+                warn_template.append(msg.locale.t('tos.message.appeal', issue_url=Config('issue_url')))
         await msg.send_message('\n'.join(warn_template))
 
 
@@ -36,9 +40,26 @@ async def pardon_user(user: str):
     BotDBUtil.SenderInfo(user).edit('warns', 0)
 
 
-async def warn_user(user: str, count=1):
+async def warn_user(user: str, count = 1):
     current_warns = int(BotDBUtil.SenderInfo(user).query.warns) + count
     BotDBUtil.SenderInfo(user).edit('warns', current_warns)
     if current_warns > WARNING_COUNTS and WARNING_COUNTS >= 1:
         BotDBUtil.SenderInfo(user).edit('isInBlockList', True)
     return current_warns
+
+
+async def tos_report(sender, target, reason = None, banned = False):
+    locale = Locale(lang)
+    if report_targets:
+        warn_template = [locale.t("tos.message.report", sender=sender, target=target)]
+        if reason:
+            warn_template.append(locale.t("tos.message.reason") + reason)
+        if banned:
+            action = locale.t("tos.message.action.banned")
+        else:
+            action = locale.t("tos.message.action.warning")
+        warn_template.append(locale.t("tos.message.action") + action)
+
+        for target in report_targets:
+            if f := await Bot.FetchTarget.fetch_target(target):
+                await f.send_direct_message('\n'.join(warn_template), disable_secret_check=True)
