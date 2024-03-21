aa2a6f09c478304358a76b08d0ee7478a01421d2
多羅狼 <Dorowolf314@gmail.com>
2024-03-21 16:11:11 +0800

    🙃

--- modules/wiki/utils/__init__.py
+++ modules/wiki/utils/__init__.py
@@ -13,6 +13,7 @@ from .rc_qq import rc_qq
 
 rc_ = module('rc', developers=['OasisAkari'], recommend_modules='wiki')
 
+@rc_.command()
 @rc_.command('[-l] {{wiki.help.rc}}',
              options_desc={'-l': '{help.option.l}'},
              available_for=['QQ', 'QQ|Group'])
@@ -47,6 +48,7 @@ async def rc_loader(msg: Bot.MessageSession):
 ab_ = module('ab', developers=['OasisAkari'], recommend_modules='wiki')
 
 
+@ab_.command()
 @ab_.command('[-l] {{wiki.help.ab}}',
            options_desc={'-l': '{help.option.l}'},
            available_for=['QQ', 'QQ|Group'])
