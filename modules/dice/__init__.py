b09603c45cb1f73157101840223233f16132b349
多羅狼 <Dorowolf314@gmail.com>
2024-03-23 13:38:39 +0800

    Update __init__.py

--- modules/dice/__init__.py
+++ modules/dice/__init__.py
@@ -1,6 +1,6 @@
 from core.builtins import Bot
 from core.component import module
-from .dice import GenerateMessage
+from .dice import generate_message
 
 dice = module('dice', alias=['rd', 'roll'], developers=['Light-Beacon'], desc='{dice.help.desc}')
 
@@ -15,7 +15,7 @@ async def _(msg: Bot.MessageSession, dices: str, dc = None):
         times = '1'
     if not times.isdigit():
         await msg.finish(msg.locale.t('dice.message.N.invalid') + times)
-    await msg.finish(await GenerateMessage(msg, dices, int(times), dc))
+    await msg.finish(await generate_message(msg, dices, int(times), dc))
 
 
 @dice.regex(r"[扔投掷擲丢]([0-9]*)?[个個]([0-9]*面)?骰子?([0-9]*次)?", desc="{dice.help.regex.desc}")
@@ -23,7 +23,7 @@ async def _(msg: Bot.MessageSession):
     groups = msg.matched_msg.groups()
     dice_type = groups[1][:-1] if groups[1] else '6'
     roll_time = groups[2][:-1] if groups[2] else '1'
-    await msg.finish(await GenerateMessage(msg, f'{groups[0]}D{dice_type}', int(roll_time), None))
+    await msg.finish(await generate_message(msg, f'{groups[0]}D{dice_type}', int(roll_time), None))
 
 
 @dice.command('rule {{dice.help.rule}}', required_admin=True)
