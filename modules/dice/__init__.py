89715b9ae582d7b98e6e998d0ea3019b08429873
多羅狼 <Dorowolf314@gmail.com>
2024-03-24 00:04:05 +0800

    Update default dice

--- modules/dice/__init__.py
+++ modules/dice/__init__.py
@@ -21,7 +21,8 @@ async def _(msg: Bot.MessageSession, dices: str, dc = None):
 @dice.regex(r"[扔投掷擲丢]([0-9]*)?[个個]([0-9]*面)?骰子?([0-9]*次)?", desc="{dice.help.regex.desc}")
 async def _(msg: Bot.MessageSession):
     groups = msg.matched_msg.groups()
-    dice_type = groups[1][:-1] if groups[1] else '6'
+    default_type = msg.data.options.get('dice_default_face') if msg.data.options.get('dice_default_face') else '6'
+    dice_type = groups[1][:-1] if groups[1] else default_type
     roll_time = groups[2][:-1] if groups[2] else '1'
     await msg.finish(await process_expression(msg, f'{groups[0]}D{dice_type}', int(roll_time), None))
 
