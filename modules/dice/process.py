4545a02b25ef28e91af9ab0904d885887db618f6
多羅狼 <Dorowolf314@gmail.com>
2024-03-25 12:50:51 +0800

    fix

--- modules/dice/process.py
+++ modules/dice/process.py
@@ -39,9 +39,6 @@ se = SimpleEval()
 se.functions.update(math_funcs)
 
 
-
-
-
 async def process_expression(msg, expr: str, dc, use_markdown = False):
     if not all([MAX_DICE_COUNT > 0, MAX_ROLL_TIMES > 0, MAX_OUTPUT_CNT > 0,
                 MAX_OUTPUT_LEN > 0, MAX_DETAIL_CNT > 0, MAX_ITEM_COUNT > 0]):
@@ -143,6 +140,8 @@ def generate_dice_message(msg, expr, dice_expr_list, dice_count, times, dc, use_
     success_num = 0
     fail_num = 0
     output = msg.locale.t('dice.message.output')
+    if '#' in expr:
+        expr = expr.partition('#')[2]
     if times <= 0 or times > MAX_ROLL_TIMES:
         return DiceValueError(msg,
                                  msg.locale.t("dice.message.error.value.N.out_of_range", max=MAX_ROLL_TIMES),
