37007209d9701ffdfa1e6fc35a9e962e3f91177a
多羅狼 <Dorowolf314@gmail.com>
2024-04-06 02:26:18 +0800

    :thinking:

--- config/__init__.py
+++ config/__init__.py
@@ -76,6 +76,8 @@ class CFG:
         value = value_s.get(q)
         if value is None:
             value = value_n.get(q)
+        if value is None:
+            print(f'[Config] Config {q} not found, is it configured?')
         if value is None and default is not None:
             return default
         if default is not None:
