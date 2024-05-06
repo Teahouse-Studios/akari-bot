9b991ceb167b32781e8e58ff8fdfd7f1a0c64019
多羅狼 <Dorowolf314@gmail.com>
2024-05-06 16:55:52 +0800

    update osu dbutils

--- modules/osu/__init__.py
+++ modules/osu/__init__.py
@@ -1,19 +1,22 @@
+from config import Config
 from core.builtins import Bot
 from core.component import module
+from core.exceptions import ConfigValueError
+from .dbutils import OsuBindInfoManager
+from .utils import get_profile_name
 
 osu = module('osu', developers=['DoroWolf'], desc='{osu.help.desc}')
 
 @osu.handle('bind <username> {{osu.help.bind}}')
 async def _(msg: Bot.MessageSession, username: str):
+    if not Config('osu_api_key', cfg_type=str):
+        raise ConfigValueError(msg.locale.t('error.config.secret.not_found'))
     code: str = username.lower()
     getcode = await get_profile_name(code)
     if getcode:
         bind = OsuBindInfoManager(msg).set_bind_info(username=getcode[0])
         if bind:
-            if getcode[1]:
-                m = f'{getcode[1]}({getcode[0]})'
-            else:
-                m = getcode[0]
+            m = f'{getcode[1]}({getcode[0]})'
             await msg.finish(msg.locale.t('osu.message.bind.success') + m)
     else:
         await msg.finish(msg.locale.t('osu.message.bind.failed'))
@@ -21,6 +24,8 @@ async def _(msg: Bot.MessageSession, username: str):
 
 @osu.handle('unbind {{osu.help.unbind}}')
 async def _(msg: Bot.MessageSession):
+    if not Config('osu_api_key', cfg_type=str):
+        raise ConfigValueError(msg.locale.t('error.config.secret.not_found'))
     unbind = OsuBindInfoManager(msg).remove_bind_info()
     if unbind:
         await msg.finish(msg.locale.t('osu.message.unbind.success'))
