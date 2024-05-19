2d3326c950a5eb69ab86e941576b17507bf6a6da
xtex <xtexchooser@duck.com>
2024-05-19 12:05:56 +0800

    Fix iw set on platforms with audit unavailable (#1188)

--- modules/wiki/set.py
+++ modules/wiki/set.py
@@ -43,18 +43,18 @@ async def _(msg: Bot.MessageSession, interwiki: str, wikiurl: str):
     check = await WikiLib(wikiurl, headers=target.get_headers()).check_wiki_available()
     if check.available:
         if msg.target.target_from in audit_available_list:
-            if not check.value.in_blocklist or check.value.in_allowlist:
-                result = target.config_interwikis(interwiki, check.value.api, let_it=True)
-                if result and enable_urlmanager and not check.value.in_allowlist:
-                    prompt = '\n' + msg.locale.t("wiki.message.wiki_audit.untrust")
-                    if Config("wiki_whitelist_url", cfg_type=str):
-                        prompt += '\n' + msg.locale.t("wiki.message.wiki_audit.untrust.address",
-                                                      url=Config("wiki_whitelist_url", cfg_type=str))
-                else:
-                    prompt = ''
-                await msg.finish(msg.locale.t("wiki.message.iw.add.success", iw=interwiki, name=check.value.name) + prompt)
-            else:
+            if check.value.in_blocklist and not check.value.in_allowlist:
                 await msg.finish(msg.locale.t("wiki.message.invalid.blocked", name=check.value.name))
+                return
+        result = target.config_interwikis(interwiki, check.value.api, let_it=True)
+        if result and enable_urlmanager and not check.value.in_allowlist:
+            prompt = '\n' + msg.locale.t("wiki.message.wiki_audit.untrust")
+            if Config("wiki_whitelist_url", cfg_type=str):
+                prompt += '\n' + msg.locale.t("wiki.message.wiki_audit.untrust.address",
+                                              url=Config("wiki_whitelist_url", cfg_type=str))
+        else:
+            prompt = ''
+            await msg.finish(msg.locale.t("wiki.message.iw.add.success", iw=interwiki, name=check.value.name) + prompt)
     else:
         result = msg.locale.t('wiki.message.error.add') + \
             ('\n' + msg.locale.t('wiki.message.error.info') + check.message if check.message != '' else '')
