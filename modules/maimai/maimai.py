005e7ff11226bd03482e349564b68d2ff46c351a
多羅狼 <Dorowolf314@gmail.com>
2024-06-19 02:44:05 +0800

    Update maimai.py
    bluh

--- modules/maimai/maimai.py
+++ modules/maimai/maimai.py
@@ -236,10 +236,9 @@ async def _(msg: Bot.MessageSession, username: str = None):
         payload = {'username': username, 'b50': True}
         try:
             img = await generate(msg, payload)
+            await msg.finish([BImage(img)])
         except BaseException:
             traceback.print_exc()
-    await msg.finish([BImage(img)])
-
 
 @mai.command('id <id> [<diff>] {{maimai.help.id}}')
 @mai.command('song <id_or_alias> [-d <diff>] {{maimai.help.song}}',
