844330874960d642e6e0aaf3e0f62d828092bb5e
多羅狼 <Dorowolf314@gmail.com>
2024-06-21 17:05:32 +0800

    wtf

--- modules/ip/__init__.py
+++ modules/ip/__init__.py
@@ -122,46 +122,12 @@ async def format_ip(msg, info: Dict[str, Any]):
     }
 
     return f'''\
-{
-        info['ip']} {
-        msg.locale.t('ip.message.type')}IPv{
-            info['version']} {
-                ip_property[
-                    info['ip_property']]}{
-                        msg.locale.t('ip.message.ip_property')}{
-                            f""" {
-                                msg.locale.t('ip.message.real_ip')}{
-                                    info['real_ip']}""" if info['real_ip'] else ''}{
-                                        f""" {
-                                            msg.locale.t('ip.message.location')}{
-                                                f"{
-                                                    info['city']}, " if info['city'] else ''}{
-                                                        f"{
-                                                            info['region']}, " if info['region'] else ''}{
-                                                                info['country']}""" if info['country'] else ''}{
-                                                                    f" ({
-                                                                        parse_coordinate(
-                                                                            'longitude',
-                                                                            info['longitude'])}, {
-                                                                                parse_coordinate(
-                                                                                    'latitude',
-                                                                                    info['latitude'])})" if info['longitude'] and info['latitude'] else ''}{
-                                                                                        f""" {
-                                                                                            msg.locale.t('ip.message.postal_code')}{
-                                                                                                info['postal_code']}""" if info['postal_code'] else ''}{
-                                                                                                    f""" {
-                                                                                                        msg.locale.t('ip.message.organization')}{
-                                                                                                            info['organization']}""" if info['organization'] else ''}{
-                                                                                                                f""" {
-                                                                                                                    msg.locale.t('ip.message.asn')}{
-                                                                                                                        info['asn']}""" if info['asn'] else ''}{
-                                                                                                                            f" ({
-                                                                                                                                info['asn_organization']}) " if info['asn_organization'] else ''}{
-                                                                                                                                    f""" {
-                                                                                                                                        msg.locale.t('ip.message.utc')}UTC{
-                                                                                                                                            (
-                                                                                                                                                info['offset'] /
-                                                                                                                                                3600):+g}""" if info['offset'] else ''}{
-                                                                                                                                                    f""" {
-                                                                                                                                                        msg.locale.t('ip.message.reverse')}{
-                                                                                                                                                            info['reverse']}""" if info['reverse'] and info['reverse'] != info['ip'] else ''}'''
+{info['ip']}
+{msg.locale.t('ip.message.type')}IPv{info['version']} {ip_property[info['ip_property']]}{msg.locale.t('ip.message.ip_property')}{f"""
+{msg.locale.t('ip.message.real_ip')}{info['real_ip']}""" if info['real_ip'] else ''}{f"""
+{msg.locale.t('ip.message.location')}{f"{info['city']}, " if info['city'] else ''}{f"{info['region']}, " if info['region']  else ''}{info['country']}""" if info['country'] else ''}{f" ({parse_coordinate('longitude', info['longitude'])}, {parse_coordinate('latitude', info['latitude'])})" if info['longitude'] and info['latitude'] else ''}{f"""
+{msg.locale.t('ip.message.postal_code')}{info['postal_code']}""" if info['postal_code'] else ''}{f"""
+{msg.locale.t('ip.message.organization')}{info['organization']}""" if info['organization'] else ''}{f"""
+{msg.locale.t('ip.message.asn')}{info['asn']}""" if info['asn'] else ''}{f" ({info['asn_organization']}) " if info['asn_organization'] else ''}{f"""
+{msg.locale.t('ip.message.utc')}UTC{(info['offset'] / 3600):+g}""" if info['offset'] else ''}{f"""
+{msg.locale.t('ip.message.reverse')}{info['reverse']}""" if info['reverse'] and info['reverse'] != info['ip'] else ''}'''
