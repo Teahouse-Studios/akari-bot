6cc09fd206b35f32e03db4761182e6e9b178eaf6
多羅狼 <Dorowolf314@gmail.com>
2024-06-28 15:07:24 +0800

    typo

--- core/builtins/utils/__init__.py
+++ core/builtins/utils/__init__.py
@@ -8,7 +8,7 @@ from core.types import PrivateAssets, Secret
 default_confirm_command = ['是', '对', '對', 'yes', 'Yes', 'YES', 'y', 'Y']
 default_command_prefix = ['~', '～']
 confirm_command = [i for i in Config('confirm_command', default_confirm_command) if i.strip()] or default_confirm_command
-command_prefix = [i for i in Config('command_prefix', default_command_prefix) if i.strip()] or default_confirm_command  # 消息前缀
+command_prefix = [i for i in Config('command_prefix', default_command_prefix) if i.strip()] or default_command_prefix  # 消息前缀
 
 
 class EnableDirtyWordCheck:
