70e94bd9f7ea0279bd80419069a51a1bea830e64
多羅狼 <Dorowolf314@gmail.com>
2024-06-28 13:22:03 +0800

    Add default confirm_command & command_prefix

--- core/builtins/utils/__init__.py
+++ core/builtins/utils/__init__.py
@@ -5,9 +5,10 @@ from datetime import datetime
 from config import Config
 from core.types import PrivateAssets, Secret
 
-
-confirm_command = [i for i in Config('confirm_command', ['是', '对', '對', 'yes', 'Yes', 'YES', 'y', 'Y']) if i.strip()]
-command_prefix = [i for i in Config('command_prefix', ['~', '～']) if i.strip()]  # 消息前缀
+default_confirm_command = ['是', '对', '對', 'yes', 'Yes', 'YES', 'y', 'Y']
+default_command_prefix = ['~', '～']
+confirm_command = [i for i in Config('confirm_command', default_confirm_command) if i.strip()] or default_confirm_command
+command_prefix = [i for i in Config('command_prefix', default_command_prefix) if i.strip()] or default_command_prefix  # 消息前缀
 
 
 class EnableDirtyWordCheck:
