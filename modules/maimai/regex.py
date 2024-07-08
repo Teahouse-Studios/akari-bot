6384c65a70cfcdeb3819f9c4de008dcd13d52475
多羅狼 <Dorowolf314@gmail.com>
2024-07-08 20:31:52 +0800

    Update regex.py

--- modules/maimai/regex.py
+++ modules/maimai/regex.py
@@ -31,7 +31,7 @@ async def _(msg: Bot.MessageSession):
             res = msg.locale.t("maimai.message.disambiguation") + "\n"
             for sid in sorted(sid_list, key=int):
                 s = (await total_list.get()).by_id(sid)
-                res += f"{s['id']} -{s['title']}{' (DX)' if s['type'] == 'DX' else ''}\n"
+                res += f"{s['id']} - {s['title']}{' (DX)' if s['type'] == 'DX' else ''}\n"
             res += msg.locale.t("maimai.message.song.prompt", prefix=msg.prefixes[0])
             await msg.finish(res)
         else:
