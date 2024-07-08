0175949316db15fa8222b6fe81e0ef2f8c83c313
多羅狼 <Dorowolf314@gmail.com>
2024-07-08 16:32:25 +0800

    fix

--- bots/aiocqhttp/info.py
+++ bots/aiocqhttp/info.py
@@ -5,7 +5,8 @@ client_name = 'QQ'
 
 def qq_frame_type() -> str:
     '''获取正在使用的QQ机器人框架'''
-    frame_type = Config('qq_frame_type', cfg_type=str).lower()
+    frame_type = (Config('qq_frame_type', cfg_type=str) or '').lower()
+
     if frame_type in ['llonebot', 'napcat', 'napcatqq', ] or Config('use_llonebot', False):
         return 'ntqq'
     elif frame_type in ['shamrock', 'openshamrock', ] or Config('use_shamrock', False):
