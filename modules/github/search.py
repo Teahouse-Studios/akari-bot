11d8dba3c2086717a71eeb068f4e1ac66c7bc53f
多羅狼 <Dorowolf314@gmail.com>
2024-07-31 13:52:58 +0800

    u

--- modules/github/search.py
+++ modules/github/search.py
@@ -1,34 +1,28 @@
-import traceback
-
 from core.builtins import Bot, Url
 from core.dirty_check import rickroll
-from core.logger import Logger
 from core.utils.http import get_url
 from modules.github.utils import dirty_check, darkCheck
 
 
 async def search(msg: Bot.MessageSession, keyword: str):
-    try:
-        result = await get_url('https://api.github.com/search/repositories?q=' + keyword, 200,
-                               fmt='json')
-        if result['total_count'] == 0:
-            message = msg.locale.t("github.message.search.not_found")
-        else:
-            items = result['items']
-            items_out = []
-            for item in items:
-                try:
-                    items_out.append(str(item['full_name'] + ': ' + str(Url(item['html_url']))))
-                except TypeError:
-                    continue
-            message = msg.locale.t("github.message.search") + '\n' + '\n'.join(items_out[0:5])
-            if result['total_count'] > 5:
-                message += '\n' + msg.locale.t("message.collapse", amount="5")
+    result = await get_url('https://api.github.com/search/repositories?q=' + keyword, 200,
+                           fmt='json')
+    if result['total_count'] == 0:
+        message = msg.locale.t("github.message.search.not_found")
+    else:
+        items = result['items']
+        items_out = []
+        for item in items:
+            try:
+                items_out.append(str(item['full_name'] + ': ' + str(Url(item['html_url']))))
+            except TypeError:
+                continue
+        message = msg.locale.t("github.message.search") + '\n' + '\n'.join(items_out[0:5])
+        if result['total_count'] > 5:
+            message += '\n' + msg.locale.t("message.collapse", amount="5")
 
-        is_dirty = await dirty_check(message) or darkCheck(message)
-        if is_dirty:
-            await msg.finish(rickroll(msg))
+    is_dirty = await dirty_check(message) or darkCheck(message)
+    if is_dirty:
+        await msg.finish(rickroll(msg))
 
-        await msg.finish(message)
-    except BaseException:
-        Logger.error(traceback.format_exc())
+    await msg.finish(message)
