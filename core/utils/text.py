326fdf533ca8bf3be1b73e9cdc7c58b3f968ce7f
多羅狼 <dorowolf314@gmail.com>
2024-09-22 14:35:37 +0800

    u

--- core/utils/text.py
+++ core/utils/text.py
@@ -1,8 +1,8 @@
 import random
 import re
-import sys
 from datetime import timedelta
 from typing import TypeVar
+
 from ff3 import FF3Cipher
 
 from config import Config
@@ -10,18 +10,13 @@ from config import Config
 
 T = TypeVar("T", str, bytes, bytearray)
 
-if sys.version_info.minor > 8:  # PY39
-    def remove_suffix(string: T, suffix: T) -> T:
-        return string.removesuffix(suffix)
 
-    def remove_prefix(string: T, prefix: T) -> T:
-        return string.removeprefix(prefix)
-else:
-    def remove_suffix(string: T, suffix: T) -> T:
-        return string[:-len(suffix)] if string.endswith(suffix) else string
+def remove_suffix(string: T, suffix: T) -> T:
+    return string.removesuffix(suffix)
 
-    def remove_prefix(string: T, prefix: T) -> T:
-        return string[len(prefix):] if string.startswith(prefix) else string
+
+def remove_prefix(string: T, prefix: T) -> T:
+    return string.removeprefix(prefix)
 
 
 def isfloat(num_str: str) -> bool:
