1d53a8460eb5c30571b9d0fe91c25f551719a77c
多羅狼 <dorowolf314@gmail.com>
2024-10-05 21:18:04 +0800

    u

--- bots/aiocqhttp/utils.py
+++ bots/aiocqhttp/utils.py
@@ -27,7 +27,8 @@ def qq_frame_type() -> str:
 
 class CQCodeHandler:
     get_supported = ['at', 'dice', 'face', 'forward', 'json', 'poke', 'rps', 'text']
-    pattern = re.compile(r'\[CQ:(\w+),[^\]]*\]')
+    pattern1 = re.compile(r'\[CQ:\w+,[^\]]*\]')
+    pattern2 = re.compile(r'\[CQ:(\w+),[^\]]*\]')
 
     @staticmethod
     def filter_cq(match: str):
