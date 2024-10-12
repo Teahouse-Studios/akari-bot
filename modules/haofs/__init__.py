4a91095ae89240c2278d9b984e7d4617b11907a3
好耶！ <86039582+cloudw233@users.noreply.github.com>
2024-10-12 18:58:32 +0800

    新模块#haofs

---
+++ modules/haofs/__init__.py
@@ -0,0 +1,24 @@
+from hfs import Account
+from core.builtins import Bot, Plain
+from core.component import module
+
+def hfs_(name,id):
+    student = Account()
+    email, password = student.register(name, id, password='password')
+    student.login(email,password)
+    exam = student.get_exam(0)
+    return exam.data
+
+hfs_m = module('haofs', desc='基于 ghplvh/haofs 项目开发，用于查询好分数排名等')
+@hfs_m.command('<姓名> <考号> {查询排名}')
+async def _(msg:Bot.MessageSession):
+    try:
+        name = msg.parsed_msg['<姓名>']
+        id = msg.parsed_msg['<考号>']
+        haofs = hfs_(name,id)
+        await msg.sendMessage([Plain(f"{name}({id}) 的 {haofs.get('name')} 成绩详情："),
+                               Plain(f"分数：{haofs.get('score')}/{haofs.get('manfen')},"),
+                               Plain(f"排名：班级 {haofs.get('classRank')}({haofs.get('classRankPart')}) | 级部 {haofs.get('gradeRank')}({haofs.get('gradeRankPart')})")])
+    except Exception:
+        await msg.sendMessage('发生错误：考号或学生姓名错误')
+
