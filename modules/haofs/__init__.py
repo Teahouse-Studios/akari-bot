f6df811ddbaf5bb78577ee877c99a413dd5eaa12
好耶！ <86039582+cloudw233@users.noreply.github.com>
2024-10-12 21:38:08 +0800

    fxxk u gil

--- modules/haofs/__init__.py
+++ modules/haofs/__init__.py
@@ -1,11 +1,10 @@
 from hfs import Account
 from core.builtins import Bot, Plain
 from core.component import module
-from concurrent.futures import ThreadPoolExecutor
 
 import asyncio
 
-def hfs_(name,id):
+async def hfs_(name,id):
     student = Account()
     email, password = student.register(name, id, password='password')
     student.login(email,password)
@@ -15,21 +14,15 @@ def hfs_(name,id):
 hfs_m = module('haofs', desc='基于 ghplvh/haofs 项目开发，用于查询好分数排名等')
 @hfs_m.command('<姓名> <考号> {查询排名}')
 async def _(msg:Bot.MessageSession):
-    def send():
+    async def send():
         try:
             name = msg.parsed_msg['<姓名>']
             id = msg.parsed_msg['<考号>']
-            haofs = hfs_(name, id)
-            asyncio.run(msg.sendMessage([Plain(f"{name}({id}) 的 {haofs.get('name')} 成绩详情："),
-                                   Plain(f"分数：{haofs.get('score')}/{haofs.get('manfen')},"),
-                                   Plain(
-                                       f"排名：班级 {haofs.get('classRank')}({haofs.get('classRankPart')}) | 级部 {haofs.get('gradeRank')}({haofs.get('gradeRankPart')})")])
-                        )
+            haofs = await hfs_(name,id)
+            await msg.sendMessage([Plain(f"{name}({id}) 的 {haofs.get('name')} 成绩详情："),
+                                  Plain(f"分数：{haofs.get('score')}/{haofs.get('manfen')},"),
+                                  Plain(f"排名：班级 {haofs.get('classRank')}({haofs.get('classRankPart')}) | 级部 {haofs.get('gradeRank')}({haofs.get('gradeRankPart')})")])
         except Exception:
-            asyncio.run(msg.sendMessage('发生错误'))
-    with ThreadPoolExecutor() as t:
-        t.submit(send)
-
-
-
+             await msg.sendMessage('发生错误')
+    await asyncio.to_thread(send)
 
