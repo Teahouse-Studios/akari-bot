c12bd87066020533b77ac994bad8fb06b58419ba
yzhh <31803608+OasisAkari@users.noreply.github.com>
2024-10-21 19:27:17 +0800

    Update pre_build.py

--- .github/scripts/pre_build.py
+++ .github/scripts/pre_build.py
@@ -4,10 +4,10 @@ import os
 if __name__ == "__main__":
     modules_folder_list = os.listdir('modules')
     with open('assets/modules_list.json', 'w') as f:
-        json.dump(modules_folder_list, f)
+        f.write(json.dumps(modules_folder_list))
     schedulers_folder_list = os.listdir('schedulers')
     with open('assets/schedulers_list.json', 'w') as f:
-        json.dump(schedulers_folder_list, f)
+        f.write(json.dumps(schedulers_folder_list))
     slash_list = os.listdir('bots/discord/slash')
     with open('assets/discord_slash_list.json', 'w') as f:
-        json.dump(slash_list, f)
+        f.write(json.dumps(slash_list))
