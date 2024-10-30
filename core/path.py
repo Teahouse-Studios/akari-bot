0fc33a2fca05861ffcce2fdb98e573ce99a4b72a
多羅狼 <dorowolf314@gmail.com>
2024-10-30 12:28:18 +0800

    fix

--- core/path.py
+++ core/path.py
@@ -16,7 +16,7 @@ templates_path = join(assets_path, 'templates')
 
 # 字体文件路径
 noto_sans_bold_path = join(fonts_path, 'Noto Sans CJK Bold.otf')
-noto_sans_demilight_path = join(fonts_path, 'Noto Sans CJK Demilight.otf')
+noto_sans_demilight_path = join(fonts_path, 'Noto Sans CJK DemiLight.otf')
 noto_sans_symbol_path = join(fonts_path, 'Noto Sans Symbols2 Regular.ttf')
 
 nunito_light_path = join(fonts_path, 'Nunito Light.ttf')
