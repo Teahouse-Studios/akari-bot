34a01d46c58858142ec16648a0132c0eddaa354c
多羅狼 <Dorowolf314@gmail.com>
2024-11-04 23:04:51 +0800

    Update info.py

--- bots/ntqq/info.py
+++ bots/ntqq/info.py
@@ -1,8 +1,8 @@
 client_name = 'QQ'
 
-sender_name = f'{client_name}|Open'
+sender_name = f'{client_name}|Bot'
 sender_name_list = [sender_name]
 
-target_group_name = f'{client_name}|Open|Group'
-target_guild_name = f'{client_name}|Open|Guild'
+target_group_name = f'{client_name}|Bot|Group'
+target_guild_name = f'{client_name}|Bot|Guild'
 target_name_list = [target_guild_name]
