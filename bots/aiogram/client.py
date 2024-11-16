6a46b3f329674920059e8c5b9d36aa3d7fa7c6ae
yzhh <31803608+OasisAkari@users.noreply.github.com>
2024-11-17 04:43:21 +0800

    u

--- bots/aiogram/client.py
+++ bots/aiogram/client.py
@@ -1,11 +1,12 @@
 from aiogram import Bot, Dispatcher
 
 from core.config import Config
+from core.utils.http import proxy
 
 token = Config('telegram_token', cfg_type=str)
 
 if token:
-    bot = Bot(token=token)
+    bot = Bot(token=token, proxy=Config('proxy', cfg_type=str))
     dp = Dispatcher()
 else:
     bot = dp = False
