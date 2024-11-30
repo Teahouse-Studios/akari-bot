037be4b486ca46b6988934e50a2f722e576a6bb0
多羅狼 <Dorowolf314@gmail.com>
2024-11-30 20:27:13 +0800

    Update __init__.py

--- modules/oba/__init__.py
+++ modules/oba/__init__.py
@@ -14,7 +14,7 @@ oba = module('oba', desc='{oba.help.desc}', developers='WorldHim')
 def size_convert(value):
     units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
     size = 1024.0
-    for i, unit in enumerate(units):
+    for _, unit in enumerate(units):
         if (value / size) < 1:
             return '%.2f%s' % (value, ' ' + unit)
         value /= size
