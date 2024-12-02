b41ed33be5240b4650697c3fe38ea58bbeda1810
多羅狼 <Dorowolf314@gmail.com>
2024-12-02 09:58:22 +0800

    Update __init__.py

--- core/builtins/utils/__init__.py
+++ core/builtins/utils/__init__.py
@@ -1,8 +1,10 @@
 from core.config import Config
 from core.constants import PrivateAssets, Secret, confirm_command, command_prefix
 
-confirm_command = Config('confirm_command', confirm_command)  # 确认指令
-command_prefix = Config('command_prefix', command_prefix)  # 指令前缀
+confirm_command = filter(str.strip, Config('confirm_command',
+                         default_confirm_command)) or default_confirm_command  # 确认指令
+command_prefix = filter(str.strip, Config('confirm_command',
+                        default_command_prefix)) or default_command_prefix  # 消息前缀
 
 
 __all__ = ["confirm_command", "command_prefix", "PrivateAssets", "Secret"]
