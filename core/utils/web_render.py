ad9f41a6cf99d06dc642b6aece7f0542b19ce2f2
yzhh <31803608+OasisAkari@users.noreply.github.com>
2024-12-06 13:35:55 +0800

    Update web_render.py

--- core/utils/web_render.py
+++ core/utils/web_render.py
@@ -10,7 +10,7 @@ web_render = Config('web_render', secret=True, get_url=True)
 web_render_local = Config('web_render_local', get_url=True)
 
 
-def webrender(method: str = '', url: str = '', use_local: bool = True) -> Union[str, None]:
+def webrender(method: str = '', url: str = '', use_local: bool = True, ignore_status=False) -> Union[str, None]:
     '''根据请求方法生成 WebRender URL。
 
     :param method: API 方法。
@@ -18,15 +18,15 @@ def webrender(method: str = '', url: str = '', use_local: bool = True) -> Union[
     :param use_local: 是否使用本地 WebRender。
     :returns: 生成的 WebRender URL。
     '''
-    if use_local and not Info.web_render_local_status:
+    if use_local and (not Info.web_render_local_status or ignore_status):
         use_local = False
     if method == 'source':
-        if Info.web_render_status:
+        if Info.web_render_status or ignore_status:
             return f'{(web_render_local if use_local else web_render)}source?url={url}'
         else:
             return url
     else:
-        if Info.web_render_status:
+        if Info.web_render_status or ignore_status:
             return (web_render_local if use_local else web_render) + method
         else:
             return None
@@ -47,7 +47,7 @@ async def check_web_render() -> Tuple[bool, bool]:
     if web_render_status:
         try:
             Logger.info('[WebRender] Checking WebRender status...')
-            await get_url(webrender('source', ping_url), 200, request_private_ip=True)
+            await get_url(webrender('source', ping_url, ignore_status=True), 200, request_private_ip=True)
             Logger.info('[WebRender] WebRender is working as expected.')
         except Exception:
             Logger.error('[WebRender] WebRender is not working as expected.')
