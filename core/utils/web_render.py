fa8dddf9159377f8753737999228236fee3fc378
多羅狼 <Dorowolf314@gmail.com>
2024-12-07 17:04:37 +0800

    Update web_render.py

--- core/utils/web_render.py
+++ core/utils/web_render.py
@@ -1,5 +1,5 @@
 import traceback
-from typing import Tuple, Union
+from typing import Tuple, Optional, Union
 
 from core.config import Config
 from core.constants.info import Info
@@ -10,7 +10,7 @@ web_render = Config('web_render', secret=True, get_url=True)
 web_render_local = Config('web_render_local', get_url=True)
 
 
-def webrender(method: str = '', url: str = '', use_local: bool = True, ignore_status=False) -> str:
+def webrender(method: str = 'source', url: Optional[str] = None, use_local: bool = True, _ignore_status=False) -> str:
     '''根据请求方法生成 WebRender URL。
 
     :param method: API 方法。
@@ -18,15 +18,16 @@ def webrender(method: str = '', url: str = '', use_local: bool = True, ignore_st
     :param use_local: 是否使用本地 WebRender。
     :returns: 生成的 WebRender URL。
     '''
-    if use_local and (not Info.web_render_local_status or ignore_status):
+    if use_local and (not Info.web_render_local_status or _ignore_status):
         use_local = False
     if method == 'source':
-        if Info.web_render_status or ignore_status:
+        url = '' if not url else url
+        if Info.web_render_status or _ignore_status:
             return f'{(web_render_local if use_local else web_render)}source?url={url}'
         else:
             return url
     else:
-        if Info.web_render_status or ignore_status:
+        if Info.web_render_status or _ignore_status:
             return (web_render_local if use_local else web_render) + method
         else:
             return ''
@@ -47,7 +48,7 @@ async def check_web_render() -> Tuple[bool, bool]:
     if web_render_status:
         try:
             Logger.info('[WebRender] Checking WebRender status...')
-            await get_url(webrender('source', ping_url, ignore_status=True), 200, request_private_ip=True)
+            await get_url(webrender('source', ping_url, _ignore_status=True), 200, request_private_ip=True)
             Logger.info('[WebRender] WebRender is working as expected.')
         except Exception:
             Logger.error('[WebRender] WebRender is not working as expected.')
