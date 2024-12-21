6423c8e1375519cb43d13e4ea41924799a42fb7f
好耶！ <qrz20081004@outlook.com>
2024-12-21 20:47:11 +0800

    ?
    Signed-off-by: 好耶！ <qrz20081004@outlook.com>

--- modules/msweather/__init__.py
+++ modules/msweather/__init__.py
@@ -1,8 +1,10 @@
+import orjson as json
+
+from core.utils.web_render import webrender
+from core.utils.http import download
 from core.builtins import Bot, Image, Plain
 from core.component import module
 from urllib.parse import quote
-
-from modules.wiki.utils.screenshot_image import generate_screenshot_v2
 
 msw = module('msweather', desc='MSN天气查询', developers='haoye_qwq', alias='weather')
 
@@ -12,26 +14,37 @@ async def _now(msg: Bot.MessageSession):
     city = msg.parsed_msg['<city>']
     url = f"www.msn.cn/zh-cn/weather/forecast/in-{city}"
     url = 'https://' + quote(url)
-    img = await generate_screenshot_v2(url, element=[
-        '.weatherOverview_root-DS-EntryPoint1-1'], content_mode=False)
-    msg_ = []
-    if img:
-        for i in img:
-            msg_.append(Image(i))
-    msg_.append(Plain(url))
-    await msg.finish(msg_)
+    weather_now = [
+        Image(await download(
+            webrender("page_screenshot"),
+            status_code=200,
+            headers={"Content-Type": "application/json"},
+            method="POST",
+            post_data=json.dumps({"url": url}),
+            attempt=1,
+            timeout=30,
+            request_private_ip=True,
+        )),
+        Plain(url)
+    ]
+    await msg.finish(weather_now)
 
-
-@msw.command('month <city> {获取周天气}')
-async def _month(msg: Bot.MessageSession):
-    city = msg.parsed_msg['<city>']
-    url = f"www.msn.cn/zh-cn/weather/forecast/in-{city}"
-    url = 'https://' + quote(url)
-    img = await generate_screenshot_v2(url, element=[
-        '.monthCalendarRoot-DS-EntryPoint1-1'], content_mode=False)
-    msg_ = []
-    if img:
-        for i in img:
-            msg_.append(Image(i))
-    msg_.append(Plain(url))
-    await msg.finish(msg_)
+# @msw.command('month <city> {获取周天气}')
+# async def _month(msg:Bot.MessageSession):
+#     city = msg.parsed_msg['<city>']
+#     url = f"www.msn.cn/zh-cn/weather/forecast/in-{city}"
+#     url = 'https://'+quote(url)
+#     weather_now = [
+#         Image(await download(
+#             webrender("section_screenshot"),
+#             status_code=200,
+#             headers={"Content-Type": "application/json"},
+#             method="POST",
+#             post_data=json.dumps({"url": url, "section": ['#WeatherOverviewCurrentSection']}),
+#             attempt=1,
+#             timeout=30,
+#             request_private_ip=True,
+#         )),
+#         Plain(url)
+#     ]
+#     await msg.finish(weather_now)
