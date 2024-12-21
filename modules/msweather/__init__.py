04989c8d45182f4f1c24a1269e937f84234aa35b
好耶！ <qrz20081004@outlook.com>
2024-12-21 20:34:21 +0800

    ?
    Signed-off-by: 好耶！ <qrz20081004@outlook.com>

--- modules/msweather/__init__.py
+++ modules/msweather/__init__.py
@@ -6,6 +6,8 @@ from core.builtins import Bot, Image, Plain
 from core.component import module
 from urllib.parse import quote
 
+from modules.wiki.utils.screenshot_image import generate_screenshot_v2
+
 msw = module('msweather', desc='MSN天气查询', developers='haoye_qwq', alias='weather')
 
 
@@ -14,37 +16,26 @@ async def _now(msg: Bot.MessageSession):
     city = msg.parsed_msg['<city>']
     url = f"www.msn.cn/zh-cn/weather/forecast/in-{city}"
     url = 'https://' + quote(url)
-    weather_now = [
-        Image(await download(
-            webrender("section_screenshot"),
-            status_code=200,
-            headers={"Content-Type": "application/json"},
-            method="POST",
-            post_data=json.dumps({"url": url, "section": ['#WeatherOverviewCurrentSection']}),
-            attempt=1,
-            timeout=30,
-            request_private_ip=True,
-        )),
-        Plain(url)
-    ]
-    await msg.finish(weather_now)
+    img = await generate_screenshot_v2(url, element=[
+        'div#WeatherOverviewCurrentSection.weatherOverview_root-DS-EntryPoint1-1'], content_mode=False)
+    msg_ = []
+    if img:
+        for i in img:
+            msg_.append(Image(i))
+    msg_.append(Plain(url))
+    await msg.finish(msg_)
+
 
 @msw.command('month <city> {获取周天气}')
-async def _month(msg:Bot.MessageSession):
+async def _month(msg: Bot.MessageSession):
     city = msg.parsed_msg['<city>']
     url = f"www.msn.cn/zh-cn/weather/forecast/in-{city}"
-    url = 'https://'+quote(url)
-    weather_now = [
-        Image(await download(
-            webrender("section_screenshot"),
-            status_code=200,
-            headers={"Content-Type": "application/json"},
-            method="POST",
-            post_data=json.dumps({"url": url, "section": ['#WeatherOverviewCurrentSection']}),
-            attempt=1,
-            timeout=30,
-            request_private_ip=True,
-        )),
-        Plain(url)
-    ]
-    await msg.finish(weather_now)
+    url = 'https://' + quote(url)
+    img = await generate_screenshot_v2(url, element=[
+        'div.monthCalendarRoot-DS-EntryPoint1-1.visualReady'], content_mode=False)
+    msg_ = []
+    if img:
+        for i in img:
+            msg_.append(Image(i))
+    msg_.append(Plain(url))
+    await msg.finish(msg_)
