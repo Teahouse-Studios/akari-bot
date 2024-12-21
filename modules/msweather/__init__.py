2f6025225cc4302af7c91ad49c01462eda24acea
好耶！ <qrz20081004@outlook.com>
2024-12-21 20:15:39 +0800

    ?
    Signed-off-by: 好耶！ <qrz20081004@outlook.com>

--- modules/msweather/__init__.py
+++ modules/msweather/__init__.py
@@ -4,6 +4,7 @@ from core.utils.web_render import webrender
 from core.utils.http import download
 from core.builtins import Bot, Image, Plain
 from core.component import module
+from urllib.parse import quote
 
 msw = module('msweather', desc='MSN天气查询', developers='haoye_qwq', alias='weather')
 
@@ -12,6 +13,7 @@ msw = module('msweather', desc='MSN天气查询', developers='haoye_qwq', alias=
 async def _now(msg: Bot.MessageSession):
     city = msg.parsed_msg['<city>']
     url = f"https://www.msn.cn/zh-cn/weather/forecast/in-%E6%B2%B3%E5%8D%97{city}"
+    url = quote(url)
     weather_now = [
         Image(await download(
             webrender("element_screenshot"),
@@ -31,6 +33,7 @@ async def _now(msg: Bot.MessageSession):
 async def _month(msg:Bot.MessageSession):
     city = msg.parsed_msg['<city>']
     url = f"https://www.msn.cn/zh-cn/weather/forecast/in-%E6%B2%B3%E5%8D%97{city}"
+    url = quote(url)
     weather_now = [
         Image(await download(
             webrender("element_screenshot"),
