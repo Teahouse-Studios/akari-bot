1c635b22ede05d4c0293440e05705bd041d269f7
多羅狼 <dorowolf314@gmail.com>
2025-01-26 15:13:07 +0800

    update

--- core/database_v2/__init__.py
+++ core/database_v2/__init__.py
@@ -1,21 +1,20 @@
-
 from core.config import Config
 from core.constants import db_path_default
 from tortoise import Tortoise
 
 db_link = Config("db_path", default=db_path_default, secret=True)
-models = [
-    'core.database_v2.models',
-    'modules.cytoid.models',
-    'modules.maimai.models',
-    'modules.osu.models',
-    'modules.phigros.models']
 
 
 async def init_db():
     await Tortoise.init(
         db_url=db_link,
-        modules={'models': models, },
+        modules={'models': [
+            'core.database_v2.models',
+            'modules.cytoid.models',
+            'modules.maimai.models',
+            'modules.osu.models',
+            'modules.phigros.models']
+        },
     )
 
     await Tortoise.generate_schemas()
--- core/database_v2/models.py
+++ core/database_v2/models.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import hashlib
 import uuid
 from collections import Counter
 from datetime import datetime, UTC
@@ -359,6 +360,42 @@ class JobQueuesTable(Model):
         ).all()
 
 
+class DirtyWordCache(Model):
+    desc = fields.TextField(pk=True)
+    result = fields.JSONField(default={})
+    timestamp = fields.DatetimeField(auto_now=True)
+
+    class Meta:
+        table = "dirty_word_cache"
+
+    @classmethod
+    async def check(cls, query_word):
+        query = await cls.filter(desc=query_word).first()
+
+        if query and datetime.now().timestamp() - query.timestamp.timestamp() > 86400:
+            await query.delete()
+
+        return query
+
+
+class CrowdinActivityRecords(Model):
+    hash_id = fields.TextField(pk=True)
+
+    class Meta:
+        table = "crowdin_activity_records"
+
+    @classmethod
+    async def check(cls, txt: str):
+        query_hash = hashlib.md5(txt.encode(encoding="UTF-8"), usedforsecurity=False).hexdigest()
+
+        query = await cls.filter(hash_id=query_hash).first()
+        if not query:
+            query = await cls.create(hash_id=query_hash)
+            await query.save()
+            return False
+        return True
+
+
 __all__ = [
     "SenderInfo",
     "TargetInfo",
@@ -367,4 +404,6 @@ __all__ = [
     "DBVersion",
     "UnfriendlyActionRecords",
     "JobQueuesTable",
+    "DirtyWordCache",
+    "CrowdinActivityRecords",
 ]
