cd394ba88daae36caec160ef0414a3fe3dae5243
多羅狼 <Dorowolf314@gmail.com>
2025-04-10 13:45:32 +0800

    update

--- core/database/link.py
+++ core/database/link.py
@@ -4,11 +4,19 @@ from core.config import Config
 from core.constants import db_path_default, database_path
 
 db_link = Config("db_path", default=db_path_default, secret=True)
+db_type = db_link.split("://")[0].split("+")[0]
 db_path = database_path
 
-db_type = db_link.split("://")[0].split("+")[0]
 if db_type == "sqlite":
-    db_path = os.path.dirname(db_link.replace("sqlite://", "").lstrip("/"))
+    # 兼容 SQLAlchemy 格式 SQLite 连接串
+    absolute = False
+    if db_link.startswith("sqlite:////"):
+        db_link = db_link.replace("sqlite:////", "sqlite:///")
+        absolute = True
+    if db_link.startswith("sqlite:///") and not absolute:
+        db_link = db_link.replace("sqlite:///", "sqlite://")
+
+    db_path = os.path.dirname(db_link.replace("sqlite://", ""))
 os.makedirs(db_path, exist_ok=True)
 
 
