aead3c114d4d8c0833cf2532c955016bab9f5ac1
多羅狼 <Dorowolf314@gmail.com>
2025-05-16 12:20:48 +0800

    Update petal.py

--- modules/core/petal.py
+++ modules/core/petal.py
@@ -32,8 +32,8 @@ async def _(msg: Bot.MessageSession):
         await msg.finish(I18NContext("core.message.petal.sign.already"))
 
 
-@petal_.command("give <user> <petal> {[I18N:core.help.petal.give]}")
-async def _(msg: Bot.MessageSession, user: str, petal: int):
+@petal_.command("give <petal> <user> {[I18N:core.help.petal.give]}")
+async def _(msg: Bot.MessageSession, petal: int, user: str):
     if petal <= 0:
         await msg.finish(I18NContext("petal.message.count.invalid"))
     if not user.startswith(f"{msg.target.client_name}|"):
@@ -53,47 +53,47 @@ async def _(msg: Bot.MessageSession, user: str, petal: int):
         await msg.finish()
 
 
-@petal_.command(["[<sender>]",
-                 "modify <petal> [<sender>]",
-                 "clear [<sender>]"],
+@petal_.command(["[<user>]",
+                 "modify <petal> [<user>]",
+                 "clear [<user>]"],
                 required_superuser=True)
 async def _(msg: Bot.MessageSession):
-    sender = msg.parsed_msg.get("<sender>", False)
+    user = msg.parsed_msg.get("<user>", False)
     if msg.parsed_msg.get("modify", False):
         petal = msg.parsed_msg.get("<petal>", False)
-        if sender:
+        if user:
             if not any(sender.startswith(f"{sender_from}|") for sender_from in sender_list):
                 await msg.finish(I18NContext("message.id.invalid.sender", sender=msg.target.sender_from))
-            sender_info = await SenderInfo.get_by_sender_id(sender)
+            sender_info = await SenderInfo.get_by_sender_id(user)
             await sender_info.modify_petal(petal)
-            await msg.finish(I18NContext("core.message.petal.modify", sender=sender, add_petal=petal, petal=sender_info.petal))
+            await msg.finish(I18NContext("core.message.petal.modify", sender=user, add_petal=petal, petal=sender_info.petal))
         else:
             await msg.sender_info.modify_petal(petal)
             await msg.finish(I18NContext("core.message.petal.modify.self", add_petal=petal, petal=sender_info.petal))
     elif msg.parsed_msg.get("clear", False):
-        if sender:
+        if user:
             if not any(sender.startswith(f"{sender_from}|") for sender_from in sender_list):
                 await msg.finish(I18NContext("message.id.invalid.sender", sender=msg.target.sender_from))
-            sender_info = await SenderInfo.get_by_sender_id(sender, create=False)
+            sender_info = await SenderInfo.get_by_sender_id(user, create=False)
             if not sender_info:
                 if not await msg.wait_confirm(I18NContext("message.id.init.sender.confirm"), append_instruction=False):
                     await msg.finish()
-                await SenderInfo.create(sender_id=sender)
+                await SenderInfo.create(sender_id=user)
             await sender_info.clear_petal()
-            await msg.finish(I18NContext("core.message.petal.clear", sender=sender))
+            await msg.finish(I18NContext("core.message.petal.clear", sender=user))
         else:
             await msg.sender_info.clear_petal()
             await msg.finish(I18NContext("core.message.petal.clear.self"))
     else:
-        if sender:
+        if user:
             if not any(sender.startswith(f"{sender_from}|") for sender_from in sender_list):
                 await msg.finish(I18NContext("message.id.invalid.sender", sender=msg.target.sender_from))
-            sender_info = await SenderInfo.get_by_sender_id(sender, create=False)
+            sender_info = await SenderInfo.get_by_sender_id(user, create=False)
             if not sender_info:
                 if not await msg.wait_confirm(I18NContext("message.id.init.sender.confirm"), append_instruction=False):
                     await msg.finish()
-                await SenderInfo.create(sender_id=sender)
-            await msg.finish(I18NContext("core.message.petal", sender=sender, petal=sender_info.petal))
+                await SenderInfo.create(sender_id=user)
+            await msg.finish(I18NContext("core.message.petal", sender=user, petal=sender_info.petal))
         else:
             await msg.finish(I18NContext("core.message.petal.self", petal=msg.petal))
         
