b0d7077c8715322e4cc809992883a584cfa34428
多羅狼 <Dorowolf314@gmail.com>
2025-08-05 17:54:35 +0800

    add heartbeat

--- bots/web/bot.py
+++ bots/web/bot.py
@@ -9,7 +9,7 @@ from bots.web.api import *  # noqa: E402
 from bots.web.info import *  # noqa: E402
 from bots.web.client import web_host, avaliable_web_port  # noqa: E402
 from bots.web.context import WebContextManager  # noqa: E402
-from bots.web.utils import generate_webui_config  # noqa: E402
+from bots.web.webui_config import generate_webui_config  # noqa: E402
 from core.builtins.bot import Bot  # noqa: E402
 from core.builtins.message.chain import MessageChain  # noqa: E402
 from core.builtins.session.info import SessionInfo  # noqa: E402
@@ -34,7 +34,17 @@ async def websocket_chat(websocket: WebSocket):
         while True:
             rmessage = await websocket.receive_text()
             if rmessage:
-                message = json.loads(rmessage)
+                try:
+                    message = json.loads(rmessage)
+                except json.JSONDecodeError:
+                    continue
+
+                if message["action"] == "heartbeat" and message["message"] == "ping!":
+                    Logger.debug("Heartbeat received.")
+                    resp = {"action": "heartbeat", "message": "pong!"}
+                    await websocket.send_text(json.dumps(resp).decode())
+                    continue
+
                 msg_chain = MessageChain.assign(message["message"][0]["content"])
                 session = await SessionInfo.assign(target_id=target_id,
                                                    sender_id=sender_id,
@@ -78,6 +88,6 @@ if Config("enable", True, table_name="bot_web") or __name__ == "__main__":
         Logger.warning("HTTPS is disabled. HTTP mode is insecure and should only be used in trusted environments.")
 
     if os.path.exists(webui_path):
-        generate_webui_config(enable_https, default_locale)
+        generate_webui_config()
 
     uvicorn.run(app, host=web_host, port=avaliable_web_port, log_level="info")
---
+++ bots/web/webui_config.py
@@ -0,0 +1,20 @@
+import os
+
+import orjson as json
+
+from core.constants.path import webui_path
+from core.config import Config
+
+
+def generate_webui_config():
+    webui_config_path = os.path.join(webui_path, "config.json")
+    if not os.path.exists(webui_config_path):
+        with open(webui_config_path, "wb") as f:
+            f.write(json.dumps(
+                {"enable_https": Config("enable_https", False, table_name="bot_web"),
+                 "locale": Config("default_locale", cfg_type=str),
+                 "heartbeat_interval": Config("heartbeat_interval", 30, table_name="bot_web"),
+                 "heartbeat_timeout": Config("heartbeat_timeout", 5, table_name="bot_web"),
+                 "heartbeat_prompt": Config("heartbeat_prompt", 3, table_name="bot_web")
+                 },
+                option=json.OPT_INDENT_2))
