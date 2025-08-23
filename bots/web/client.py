c9d46e577ae35ab4aaf23c2bd529066345983802
多羅狼 <Dorowolf314@gmail.com>
2025-08-23 17:18:22 +0800

    fix

--- bots/web/client.py
+++ bots/web/client.py
@@ -7,20 +7,25 @@ from fastapi import FastAPI
 from fastapi.middleware.wsgi import WSGIMiddleware
 from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
-from flask import Flask, abort, send_from_directory
+from flask import Flask, send_from_directory
 from slowapi import Limiter
+from fastapi.responses import FileResponse
 from slowapi.util import get_remote_address
 from tortoise import Tortoise
 
 from bots.web.info import *
 from core.client.init import client_init
 from core.config import Config
-from core.constants.path import webui_path
+from core.constants.path import assets_path, webui_path
 from core.database.models import JobQueuesTable, SenderInfo
 from core.logger import Logger
 from core.utils.socket import find_available_port, get_local_ip
 
-from akari_bot_webui.entrypoint import dist_path
+try:
+    from akari_bot_webui.entrypoint import dist_path
+except ImportError:
+    dist_path = ""
+
 
 enable_https = Config("enable_https", default=False, table_name="bot_web")
 protocol = "https" if enable_https else "http"
@@ -30,6 +35,7 @@ web_port = Config("web_port", 6485, table_name="bot_web")
 
 avaliable_web_port = find_available_port(web_port)
 
+allow_origins = Config("allow_origins", default=[], secret=True, table_name="bot_web")
 jwt_secret = Config("jwt_secret", cfg_type=str, secret=True, table_name="bot_web")
 
 
@@ -59,7 +65,7 @@ def _webui_message():
 async def lifespan(app: FastAPI):
     await client_init(target_prefix_list, sender_prefix_list)
     await SenderInfo.update_or_create(defaults={"superuser": True}, sender_id=f"{sender_prefix}|0")
-    if os.path.exists(webui_path):
+    if os.path.exists(dist_path):
         Logger.info(_webui_message())
     yield
     await asyncio.sleep(3)  # 等待 server 清理进程
@@ -75,7 +81,7 @@ ph = PasswordHasher()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=Config("allow_origins", cfg_type=(str, list), secret=True, table_name="bot_web"),
+    allow_origins=allow_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -85,12 +91,13 @@ app.add_middleware(
 if os.path.exists(dist_path):
     flask_app = Flask(__name__, root_path="webui")
 
-    @flask_app.route("/")
+    @flask_app.route("/", defaults={"path": ""})
     @flask_app.route("/<path:path>")
-    def serve_webui(path=None):
-        if not path:
-            return send_from_directory(dist_path, "index.html")
-        return send_from_directory(dist_path, path)
+    def serve_webui(path):
+        file_path = os.path.join(dist_path, path)
+        if path != "" and os.path.exists(file_path):
+            return send_from_directory(dist_path, path)
+        return send_from_directory(dist_path, "index.html")
 
     app.mount("/webui", WSGIMiddleware(flask_app), 'webui')
 
@@ -102,3 +109,8 @@ else:
     @app.get("/")
     async def redirect_to_api():
         return RedirectResponse(url="/api")
+
+
+@app.get("/favicon.ico")
+async def favicon():
+    return FileResponse(os.path.join(assets_path, "favicon.ico"))
