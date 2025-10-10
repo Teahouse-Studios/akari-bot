f9fae201255f7c697dcfa614d5da3537e1d35f11
多羅狼 <Dorowolf314@gmail.com>
2025-10-10 11:35:22 +0800

    Update base.py

--- core/config/base.py
+++ core/config/base.py
@@ -28,7 +28,7 @@ class Config:
     enable_analytics: bool = True
     enable_commit_url: bool = True
     enable_dirty_check: bool = False
-    check_use_textscan_v1 = False
+    check_use_textscan_v1: bool = False
     enable_db: bool = False
     enable_eval: bool = False
     enable_urlmanager: bool = False
