8d424d2b27ea57ad0cd835ea2d74d3388597cae7
多羅狼 <Dorowolf314@gmail.com>
2025-11-02 17:28:55 +0800

    fix

--- core/web_render.py
+++ core/web_render.py
@@ -14,8 +14,9 @@ remote_web_render_url = Config("remote_web_render_url", cfg_type=str, table_name
 web_render_browser = Config("browser_type", "chrome", table_name="webrender")
 browser_executable_path = Config("browser_executable_path", cfg_type=str, table_name="webrender")
 
-web_render = WebRender(debug=True,
+web_render = WebRender(debug=False,
                        remote_webrender_url=remote_web_render_url,
+                       remote_only=bool(remote_web_render_url),
                        export_logs=True,
                        logs_path=str(logs_path))
 
