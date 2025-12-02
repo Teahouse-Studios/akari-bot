f6f4796632612b917cac6a66b7867d05a2dc40a6
多羅狼 <Dorowolf314@gmail.com>
2025-12-02 09:41:05 +0800

    fix ExpiringTempDict

--- core/utils/temp.py
+++ core/utils/temp.py
@@ -149,13 +149,12 @@ class TempList:
 class ExpiringTempDict:
     _registry: ClassVar[list] = []
     _lock: ClassVar[threading.RLock] = threading.RLock()
+    _clear_lock: ClassVar[asyncio.Lock] = asyncio.Lock()
 
     def __init__(self, exp: Union[int, float] = 86400.0, ts: Union[int, float] = time.time(), data: Any = None):
         self.exp = exp
         self.data = data or {}
         self.ts = float(ts)
-
-    def __post_init__(self):
         with self._lock:
             self.__class__._registry.append(self)
 
@@ -203,12 +202,10 @@ class ExpiringTempDict:
                     if should_delete:
                         to_delete.append(k)
                 else:
-                    to_delete.append(k)
-
+                    continue
             for k in to_delete:
                 del self.data[k]
-
-            if hasattr(self, "_ts") and self.is_expired() and not self.data:
+            if hasattr(self, "ts") and self.is_expired() and not self.data:
                 return True
             return False
 
@@ -221,16 +218,16 @@ class ExpiringTempDict:
                     if should_delete:
                         to_delete.append(k)
                 else:
-                    to_delete.append(k)
+                    continue
             for k in to_delete:
                 del self.data[k]
-            if hasattr(self, "_ts") and self.is_expired() and not self.data:
+            if hasattr(self, "ts") and self.is_expired() and not self.data:
                 return True
             return False
 
     @classmethod
     async def clear_all(cls):
-        async with asyncio.Lock():
+        async with cls._clear_lock:
             for obj in cls._registry:
                 await obj.async_clear_expired()
 
@@ -336,9 +333,6 @@ class ExpiringTempDict:
 
     def __ne__(self, other):
         return not self.__eq__(other)
-
-    def __hash__(self):
-        return hash(frozenset(self.data.items()))
 
     def __bool__(self):
         return bool(self.data)
