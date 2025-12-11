656e47734504e7a86585a8d06badaa9644f8d97c
多羅狼 <Dorowolf314@gmail.com>
2025-12-11 18:39:42 +0800

    Update background_tasks.py

--- core/server/background_tasks.py
+++ core/server/background_tasks.py
@@ -11,9 +11,13 @@ from core.web_render import init_web_render, web_render
 
 
 @Scheduler.scheduled_job(IntervalTrigger(minutes=60))
-async def bg():
+async def _():
     await SessionTaskManager.bg_check()
     await JobQueuesTable.clear_task()
+
+
+@Scheduler.scheduled_job(IntervalTrigger(seconds=1), max_instances=1)
+async def _():
     await ExpiringTempDict.clear_all()
 
 
