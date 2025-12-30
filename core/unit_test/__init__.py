863a333da5bcf7c512823e91bd7c5a205cd4dcb2
多羅狼 <Dorowolf314@gmail.com>
2025-12-30 22:45:57 +0800

    u

--- core/unit_test/__init__.py
+++ core/unit_test/__init__.py
@@ -9,20 +9,21 @@ _REGISTRY: List[dict] = []
 def case(
     input: str,
     expected: Optional[Chainable] = None,
-    note: Optional[str] = None,
+    note: Optional[str] = None
 ):
     """
     注册一个单元测试案例。
 
-    參數:
-    - input: 预期输入。
-    - expected: 预期输出；可以是字符串或任意可比較形态；若为 None 表示人工判定。
-    - note: 额外说明文字。
-
-    用法示例:
+    示例：
+    ```
     @case(input="~test say Hello", expected="Hello is Hello")
-    async def _(msg: Bot.MessageSession):
-        ...
+    @test.command("say <word>")
+    async def _(msg: Bot.MessageSession, word: str):
+        await msg.finish(f"{word} is {msg.parsed_msg["<word>"]}")
+    ```
+    :param input: 预期输入。
+    :param expected: 预期输出，若为 None 则表示人工判断。
+    :param note: 额外说明文字。
     """
 
     def _decorator(fn: Callable):
