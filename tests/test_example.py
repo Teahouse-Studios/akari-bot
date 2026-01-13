e4d344177105869bd44db337c79bea8e4ca5e791
多羅狼 <Dorowolf314@gmail.com>
2026-01-13 16:17:04 +0800

    u

--- tests/test_example.py
+++ tests/test_example.py
@@ -4,7 +4,7 @@ from core.tester import Tester, func_case
 @func_case
 async def _(tester: Tester):
     """This is a test example"""
-    await tester.input("~echo hi", True, "should output hi")
+    await tester.input("~echo hi", None, "should output hi")
     await tester.input("~echo hello", "hello", "should output hello")
     await tester.input(["~echo", "lol"], True, "should output lol")
     return tester
