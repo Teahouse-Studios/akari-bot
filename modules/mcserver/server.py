c403b41165558035e31a97fd2ed9848100147f90
好耶！ <86039582+cloudw233@users.noreply.github.com>
2026-04-21 22:31:32 +0800

    format

--- modules/mcserver/server.py
+++ modules/mcserver/server.py
@@ -1,7 +1,7 @@
 import re
-import orjson as json
 
-from mcstatus import JavaServer, BedrockServer
+import orjson as json
+from mcstatus import BedrockServer, JavaServer
 
 from core.builtins.message.internal import I18NContext
 from core.logger import Logger
@@ -17,7 +17,7 @@ async def query_java_server(address: str, raw: bool = False, showplayer: bool = 
         except TimeoutError:
             host = address.rsplit(":", 1)[0]
             port = (address.rsplit(":", 1) + [None])[1]
-            url = f'http://motd.wd-api.com/v1/java?host={host}&port={port if port else 25565}'
+            url = f"http://motd.wd-api.com/v1/java?host={host}&port={port if port else 25565}"
             status: dict = json.loads(await get_url(url, 200, logging_err_resp=False))
         Logger.debug(str(status))
         query_msg.append("[JE]")
@@ -29,18 +29,21 @@ async def query_java_server(address: str, raw: bool = False, showplayer: bool = 
             query_msg.append(description.get("text", ""))
             query_msg.append("".join(extra.get("text", "") for extra in description.get("extra", [])))
 
-        onlinesplayer = \
-            f"{str(I18NContext('server.message.player'))}{status["players"]["online"]} / {status["players"]["max"]}"
+        onlinesplayer = (
+            f"{str(I18NContext('server.message.player'))}{status['players']['online']} / {status['players']['max']}"
+        )
         query_msg.append(onlinesplayer)
 
         if showplayer:
-            playerlist = [
-                player["name"] for player in status["players"]["sample"]] \
-                if status.get("players",{}).get("sample") else []
+            playerlist = (
+                [player["name"] for player in status["players"]["sample"]]
+                if status.get("players", {}).get("sample")
+                else []
+            )
             players_text = "\n".join(playerlist) if playerlist else str(I18NContext("message.none"))
             query_msg.append(str(I18NContext("server.message.player.current")) + "\n" + players_text)
 
-        if status.get("version") and status.get("version",{}).get("name"):
+        if status.get("version") and status.get("version", {}).get("name"):
             query_msg.append(str(I18NContext("server.message.version")) + status["version"]["name"])
 
     except OSError:
@@ -66,14 +69,14 @@ async def query_bedrock_server(address, raw=False) -> str:
             status["motd"] = status["motd"] + " - " + status.get("map_name", "")
         except TimeoutError:
             host = address.rsplit(":", 1)[0]
-            port = (address.rsplit(":", 1)+[None])[1]
-            url = f'http://motd.wd-api.com/v1/bedrock?host={host}&port={port if port else 19132}'
+            port = (address.rsplit(":", 1) + [None])[1]
+            url = f"http://motd.wd-api.com/v1/bedrock?host={host}&port={port if port else 19132}"
             raw_data = json.loads(await get_url(url, 200, logging_err_resp=False))
             status: dict = {
                 key: value
-                for key,value in zip(
-                    ["edition","motd_1","_","version","players_online","players_max","__","motd_2","gamemode"],
-                    raw_data["data"].split(";")
+                for key, value in zip(
+                    ["edition", "motd_1", "_", "version", "players_online", "players_max", "__", "motd_2", "gamemode"],
+                    raw_data["data"].split(";"),
                 )
             }
             Logger.debug(str(status))
@@ -82,8 +85,9 @@ async def query_bedrock_server(address, raw=False) -> str:
         query_msg.append("[BE]")
         query_msg.append(status["motd"])
 
-        player_count = \
-            f"{str(I18NContext('server.message.player'))}{status["players_online"]} / {status["players_max"]}"
+        player_count = (
+            f"{str(I18NContext('server.message.player'))}{status['players_online']} / {status['players_max']}"
+        )
         query_msg.append(player_count)
 
         if status.get("version") and status.get("version", {}).get("name"):
