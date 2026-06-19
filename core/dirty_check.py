ad88a92d07c552b705951839b993ae3585043fdd
OasisAkari <31803608+OasisAkari@users.noreply.github.com>
2026-06-20 02:05:01 +0800

    temp patch

--- core/dirty_check.py
+++ core/dirty_check.py
@@ -122,6 +122,7 @@ async def check(
     session: MessageSession | None = None,
     confidence: float = 60,
     additional_text: str | None = None,
+    force=False,
 ) -> list[dict]:
     """检查字符串。
 
@@ -138,8 +139,11 @@ async def check(
     if isinstance(text, (list, MessageChain)):
         text = [str(x) for x in text]
 
-    if not access_key_id or not access_key_secret or not (session and session.session_info.require_check_dirty_words):
+    if not access_key_id or not access_key_secret:
         Logger.warning("Dirty words filter was disabled, skip.")
+        return [{"content": t, "status": True, "original": t} for t in text]
+    if not force and (session and not session.session_info.require_check_dirty_words):
+        Logger.warning("Dirty words filter was disabled by session, skip.")
         return [{"content": t, "status": True, "original": t} for t in text]
 
     if not text:
@@ -152,10 +156,13 @@ async def check(
     for q in query_list:
         for pq in query_list[q]:
             if not query_list[q][pq]:
-                cache = await DirtyWordCache.check(pq)
-                if cache:
-                    query_list[q][pq] = parse_data(pq, cache.result, confidence, additional_text)
-
+                try:
+                    cache = await DirtyWordCache.check(pq)
+                    if cache:
+                        query_list[q][pq] = parse_data(pq, cache.result, confidence, additional_text)
+                except Exception:
+                    Logger.warning("Failed to get cache, skip.")
+                    Logger.exception()
     call_api_list = {}
     for q in query_list:
         for pq in query_list[q]:
@@ -283,10 +290,13 @@ async def check(
                     query_list[n][x] = final_parse_result
 
                 if res_list:
-                    hash_id = hashlib.sha256(x.encode("utf-8")).hexdigest()
-                    last_api_data = res_list[-1][2]
-                    await DirtyWordCache.create(hash_id=hash_id, desc=x, result=last_api_data)
-
+                    try:
+                        hash_id = hashlib.sha256(x.encode("utf-8")).hexdigest()
+                        last_api_data = res_list[-1][2]
+                        await DirtyWordCache.create(hash_id=hash_id, desc=x, result=last_api_data)
+                    except Exception:
+                        Logger.warning("Failed to create dirty word cache, skip.")
+                        Logger.exception()
     results = []
     Logger.debug(query_list)
     for q in query_list.values():
