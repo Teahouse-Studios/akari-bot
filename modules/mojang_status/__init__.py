1125e71e31edb52ec3a3204e83141952d358c6fb
OasisAkari <31803608+OasisAkari@users.noreply.github.com>
2026-07-06 19:20:10 +0800

    upd

--- modules/mojang_status/__init__.py
+++ modules/mojang_status/__init__.py
@@ -6,24 +6,28 @@ from core.component import module
 from core.utils.http import get_url
 
 mojang_status = module(
-    "mojang_status", desc="{I18N:mojang_status.help.desc}", alias="mcstatus", developers=["Don_Trueno"]
+    "mojang_status", desc="{I18N:mojang_status.help.desc}", alias=["mcstatus", "mjs", "mjsb"], developers=["Don_Trueno"]
 )
-
-MOJANG_SERVICES_ID = ["sessions", "api", "textures", "website", "accounts", "microsoft"]
 
 
 @mojang_status.command()
 async def _(msg: Bot.MessageSession):
+    msg_list = [
+        I18NContext(
+            "mojang_status.message.status",
+        )
+    ]
     try:  # https://www.mcstate.net/docs#api-mojang-status
         data = orjson.loads(await get_url("https://www.mcstate.net/api/mojang-status", 200))
-        msg_list = []
-        for id in MOJANG_SERVICES_ID:
-            status = data["services"][id]["status"]
-            msg_list.append(I18NContext("mojang_status.message.status.each", api=id, status=status))
-        message1 = "\n".join(msg_list)
+        for service in data.get("services", []):
+            msg_list.append(
+                I18NContext(
+                    "mojang_status.message.status.each", api=service.get("label", ""), status=service.get("status", "")
+                )
+            )
     except ValueError as e:
         if str(e).startswith("429"):
-            message1 = msg.session_info.locale.t("mojang_status.message.failed")
+            msg_list.append(I18NContext("mojang_status.message.failed"))
         else:
             raise e
-    return I18NContext("mojang_status.message.status", statuses=message1)
+    await msg.finish(msg_list)
