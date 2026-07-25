3973863b81e017adca71bcb925c27afc0c655cb9
Don-Trueno <don_trueno@foxmail.com>
2026-07-25 09:14:13 +0800

    refactor: get each status directly

--- modules/mojang_status/__init__.py
+++ modules/mojang_status/__init__.py
@@ -1,33 +1,40 @@
-import orjson
-
 from core.builtins.bot import Bot
 from core.builtins.message.internal import I18NContext
 from core.component import module
-from core.utils.http import get_url
+from core.utils.http import request_url
 
 mojang_status = module(
     "mojang_status", desc="{I18N:mojang_status.help.desc}", alias=["mcstatus", "mjs", "mjsb"], developers=["Don_Trueno"]
 )
+
+url = {
+    "account": "https://api.mojang.com",
+    "session": "https://sessionserver.mojang.com",
+    "services": "https://api.minecraftservices.com",
+}
 
 
 @mojang_status.command()
 async def _(msg: Bot.MessageSession):
     msg_list = [
         I18NContext(
-            "mojang_status.message.status",
+            "mojang_status.message.title",
         )
     ]
-    try:  # https://www.mcstate.net/docs#api-mojang-status
-        data = orjson.loads(await get_url("https://www.mcstate.net/api/mojang-status", 200))
-        for service in data.get("services", []):
-            msg_list.append(
-                I18NContext(
-                    "mojang_status.message.status.each", api=service.get("label", ""), status=service.get("status", "")
-                )
-            )
-    except ValueError as e:
-        if str(e).startswith("429"):
-            msg_list.append(I18NContext("mojang_status.message.failed"))
-        else:
-            raise e
+    for u, v in url.items():
+        api = msg.session_info.locale.t("mojang_status.service." + u)
+        try:
+            DATA = await request_url(v, method="GET", logging_err_resp=False, attempt=1)
+            if DATA is not None:
+                status = msg.session_info.locale.t("mojang_status.status.online")
+            else:
+                status = msg.session_info.locale.t("mojang_status.status.empty")
+        except ValueError as e:
+            if str(e).startswith("40"):
+                status = msg.session_info.locale.t("mojang_status.status.online")
+            elif str(e).startswith("50"):
+                status = msg.session_info.locale.t("mojang_status.status.offline")
+            else:
+                status = msg.session_info.locale.t("mojang_status.status.unknown")
+        msg_list.append(I18NContext("mojang_status.message.entry", api=api, status=status))
     await msg.finish(msg_list)
