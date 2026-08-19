c7c74f7b00ee606b54676d512926aee1025c48ea
OasisAkari <31803608+OasisAkari@users.noreply.github.com>
2026-08-19 19:59:14 +0800

    u

--- modules/core/help.py
+++ modules/core/help.py
@@ -518,7 +518,7 @@ async def qqbot_permissions(msg: Bot.MessageSession, qq_group_id: str | None = N
             "core.message.help.qqbot.permissions",
             cmd=ActionText(
                 "/help permissions ",
-                show="/help permissions [<qq群号>]",
+                show=f"/help permissions [<{msg.session_info.locale.t('core.message.help.qqbot.permissions.quick_config.qqgroup_num')}>]",
                 show_on_fallback=False,
             ),
         )
