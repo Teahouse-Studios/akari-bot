eb46eb278ac1b3192dbb439be21a096da3fda3b0
OasisAkari <31803608+OasisAkari@users.noreply.github.com>
2026-09-10 09:45:47 +0800

    Update core.py

--- core/config/core.py
+++ core/config/core.py
@@ -76,6 +76,10 @@ class CoreConfig:
     petal_lost_limit: int = 0
     petal_sign_limit: int = 5
     petal_sign_rate: float = 0.5
+    petal_sign_min: int = 1
+    petal_sign_max: int = 10
+    petal_reset_crontab: str = "0 0 1 * *"
+    petal_rebate_rate: int = 0.2
 
     # 玩笑
     enable_joke: bool = True
