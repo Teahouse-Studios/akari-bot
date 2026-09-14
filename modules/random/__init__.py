64e4b90be4e2d0e748f148ddc228e2db45f4bba7
多羅狼 <Dorowolf314@gmail.com>
2026-09-14 18:26:13 +0800

    fix random

--- modules/random/__init__.py
+++ modules/random/__init__.py
@@ -25,14 +25,14 @@ async def _(msg: Bot.MessageSession, minimum: int, maximum: int):
 
 
 @r.command("choice <choices> ... {{I18N:random.help.choice}}")
-async def _(msg: Bot.MessageSession, choice: str, extra_choices: Param("...", list) = None):
+async def _(msg: Bot.MessageSession, choice: Param("<choices>", str) = None, extra_choices: Param("...", list) = None):
     choices = [choice] + (extra_choices or [])
     c = Random.choice(choices)
     await msg.finish(c)
 
 
 @r.command("shuffle <cards> ... {{I18N:random.help.shuffle}}")
-async def _(msg: Bot.MessageSession, card: str, extra_cards: Param("...", list) = None):
+async def _(msg: Bot.MessageSession, card: Param("<cards>", str) = None, extra_cards: Param("...", list) = None):
     cards = [card] + (extra_cards or [])
     x = Random.shuffle(cards)
     await msg.finish(", ".join(x))
